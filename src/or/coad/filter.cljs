c4eb99e8d3bc1994a99f0cd217ab0d26dd3a34e8
Oliver Runge <oliver.runge@gmail.com>
2020-12-17 15:11:49 +0100

    Move filters into own module

---
+++ src/or/coad/filter.cljs
@@ -0,0 +1,55 @@
+(ns or.coad.filter)
+
+(def shadow
+  [:filter#shadow {:x      "-20%"
+                   :y      "-20%"
+                   :width  "200%"
+                   :height "200%"}
+   [:feOffset {:result "offsetOut"
+               :in     "SourceAlpha"
+               :dx     "1"
+               :dy     "1"}]
+   [:feGaussianBlur {:result        "blurOut"
+                     :in            "offsetOut"
+                     :std-deviation "5"}]
+   [:feBlend {:in   "SourceGraphic"
+              :in2  "blurOut"
+              :mode "normal"}]])
+
+(def shiny
+  [:filter#shiny {:x      0
+                  :y      0
+                  :width  "150%"
+                  :height "150%"}
+   [:feGaussianBlur {:std-deviation 4
+                     :in            "SourceAlpha"
+                     :result        "blur1"}]
+   [:feSpecularLighting {:specular-exponent 20
+                         :lighting-color    "#696969"
+                         :in                "blur1"
+                         :result            "specOut"}
+    [:fePointLight {:x 300
+                    :y 300
+                    :z 500}]]
+   [:feComposite {:k1       0
+                  :k2       1
+                  :k3       1
+                  :k4       0
+                  :operator "arithmetic"
+                  :in       "SourceGraphic"
+                  :in2      "specOut"
+                  :result   "highlight"}]
+   [:feOffset {:dx     14
+               :dy     14
+               :in     "SourceGraphic"
+               :result "offOut"}]
+   [:feColorMatrix {:values "0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0"
+                    :type   "matrix"
+                    :in     "offOut"
+                    :result "matrixOut"}]
+   [:feGaussianBlur {:std-deviation 10
+                     :in            "matrixOut"
+                     :result        "blurOut"}]
+   [:feBlend {:mode "normal"
+              :in   "highlight"
+              :in2  "blurOut"}]])
--- src/or/coad/main.cljs
+++ src/or/coad/main.cljs
@@ -3,6 +3,7 @@
             [hodgepodge.core :refer [local-storage clear!]]
             [or.coad.division :as division]
             [or.coad.escutcheon :as escutcheon]
+            [or.coad.filter :as filter]
             [or.coad.ordinary :as ordinary]
             [re-frame.core :as rf]
             [reagent.core :as r]))
@@ -39,66 +40,11 @@
    (assoc-in db path value)))
                                         ; views
 
-(def filter-shadow
-  [:filter#shadow {:x      "-20%"
-                   :y      "-20%"
-                   :width  "200%"
-                   :height "200%"}
-   [:feOffset {:result "offsetOut"
-               :in     "SourceAlpha"
-               :dx     "1"
-               :dy     "1"}]
-   [:feGaussianBlur {:result        "blurOut"
-                     :in            "offsetOut"
-                     :std-deviation "5"}]
-   [:feBlend {:in   "SourceGraphic"
-              :in2  "blurOut"
-              :mode "normal"}]])
-
-(def filter-shiny
-  [:filter#shiny {:x      0
-                  :y      0
-                  :width  "150%"
-                  :height "150%"}
-   [:feGaussianBlur {:std-deviation 4
-                     :in            "SourceAlpha"
-                     :result        "blur1"}]
-   [:feSpecularLighting {:specular-exponent 20
-                         :lighting-color    "#696969"
-                         :in                "blur1"
-                         :result            "specOut"}
-    [:fePointLight {:x 300
-                    :y 300
-                    :z 500}]]
-   [:feComposite {:k1       0
-                  :k2       1
-                  :k3       1
-                  :k4       0
-                  :operator "arithmetic"
-                  :in       "SourceGraphic"
-                  :in2      "specOut"
-                  :result   "highlight"}]
-   [:feOffset {:dx     14
-               :dy     14
-               :in     "SourceGraphic"
-               :result "offOut"}]
-   [:feColorMatrix {:values "0.2 0 0 0 0 0 0.2 0 0 0 0 0 0.2 0 0 0 0 0 1 0"
-                    :type   "matrix"
-                    :in     "offOut"
-                    :result "matrixOut"}]
-   [:feGaussianBlur {:std-deviation 10
-                     :in            "matrixOut"
-                     :result        "blurOut"}]
-   [:feBlend {:mode "normal"
-              :in   "highlight"
-              :in2  "blurOut"}]])
-
 (def defs
   (into
    [:defs
-    filter-shadow
-    filter-shiny
-    filter-shiny]))
+    filter/shadow
+    filter/shiny]))
 
 (defn render-coat-of-arms [data]
   (let [division   (:division data)
