4108c1075aeb408380b5d6b54ab9d5c10d0d0ca4
Oliver Runge <oliver.runge@gmail.com>
2020-12-18 10:13:01 +0100

    Add svg module
    About to be rewritten anyway, but here's an initial approach to use JS
    functions. This doesn't work so well, tho, as the SVG needs to be
    rendered. A work-around to not have it visible is to position it
    absolutely at 0, 0 and give it an opacity of 0, but even then the
    document needs to be rendered before the values can be used.

---
+++ src/or/coad/svg.cljs
@@ -0,0 +1,27 @@
+(ns or.coad.svg)
+
+(def svg
+  (js/document.createElementNS "http://www.w3.org/2000/svg" "svg"))
+
+(defn path [d]
+  (let [p (js/document.createElementNS "http://www.w3.org/2000/svg" "path")]
+    (.setAttribute p "d" d)
+    (.appendChild svg p)
+    p))
+
+(defn origin [node]
+  (let [point                     (.createSVGPoint svg)
+        ^js/SVGSVGElement  parent (.-parentElement node)
+        ctm                       (.getScreenCTM parent)
+        transformed               (.matrixTransform point ctm)]
+    [(.-x transformed) (.-y transformed)]))
+
+(defn bounding-box [d]
+  (let [p                  (path d)
+        [ox oy]            (origin p)
+        box                (.getBoundingClientRect p)
+        [x y width height] [(.-x box)
+                            (.-y box)
+                            (.-width box)
+                            (.-height box)]]
+    (println ox oy [x y width height])))
