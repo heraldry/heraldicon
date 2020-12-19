47042bdb07a97f78c01b4417c14d2908efa7e03f
Oliver Runge <oliver.runge@gmail.com>
2020-12-19 17:01:05 +0100

    Remove tincture shape growing again
    With the overlap this has no impact at all, just wanted to have it as
    commit, might be useful later. The functions to calculate a shape's
    middle also should be useful.

--- src/or/coad/field_content.cljs
+++ src/or/coad/field_content.cljs
@@ -1,18 +1,15 @@
 (ns or.coad.field-content
   (:require [or.coad.division :as division]
-            [or.coad.svg :as svg]
             [or.coad.tincture :as tincture]))
 
 (defn render [content field]
   (let [division   (:division content)
         ordinaries (:ordinaries content)
-        tincture   (:tincture content)
-        [mx my]    (svg/center (:shape field))]
+        tincture   (:tincture content)]
     [:<>
      (cond
-       tincture [:path {:d         (:shape field)
-                        :transform (str "translate(" mx "," my ") scale(1.1, 1.1) translate(" (- mx) "," (- my) ")")
-                        :fill      (get tincture/tinctures tincture)}]
+       tincture [:path {:d    (:shape field)
+                        :fill (get tincture/tinctures tincture)}]
        division [division/render division field render])
      #_(for [[idx ordinary] (map-indexed vector ordinaries)]
          ^{:key idx} [ordinary/render ordinary])]))
