6c7fa1665cfb8e934d1c0820b2c2aae46b8de9d5
Oliver Runge <oliver.runge@gmail.com>
2020-12-19 16:54:33 +0100

    Add attempt to solve anti-aliasing issue by growing tincture shape
    This seems to be an improvement over naive rendering, but it doesn't
    yield the same result as the overlap.

--- src/or/coad/field_content.cljs
+++ src/or/coad/field_content.cljs
@@ -1,15 +1,18 @@
 (ns or.coad.field-content
   (:require [or.coad.division :as division]
+            [or.coad.svg :as svg]
             [or.coad.tincture :as tincture]))
 
 (defn render [content field]
   (let [division   (:division content)
         ordinaries (:ordinaries content)
-        tincture   (:tincture content)]
+        tincture   (:tincture content)
+        [mx my]    (svg/center (:shape field))]
     [:<>
      (cond
-       tincture [:path {:d    (:shape field)
-                        :fill (get tincture/tinctures tincture)}]
+       tincture [:path {:d         (:shape field)
+                        :transform (str "translate(" mx "," my ") scale(1.1, 1.1) translate(" (- mx) "," (- my) ")")
+                        :fill      (get tincture/tinctures tincture)}]
        division [division/render division field render])
      #_(for [[idx ordinary] (map-indexed vector ordinaries)]
          ^{:key idx} [ordinary/render ordinary])]))
