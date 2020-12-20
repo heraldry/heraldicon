e082986567d51d65c3e469783ae099e0284112a7
Oliver Runge <oliver.runge@gmail.com>
2020-12-20 02:44:58 +0100

    Add some more line styles

--- src/or/coad/line.cljs
+++ src/or/coad/line.cljs
@@ -75,13 +75,108 @@
                   vec)
      :length (* repetitions width)}))
 
+(defn wavy [length reversed?]
+  (let [width       20
+        half-width  (/ width 2)
+        repetitions (-> length
+                        (/ width)
+                        Math/ceil
+                        int)]
+    {:line   (->> ["a" half-width width 0 0 (if reversed? 0 1) [half-width 0]
+                   "a" half-width width 0 0 (if reversed? 1 0) [half-width 0]]
+                  (repeat repetitions)
+                  (apply concat)
+                  vec)
+     :length (* repetitions width)}))
+
+(defn dovetailed [length _]
+  (let [width       10
+        half-width  (/ width 2)
+        third-width (/ width 3)
+        sixth-width (/ width 6)
+        repetitions (-> length
+                        (/ width)
+                        Math/ceil
+                        int)]
+    {:line   (->> ["l"
+                   [third-width 0]
+                   [(- sixth-width) (- half-width)]
+                   [third-width 0]
+                   [third-width 0]
+                   [(- sixth-width) half-width]
+                   [third-width 0]]
+                  (repeat repetitions)
+                  (apply concat)
+                  vec)
+     :length (* repetitions width)}))
+
+(defn raguly [length reversed?]
+  (let [width         10
+        half-width    (/ width 2)
+        quarter-width (/ width 4)
+        repetitions   (-> length
+                          (/ width)
+                          Math/ceil
+                          int)]
+    {:line   (->> (if reversed?
+                    ["l"
+                     [quarter-width 0]
+                     [quarter-width (- half-width)]
+                     [half-width 0]
+                     [(- quarter-width) half-width]
+                     [quarter-width 0]]
+                    ["l"
+                     [quarter-width 0]
+                     [(- quarter-width) (- half-width)]
+                     [half-width 0]
+                     [quarter-width half-width]
+                     [quarter-width 0]])
+                  (repeat repetitions)
+                  (apply concat)
+                  vec)
+     :length (* repetitions width)}))
+
+(defn urdy [length reversed?]
+  (let [width         10
+        quarter-width (/ width 4)
+        eighth-width  (/ width 8)
+        repetitions   (-> length
+                          (/ width)
+                          Math/ceil
+                          int)]
+    {:line   (->> (if reversed?
+                    ["l"
+                     [0 eighth-width]
+                     [quarter-width quarter-width]
+                     [quarter-width (- quarter-width)]
+                     [0 (- quarter-width)]
+                     [quarter-width (- quarter-width)]
+                     [quarter-width quarter-width]
+                     [0 eighth-width]]
+                    ["l"
+                     [0 eighth-width]
+                     [quarter-width quarter-width]
+                     [quarter-width (- quarter-width)]
+                     [0 (- quarter-width)]
+                     [quarter-width (- quarter-width)]
+                     [quarter-width quarter-width]
+                     [0 eighth-width]])
+                  (repeat repetitions)
+                  (apply concat)
+                  vec)
+     :length (* repetitions width)}))
+
 (def kinds
   [["Straight" :straight straight]
    ["Invected" :invected invected]
    ["Engrailed" :engrailed engrailed]
    ["Embattled" :embattled embattled]
    ["Indented" :indented indented]
-   ["Dancetty" :dancetty dancetty]])
+   ["Dancetty" :dancetty dancetty]
+   ["Wavy/undy" :wavy wavy]
+   ["Dovetailed" :dovetailed dovetailed]
+   ["Raguly" :raguly raguly]
+   ["Urdy" :urdy urdy]])
 
 (def kinds-function-map
   (->> kinds
