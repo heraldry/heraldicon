427eedaf77117ee90e5d5bff8f9a096befce5908
Oliver Runge <oliver.runge@gmail.com>
2020-12-23 16:56:24 +0100

    Move supported placeholder colours into config module
    This way the Python can also read it easily and generate-charge-map.py
    can add hints which placeholder colours the variant supports.

--- generate-charge-map.py
+++ generate-charge-map.py
@@ -3,9 +3,10 @@ import os
 from glob import glob
 
 import edn_format
-from edn_format.edn_lex import Keyword as K
+from edn_format.edn_lex import Keyword as K, Symbol
 
 lookup = {}
+placeholder_colours_map = {}
 
 
 def group_by_type(coll):
@@ -19,6 +20,15 @@ def group_by_type(coll):
         groups[type][child_data[K("key")]] = child_data
 
     return groups
+
+
+def get_supported_colours(data):
+    result = set()
+    for colour, key in placeholder_colours_map.items():
+        if colour in data:
+            result.add(key)
+
+    return result
 
 
 def traverse(directory, data_path):
@@ -56,19 +66,38 @@ def traverse(directory, data_path):
         def file_data(f):
             filename = os.path.basename(f)
             name = filename.rsplit(".", 1)[0]
-            return {
+            data = {
                 K("key"): K(name),
                 K("type"): K("variant"),
                 K("name"): name,
                 K("path"): os.path.join(directory, filename),
             }
 
+            supported_colours = get_supported_colours(open(f).read())
+            if supported_colours:
+                data[K("supported-tinctures")] = supported_colours
+
+            return data
+
         data.update(group_by_type(map(file_data, files)))
 
     return data
 
 
+def read_config():
+    global placeholder_colours_map
+    config = edn_format.loads("[" + open("src/or/coad/config.cljs").read() + "]")
+    for form in config:
+        if len(form) > 1 and form[0:2] == (
+            Symbol("def"),
+            Symbol("placeholder-colours"),
+        ):
+            placeholder_colours_map = dict(map(lambda v: [v[1], v[0]], form[2].items()))
+
+
 if __name__ == "__main__":
+    read_config()
+
     os.chdir("main")
     data = traverse("data/charges", [])
     data[K("lookup")] = lookup
