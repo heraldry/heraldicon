ab8af7f401567a3c5bd9e20e483e55d5318588a7
Oliver Runge <oliver.runge@gmail.com>
2020-12-24 10:18:53 +0100

    Speed up SVG to EDN processing
    The lein process starts up slowly, quickly making this script very slow,
    as the number of SVG files grows. This change calls it once with all SVG
    files, which is a good deal faster. However, it means all the results
    share the same output directory, so the SVG files must be unique
    globally. I think that'll happen anyway with the current naming scheme,
    unless a charge name exists in different groups.
    
    If that happens at some point, I'll figure out a remedy.

--- svg-to-clj.sh
+++ svg-to-clj.sh
@@ -2,32 +2,25 @@
 
 rm -rf main/data/charges
 
-(
-  cd assets/optimized-svgs/charges
-  for relpath in **/*(/); do
-    if [ $(ls -1 "$relpath" | grep "[.]svg$" | wc -l) -eq 0 ]; then
-      continue
-    fi
+cd assets/optimized-svgs/charges
+rm -rf hicv
+lein hicv 2clj **/*.svg
+perl -p -i \
+  -e 's/clippathunits/clip-path-units/g;' \
+  -e 's/spreadmethod/spread-method/g;' \
+  -e 's/gradientunits/gradient-units/g;' \
+  -e 's/gradienttransform/gradient-transform/g;' \
+  -e 's/xlink:href/xlink-href/g;' \
+  -e 's/maskunits/mask-units/g;' hicv/*
 
-    rm -rf hicv
-    lein hicv 2clj "$relpath"/*.svg
-    perl -p -i \
-      -e 's/clippathunits/clip-path-units/g;' \
-      -e 's/spreadmethod/spread-method/g;' \
-      -e 's/gradientunits/gradient-units/g;' \
-      -e 's/gradienttransform/gradient-transform/g;' \
-      -e 's/xlink:href/xlink-href/g;' \
-      -e 's/maskunits/mask-units/g;' hicv/*
+for f in **/*.svg; do
+  relpath="$(dirname "$f")"
+  filename="$(basename "$f")"
+  filename="${filename%.*}"
+  output="../../../main/data/charges/$relpath/$filename.edn"
+  mkdir -p "$(dirname "$output")"
+  echo "writing $output"
+  mv "hicv/$filename.clj" "$output"
+done
 
-    for f in hicv/*; do
-      filename="$(basename "$f")"
-      filename="${filename%.*}"
-      output="../../../main/data/charges/$relpath/$filename.edn"
-      mkdir -p "$(dirname "$output")"
-      echo "writing $output"
-      mv "$f" "$output"
-    done
-
-  done
-  rm -rf hicv
-)
+rm -rf hicv
