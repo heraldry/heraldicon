e7f559944efa991816a3cbac48e1844694da5518
Oliver Runge <oliver.runge@gmail.com>
2020-12-25 11:35:58 +0100

    Work towards using a charge as a mask for itself
    This is not done, but I want a snapshot before I try some other things.

--- src/or/coad/charge.cljs
+++ src/or/coad/charge.cljs
@@ -3,12 +3,16 @@
             [clojure.walk :as walk]
             [or.coad.config :as config]
             [or.coad.line :as line]
+            [or.coad.svg :as svg]
             [or.coad.tincture :as tincture]
             [or.coad.vector :as v]
             [re-frame.core :as rf]))
 
 (def placeholder-regex
   (re-pattern (str "(?i)(" (s/join "|" (vals config/placeholder-colours)) ")")))
+
+(def colour-regex
+  (re-pattern (str "(?i)#([a-f0-9]{6}|[a-f0-9]{3})")))
 
 (defn find-charge [charge-map [group & rest]]
   (let [next (get-in charge-map [:groups group])]
@@ -40,9 +44,17 @@
     (or (get tincture kind)
         primary)))
 
-(defn replace-tinctures [string tincture]
+(defn replace-placeholder-colours [string tincture]
   (s/replace string placeholder-regex (fn [[_ match]]
                                         (pick-placeholder-tincture match tincture))))
+
+(defn replace-non-placeholder-colour [string colour]
+  (s/replace string placeholder-regex (fn [[_ match]]
+                                        (println "match" match)
+                                        (let [match (s/lower-case match)]
+                                          (if (get config/placeholder-colours-set match)
+                                            match
+                                            colour)))))
 
 (defn split-style-value [value]
   (-> value
@@ -55,33 +67,52 @@
                         [(keyword (s/trim key)) (s/trim value)])))))
       (into {})))
 
-(defn replace-in-hiccup [hiccup tincture]
-  (walk/postwalk #(cond-> %
-                    (string? %) (replace-tinctures tincture)
-                    (and (vector? %)
-                         (-> % count (= 2))
-                         (-> % first (= :style))) ((fn [[key value]]
-                                                     (if (= key :style)
-                                                       [:style (split-style-value value)]
-                                                       [key value]))))
-                 hiccup))
+(defn fix-string-style-values [data]
+  (walk/postwalk #(if (and (vector? %)
+                           (-> % count (= 2))
+                           (-> % first (= :style))
+                           (-> % second string?))
+                    [:style (split-style-value (second %))]
+                    %)
+                 data))
 
-(defn remove-outlines [hiccup]
+(defn replace-placeholder-colours-everywhere [data tincture]
+  (walk/postwalk #(if (string? %)
+                    (replace-placeholder-colours % tincture)
+                    %)
+                 data))
+
+(defn replace-non-placeholder-colours-everywhere [data colour]
+  (walk/postwalk #(if (and (vector? %)
+                           (-> % second string?)
+                           (->> % first (get #{:stroke :fill})))
+                    [(first %) (replace-non-placeholder-colour (second %) colour)]
+                    %)
+                 data))
+
+(defn remove-outlines [data]
   (walk/postwalk #(cond-> %
                     (vector? %) ((fn [v]
                                    (if (or (= v [:stroke "#000"])
                                            (= v [:stroke "#0000000"]))
                                      [:stroke "none"]
                                      v))))
-                 hiccup))
+                 data))
 
-(defn squiggly-paths [hiccup]
+(defn squiggly-paths [data]
   (walk/postwalk #(cond-> %
                     (vector? %) ((fn [v]
                                    (if (= (first v) :d)
                                      [:d (line/squiggly-path (second v))]
                                      v))))
-                 hiccup))
+                 data))
+
+(defn make-mask [data]
+  (let [mask-id (svg/id "mask")
+        adjusted-data (-> data
+                          (replace-non-placeholder-colours-everywhere "#fff")
+                          #_(replace-placeholder-colours-everywhere {:primary "#000"}))]
+    [mask-id adjusted-data]))
 
 (defn render [{:keys [type tincture hints ui] :as charge} environment options & {:keys [db-path]}]
   (if-let [charge-data-path (-> charge
@@ -101,24 +132,37 @@
                          (v/-)
                          (v// 2)
                          (v/+ (-> environment :points :fess)))
-            adjusted-data (-> data
-                              (replace-in-hiccup (into {} (map (fn [[key value]]
-                                                                 [key (tincture/pick value options)])
-                                                               tincture)))
-                              (cond->
-                               (not (or (:outline? hints)
-                                        (:outline? options))) remove-outlines
-                               (and (:squiggly? options)
-                                    (get #{:roundel
-                                           :fusil
-                                           :billet} type)) squiggly-paths))]
-        [:g {:transform (str "translate(" (:x position) "," (:y position) ") scale(" scale "," scale ")")
-             :on-click (fn [event]
-                         (rf/dispatch [:select-component db-path])
-                         (.stopPropagation event))
-             :style {:pointer-events "visiblePainted"
-                     :cursor "pointer"
-                     :filter (when (-> ui :selected?) "url(#glow)")}}
-         (assoc adjusted-data 0 :g)])
+            adjusted-charge (-> data
+                                fix-string-style-values
+                                (cond->
+                                 (not (or (:outline? hints)
+                                          (:outline? options))) remove-outlines
+                                 (and (:squiggly? options)
+                                      (get #{:roundel
+                                             :fusil
+                                             :billet} type)) squiggly-paths)
+                                (assoc 0 :g))
+            [mask-id mask] (make-mask adjusted-charge)
+            coloured-charge (replace-placeholder-colours-everywhere
+                             adjusted-charge
+                             (-> {}
+                                 (into (map (fn [[key value]]
+                                              [key (if (= key :primary)
+                                                     "none"
+                                                     (tincture/pick value options))])
+                                            tincture))))]
+        [:<>
+         [:mask {:id mask-id}
+          mask]
+         [:g {:transform (str "translate(" (:x position) "," (:y position) ") scale(" scale "," scale ")")
+              ;; :mask (str "url(#" mask-id ")")
+              :on-click (fn [event]
+                          (rf/dispatch [:select-component db-path])
+                          (.stopPropagation event))
+              :style {:pointer-events "visiblePainted"
+                      :cursor "pointer"
+                      :filter (when (-> ui :selected?) "url(#glow)")}}
+          ;; coloured-charge
+          mask]])
       [:<>])
     [:<>]))
