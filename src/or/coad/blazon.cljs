0baabb909f3bcb46938c095cffbcdfe2ce01c24e
Oliver Runge <oliver.runge@gmail.com>
2020-12-25 23:34:02 +0100

    Blazon counterchangedness as well

--- src/or/coad/blazon.cljs
+++ src/or/coad/blazon.cljs
@@ -27,34 +27,36 @@
                                                         (util/translate t)])))
                                       [:armed :langued :attired :unguled]))]))
 
-(defn encode-field [{:keys [division ordinaries charges] :as field} & {:keys [root?]}]
-  (let [tincture               (get-in  field [:content :tincture])
-        field-description      (cond
-                                 tincture (util/translate tincture)
-                                 division (let [{:keys [type line fields]} division
-                                                mandatory-part-count       (division/mandatory-part-count type)]
-                                            (combine
-                                             " "
-                                             [(util/translate type)
-                                              (translate-line line)
-                                              (combine " and "
-                                                       (map
-                                                        (fn [[index part]]
-                                                          (cond
-                                                            (< index
-                                                               mandatory-part-count) (encode-field part)
-                                                            (not (:ref part))        (combine " " [(division/part-name type index) (encode-field part)])))
-                                                        (sort-by #(division/part-position type (first %))
-                                                                 (map-indexed vector fields))))])))
-        ordinaries-description (combine ", " (map encode-ordinary ordinaries))
-        charges-description    (combine ", " (map encode-charge charges))
-        blazon                 (util/upper-case-first
-                                (combine ", " [field-description
-                                               ordinaries-description
-                                               charges-description]))]
-    (if (or root?
-            (and tincture
-                 (-> ordinaries-description count zero?)
-                 (-> charges-description count zero?)))
-      blazon
-      (str "[" blazon "]"))))
+(defn encode-field [{:keys [division ordinaries charges counterchanged?] :as field} & {:keys [root?]}]
+  (if counterchanged?
+    "counterchanged"
+    (let [tincture               (get-in  field [:content :tincture])
+          field-description      (cond
+                                   tincture (util/translate tincture)
+                                   division (let [{:keys [type line fields]} division
+                                                  mandatory-part-count       (division/mandatory-part-count type)]
+                                              (combine
+                                               " "
+                                               [(util/translate type)
+                                                (translate-line line)
+                                                (combine " and "
+                                                         (map
+                                                          (fn [[index part]]
+                                                            (cond
+                                                              (< index
+                                                                 mandatory-part-count) (encode-field part)
+                                                              (not (:ref part))        (combine " " [(division/part-name type index) (encode-field part)])))
+                                                          (sort-by #(division/part-position type (first %))
+                                                                   (map-indexed vector fields))))])))
+          ordinaries-description (combine ", " (map encode-ordinary ordinaries))
+          charges-description    (combine ", " (map encode-charge charges))
+          blazon                 (util/upper-case-first
+                                  (combine ", " [field-description
+                                                 ordinaries-description
+                                                 charges-description]))]
+      (if (or root?
+              (and tincture
+                   (-> ordinaries-description count zero?)
+                   (-> charges-description count zero?)))
+        blazon
+        (str "[" blazon "]")))))
