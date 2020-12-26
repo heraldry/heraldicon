ff3ae0c96de89f9b6822b83fdc19de2a14e9252c
Oliver Runge <oliver.runge@gmail.com>
2020-12-26 22:41:44 +0100

    Remove unused icon map entries

--- src/or/coad/form.cljs
+++ src/or/coad/form.cljs
@@ -179,13 +179,7 @@
               :open "fa-minus-square"}
    :charge {:closed "fa-plus-square"
             :open "fa-minus-square"}
-   :variant {:normal "fa-image"}
-   :form-field {:closed "fa-plus-square"
-                :open "fa-minus-square"}
-   :form-ordinary {:closed "fa-plus-square"
-                   :open "fa-minus-square"}
-   :form-charge {:closed "fa-plus-square"
-                 :open "fa-minus-square"}})
+   :variant {:normal "fa-image"}})
 
 (defn form-for-ordinary [path & {:keys [parent-field]}]
   (let [ordinary @(rf/subscribe [:get-in path])
