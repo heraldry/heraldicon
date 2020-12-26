d90c96ab0e29e7f88df65390d95b20c13e54963a
Oliver Runge <oliver.runge@gmail.com>
2020-12-26 12:14:10 +0100

    Generalize diagonal calculation further
    This way other divisions can use it as well.

--- src/or/coad/vector.cljs
+++ src/or/coad/vector.cljs
@@ -27,6 +27,10 @@
   {:x (cljs.core// x f)
    :y (cljs.core// y f)})
 
+(defn dot [{x1 :x y1 :y} {x2 :x y2 :y}]
+  {:x (cljs.core/* x1 x2)
+   :y (cljs.core/* y1 y2)})
+
 (defn abs [{x :x y :y}]
   (Math/sqrt (cljs.core/+
               (cljs.core/* x x)
@@ -45,6 +49,13 @@
    :y (-> to-y
           (cljs.core/- from-y)
           (cljs.core// (cljs.core/- to-x from-x))
+          (cljs.core/* (cljs.core/- x from-x))
+          (cljs.core/+ from-y))})
+
+(defn project-x [{from-x :x from-y :y} {dir-x :x dir-y :y} x]
+  {:x x
+   :y (-> dir-y
+          (cljs.core// dir-x)
           (cljs.core/* (cljs.core/- x from-x))
           (cljs.core/+ from-y))})
 
