e0a04c0656cbc278ca5e8fab54d035ec6a10c3e1
Oliver Runge <oliver.runge@gmail.com>
2020-12-29 22:07:14 +0100

    Add origin to adjust divisions and ordinaries

---
+++ src/or/coad/point.cljs
@@ -0,0 +1,32 @@
+(ns or.coad.point
+  (:require [or.coad.vector :as v]))
+
+(def options
+  [["Fess" :fess]
+   ["Chief" :chief]
+   ["Base" :base]
+   ["Dexter" :dexter]
+   ["Sinister" :sinister]
+   ["Honour" :honour]
+   ["Nombril" :nombril]])
+
+(defn calculate [{:keys [point offset-x offset-y] :or {offset-x 0
+                                                       offset-y 0}} environment default]
+  (let [ref (-> point
+                (or default))
+        p (-> environment :points (get ref))
+        width (:width environment)
+        height (:height environment)
+        dx (-> offset-x
+               (* width)
+               (/ 100))
+        dy (-> offset-y
+               (* height)
+               (/ 100)
+               -)]
+    (v/v (-> p
+             :x
+             (+ dx))
+         (-> p
+             :y
+             (+ dy)))))
