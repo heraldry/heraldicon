8a49b2bc95920d48485b0287eba5dfc128bb6fb5
Oliver Runge <oliver.runge@gmail.com>
2020-12-30 17:17:32 +0100

    Make squiggly paths deterministic
    Now the same path will always experience the same squiggling.

---
+++ src/or/coad/random.cljs
@@ -0,0 +1,10 @@
+(ns or.coad.random
+  (:require ["random" :as random-lib]
+            ["seedrandom" :as seedrandom]))
+
+(defn seed [seed]
+  (.use random-lib (seedrandom seed)))
+
+#_{:clj-kondo/ignore [:redefined-var]}
+(defn float []
+  (.float random-lib))
