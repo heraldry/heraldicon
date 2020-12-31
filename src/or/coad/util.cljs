0af6edab8e85b6e069c7c9eb010ebcb3f8ed66b9
Oliver Runge <oliver.runge@gmail.com>
2020-12-31 17:55:07 +0100

    Move single "id" function into util module
    There were collisions when various things were rendered at the same time.

--- src/or/coad/util.cljs
+++ src/or/coad/util.cljs
@@ -1,6 +1,12 @@
 (ns or.coad.util
   (:require [clojure.string :as s]))
 
+(def -current-id
+  (atom 0))
+
+(defn id [prefix]
+  (str prefix "_" (swap! -current-id inc)))
+
 (defn upper-case-first [s]
   (str (s/upper-case (or (first s) "")) (s/join (rest s))))
 
