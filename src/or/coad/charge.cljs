b6b9f6dc8b90bd87e3201aa2c0f06ccb7833aa4e
Oliver Runge <oliver.runge@gmail.com>
2021-01-01 01:03:24 +0100

    Allow stretch factor of up to 3 in both directions

--- src/or/coad/charge.cljs
+++ src/or/coad/charge.cljs
@@ -157,8 +157,8 @@
                :max     100
                :default 50}
    :stretch   {:type    :range
-               :min     0.5
-               :max     2
+               :min     0.33
+               :max     3
                :default 1}
    :mirrored? {:type    :boolean
                :default false}
