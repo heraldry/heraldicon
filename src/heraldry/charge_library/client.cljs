c420fc45baaff19a8f0bc982cc7e3dfd2fb682e3
Oliver Runge <oliver.runge@gmail.com>
2021-01-06 20:12:49 +0100

    Add account creation and login
    This implements sign-up, confirmation, and login via AWS Cognito.

--- src/heraldry/charge_library/client.cljs
+++ src/heraldry/charge_library/client.cljs
@@ -6,6 +6,7 @@
             [clojure.walk :as walk]
             [com.wsscode.common.async-cljs :refer [<? go-catch]]
             [heraldry.charge-library.api.request :as api-request]
+            [heraldry.user.auth :as auth]
             [hickory.core :as hickory]
             [re-frame.core :as rf]
             [reagent.dom :as r]))
@@ -14,17 +15,25 @@
 
 (rf/reg-sub
  :get
- (fn [db [_ & args]]
-   (get-in db args)))
-
-(rf/reg-sub
- :get-in
  (fn [db [_ path]]
    (get-in db path)))
 
+(rf/reg-sub
+ :get-form-data
+ (fn [db [_ form-id]]
+   (get-in db [:form-data form-id])))
+
+(rf/reg-sub
+ :get-form-error-message
+ (fn [db [_ form-id]]
+   (get-in db [:form-error-message form-id])))
+
+(rf/reg-sub
+ :get-form-error
+ (fn [db [_ form-id key]]
+   (get-in db [:form-errors form-id key])))
 
 ;; events
-
 
 (rf/reg-event-db
  :initialize-db
@@ -33,15 +42,46 @@
 
 (rf/reg-event-db
  :set
- (fn [db [_ & args]]
-   (assoc-in db (drop-last args) (last args))))
-
-(rf/reg-event-db
- :set-in
  (fn [db [_ path value]]
    (assoc-in db path value)))
 
+(rf/reg-event-db
+ :remove
+ (fn [db [_ path]]
+   (cond-> db
+     (-> path count (= 1)) (dissoc (first path))
+     (-> path count (> 1)) (update-in (drop-last path) dissoc (last path)))))
+
+(rf/reg-event-db
+ :set-form-data-key
+ (fn [db [_ form-id key value]]
+   (assoc-in db [:form-data form-id key] value)))
+
+(rf/reg-event-db
+ :set-form-error-message
+ (fn [db [_ form-id message]]
+   (assoc-in db [:form-error-message form-id] message)))
+
+(rf/reg-event-db
+ :set-form-error-key
+ (fn [db [_ form-id key error]]
+   (assoc-in db [:form-errors form-id key] error)))
+
+(rf/reg-event-db
+ :clear-form-errors
+ (fn [db [_ form-id]]
+   (update-in db [:form-errors] dissoc form-id)))
+
+(rf/reg-event-db
+ :clear-form
+ (fn [db [_ form-id]]
+   (-> db
+       (update :form-data dissoc form-id)
+       (update :form-error-message dissoc form-id)
+       (update :form-errors dissoc form-id))))
+
 ;; functions
+
 
 #_(defn strip-svg [data]
     (walk/postwalk (fn [x]
@@ -64,7 +104,7 @@
 ;; views
 
 
-(defn load-svg-file [db-path data]
+(defn load-svg-file [form-id key data]
   (go
     (try
       (-> data
@@ -81,60 +121,57 @@
                                  (get-in [1 :width]))
                     height   (-> parsed
                                  (get-in [1 :height]))]
-                (rf/dispatch [:set-in db-path {:width  width
-                                               :height height
-                                               :data   svg-data}]))))
+                (rf/dispatch [:set-form-data-key form-id key {:width  width
+                                                              :height height
+                                                              :data   svg-data}]))))
       (catch :default e
         (println "error:" e)))))
 
-(defn preview [db-path]
+(defn preview [charge-data]
   [:div.preview
-   (let [{:keys [width height data]} @(rf/subscribe [:get-in db-path])]
+   (let [{:keys [width height data]} charge-data]
      (when data
        [:svg {:viewBox             (str "0 0 " width " " height)
               :preserveAspectRatio "xMidYMid meet"}
         data]))])
 
-(defn upload-file [event db-path]
+(defn upload-file [event form-id key]
   (let [file (-> event .-target .-files (.item 0))]
     (when file
       (let [reader (js/FileReader.)]
         (set! (.-onloadend reader) (fn []
                                      (let [raw-data (.-result reader)]
-                                       (load-svg-file db-path raw-data))))
+                                       (load-svg-file form-id key raw-data))))
         (.readAsText reader file)))))
 
-(defn form-field [db-path key function]
-  (let [field-path (conj db-path key)
-        value      @(rf/subscribe [:get-in field-path])
-        error      @(rf/subscribe [:get-in (-> [:form-error-data]
-                                               (into db-path)
-                                               (conj :errors key))])]
+(defn form-field [form-id key function]
+  (let [{value key} @(rf/subscribe [:get-form-data form-id])
+        error       @(rf/subscribe [:get-form-error form-id key])]
     (-> [:div.field {:class (when error "error")}]
         (cond->
             error (conj [:p.error-message error]))
         (conj (function :value value
                         :on-change #(let [new-value (-> % .-target .-value)]
-                                      (rf/dispatch [:set-in field-path new-value])))))))
+                                      (rf/dispatch [:set-form-data-key form-id key new-value])))))))
 
-(defn save-charge-form [db-path]
-  (let [payload @(rf/subscribe [:get-in db-path])]
+(defn save-charge-form [form-id]
+  (let [payload @(rf/subscribe [:get-form-data form-id])]
     (go
       (try
         (let [response (<! (api-request/call :save-charge payload))])
         (catch :default e
           (println "save-form error:" e))))))
 
-(defn charge-form [db-path]
-  (let [error-data    @(rf/subscribe [:get-in (into [:form-error-data] db-path)])
-        error-message (:message error-data)]
+(defn charge-form [form-id]
+  (let [error-message @(rf/subscribe [:get-form-error-message form-id])
+        charge-data   @(rf/subscribe [:get-form-data form-id])]
     [:div
-     [preview (conj db-path :charge/data)]
+     [preview (:charge/data charge-data)]
      [:div.form
       (when error-message
         [:div.error-top
          [:p.error-message error-message]])
-      [form-field db-path :charge/key
+      [form-field form-id :charge/key
        (fn [& {:keys [value on-change]}]
          [:<>
           [:label {:for "key"} "Charge Key"]
@@ -142,7 +179,7 @@
                    :value     value
                    :on-change on-change
                    :type      "text"}]])]
-      [form-field db-path :charge/name
+      [form-field form-id :charge/name
        (fn [& {:keys [value on-change]}]
          [:<>
           [:label {:for "name"} "Name"]
@@ -150,7 +187,7 @@
                    :value     value
                    :on-change on-change
                    :type      "text"}]])]
-      [form-field db-path :charge/attitude
+      [form-field form-id :charge/attitude
        (fn [& {:keys [value on-change]}]
          [:<>
           [:label {:for "attitude"} "Attitude"]
@@ -158,7 +195,7 @@
                    :value     value
                    :on-change on-change
                    :type      "text"}]])]
-      [form-field db-path :charge/data
+      [form-field form-id :charge/data
        (fn [& _]
          [:<>
           [:label {:for   "upload"
@@ -166,17 +203,187 @@
           [:input {:type      "file"
                    :accept    "image/svg+xml"
                    :id        "upload"
-                   :on-change #(upload-file % (conj db-path :charge/data))}]])]
+                   :on-change #(upload-file % form-id :charge/data)}]])]
       [:div.buttons
-       [:button.save {:on-click #(save-charge-form db-path)} "Save"]]]]))
+       [:button.save {:on-click #(save-charge-form form-id)} "Save"]]]]))
+
+(defn login-clicked [form-id]
+  (let [{:keys [username password]} @(rf/subscribe [:get-form-data form-id])]
+    (rf/dispatch [:clear-form-errors])
+    (auth/login username password
+                :on-success (fn [result]
+                              (println "login success" result)
+                              (rf/dispatch [:clear-form form-id])
+                              (rf/dispatch [:set [:user-data] {:jwt-token  (-> result
+                                                                               .getAccessToken
+                                                                               .getJwtToken)
+                                                               :logged-in? true}]))
+                :on-failure (fn [error]
+                              (println "login failure" error)
+                              (rf/dispatch [:set-form-error-message form-id (:message error)])))))
+
+(defn login-form []
+  (let [form-id       :login-form
+        error-message @(rf/subscribe [:get-form-error-message form-id])]
+    [:div.form.login
+     [:h4 {:style {:margin        0
+                   :margin-bottom "0.5em"}} "Login"]
+     (when error-message
+       [:div.error-top
+        [:p.error-message error-message]])
+     [form-field form-id :username
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "username"} "Username"]
+         [:input {:id        "username"
+                  :value     value
+                  :on-change on-change
+                  :type      "text"}]])]
+     [form-field form-id :password
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "password"} "Password:"]
+         [:input {:id        "password"
+                  :value     value
+                  :on-change on-change
+                  :type      "password"}]])]
+     [:div.buttons
+      [:button.login {:on-click #(login-clicked form-id)} "Login"]]
+     [:span {:style {:text-align "center"}}
+      [:a {:on-click #(rf/dispatch [:set [:sign-up?] true])} "Sign up"]]]))
+
+(defn sign-up-clicked [form-id]
+  (let [{:keys [username email password password-again]} @(rf/subscribe [:get-form-data form-id])]
+    (rf/dispatch [:clear-form-errors])
+    (if (not= password password-again)
+      (rf/dispatch [:set-form-error-key form-id :password-again "Passwords don't match."])
+      (auth/sign-up username password email
+                    :on-success (fn [result]
+                                  ;; data:
+                                  ;; {:user #object[CognitoUser [object Object]]
+                                  ;;  :userConfirmed false
+                                  ;;  :userSub <uuid>
+                                  ;;  :codeDeliveryDetails {:AttributeName email,
+                                  ;;                        :DeliveryMedium EMAIL,
+                                  ;;                        :Destination <masked-email>}}
+                                  (println "sign-up success" result)
+                                  (rf/dispatch [:clear-form form-id])
+                                  (rf/dispatch [:set [:user-data] {:user                 (:user result)
+                                                                   :confirmation-needed? true
+                                                                   :confirmation-email   (-> result
+                                                                                             :codeDeliveryDetails
+                                                                                             :Destination)}]))
+                    :on-failure (fn [error]
+                                  (println "sign-up failure" error)
+                                  (rf/dispatch [:set-form-error-message form-id (:message error)]))))))
+
+(defn sign-up-form []
+  (let [form-id       :sign-up-form
+        error-message @(rf/subscribe [:get-form-error-message form-id])]
+    [:div.form.login
+     [:h4 {:style {:margin        0
+                   :margin-bottom "0.5em"}} "Sign-up"]
+     (when error-message
+       [:div.error-top
+        [:p.error-message error-message]])
+     [form-field form-id :username
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "username"} "Username"]
+         [:input {:id        "username"
+                  :value     value
+                  :on-change on-change
+                  :type      "text"}]])]
+     [form-field form-id :email
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "email"} "Email"]
+         [:input {:id        "email"
+                  :value     value
+                  :on-change on-change
+                  :type      "text"}]])]
+     [form-field form-id :password
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "password"} "Password:"]
+         [:input {:id        "password"
+                  :value     value
+                  :on-change on-change
+                  :type      "password"}]])]
+     [form-field form-id :password-again
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "password-again"} "Password again:"]
+         [:input {:id        "password-again"
+                  :value     value
+                  :on-change on-change
+                  :type      "password"}]])]
+     [:div.buttons
+      [:button.sign-up {:on-click #(sign-up-clicked form-id)} "Sign-up"]]
+     [:span {:style {:text-align "center"}}
+      [:a {:on-click #(rf/dispatch [:set [:sign-up?] false])} "Back to login-clicked"]]]))
+
+(defn confirm-clicked [form-id]
+  (let [{:keys [code]} @(rf/subscribe [:get-form-data form-id])
+        user-data      @(rf/subscribe [:get [:user-data]])
+        user           (:user user-data)]
+    (rf/dispatch [:clear-form-errors form-id])
+    (auth/confirm user code
+                  :on-success (fn [result]
+                                (println "confirm success" result)
+                                (rf/dispatch [:clear-form form-id])
+                                (rf/dispatch [:set [:sign-up?] false])
+                                (rf/dispatch [:set [:user-data] {:user       user
+                                                                 :logged-in? true}]))
+                  :on-failure (fn [error]
+                                (println "confirm error" error)
+                                (rf/dispatch [:set-form-error-message form-id (:message error)])))))
+
+(defn confirmation-form []
+  (let [form-id       :confirmation-form
+        user-data     @(rf/subscribe [:get [:user-data]])
+        error-message @(rf/subscribe [:get-form-error-message form-id])]
+    [:div.form.login
+     [:h4 {:style {:margin        0
+                   :margin-bottom "0.5em"}} "Confirmation"]
+     "A confirmation code was sent to your email address: " (:confirmation-email user-data)
+     (when error-message
+       [:div.error-top
+        [:p.error-message error-message]])
+     [form-field form-id :code
+      (fn [& {:keys [value on-change]}]
+        [:<>
+         [:label {:for "code"} "Confirmation code"]
+         [:input {:id        "code"
+                  :value     value
+                  :on-change on-change
+                  :type      "text"}]])]
+
+     [:div.buttons
+      [:button.confirm {:on-click #(confirm-clicked form-id)} "Confirm"]]]))
+
+(defn not-logged-in []
+  (let [confirmation-needed? @(rf/subscribe [:get [:user-data :confirmation-needed?]])
+        sign-up?             @(rf/subscribe [:get [:sign-up?]])]
+    (cond
+      confirmation-needed? [confirmation-form]
+      sign-up?             [sign-up-form]
+      :else                [login-form])))
+
+(defn app []
+  (let [user-data @(rf/subscribe [:get [:user-data]])]
+    (if (:logged-in? user-data)
+      [charge-form :charge-form]
+      [not-logged-in])))
 
 (defn stop []
   (println "Stopping..."))
 
 (defn start []
   (rf/dispatch-sync [:initialize-db])
-  (r/render [charge-form [:charge-form]]
-            (.getElementById js/document "app")))
+  (r/render
+   [app]
+   (.getElementById js/document "app")))
 
 (defn ^:export init []
   (start))
---
+++ src/heraldry/user/auth.cljs
@@ -0,0 +1,52 @@
+(ns heraldry.user.auth
+  (:require ["amazon-cognito-identity-js" :refer [CognitoUserPool
+                                                  CognitoUser
+                                                  AuthenticationDetails]]))
+
+(def user-pool
+  (new CognitoUserPool (clj->js {:UserPoolId "eu-central-1_jUhhO2lNn"
+                                 :ClientId   "7ho46t4kkf3toohlhtjksk2iq1"})))
+
+(defn login [username password & {:keys [on-success on-failure on-new-password-required]}]
+  (let [user         (new CognitoUser (clj->js {:Username username
+                                                :Pool     user-pool}))
+        auth-details (new AuthenticationDetails (clj->js {:Username username
+                                                          :Password password}))]
+    (.authenticateUser
+     user
+     auth-details
+     (clj->js {:onSuccess           (fn [result]
+                                      (on-success (js->clj result :keywordize-keys true)))
+               :onFailure           (fn [error]
+                                      (on-failure (js->clj error :keywordize-keys true)))
+               :newPasswordRequired (fn [user-attributes _required-attributes]
+                                      (let [user-attributes (dissoc (js->clj user-attributes :keywordize-keys true) :email_verified)]
+                                        (on-new-password-required user user-attributes)))}))))
+(defn sign-up [username password email & {:keys [on-success on-failure]}]
+  (let [attributes [{:Name  "email"
+                     :Value email}]]
+    (.signUp
+     user-pool
+     username password (clj->js attributes) nil
+     (fn [err result]
+       (if err
+         (on-failure (js->clj err :keywordize-keys true))
+         (on-success (js->clj result :keywordize-keys true)))))))
+
+(defn confirm [user code & {:keys [on-success on-failure]}]
+  (.confirmRegistration
+   user
+   code true
+   (fn [err result]
+     (if err
+       (on-failure (js->clj err :keywordize-keys true))
+       (on-success (js->clj result :keywordize-keys true))))))
+
+(defn complete-new-password-challenge [user password user-attributes & {:keys [on-success on-failure]}]
+  (.completeNewPasswordChallenge
+   user
+   password (clj->js user-attributes)
+   (clj->js {:onSuccess (fn [result]
+                          (on-success (js->clj result :keywordize-keys true)))
+             :onFailure (fn [error]
+                          (on-failure (js->clj error :keywordize-keys true)))})))
