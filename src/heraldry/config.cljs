5f7cebf43cd0390db31e148fac27945ad403b197
Oliver Runge <oliver.runge@gmail.com>
2021-01-06 18:24:50 +0100

    Prepare storing charge data in the charge-library
    Gonna need a user concept before storing it, methinks.

--- src/heraldry/config.cljs
+++ src/heraldry/config.cljs
@@ -1,6 +1,6 @@
-(ns heraldry.charge-library.config
+(ns heraldry.config
   (:refer-clojure :exclude [get])
-  (:require-macros [heraldry.charge-library.config]))
+  (:require-macros [heraldry.config]))
 
 (defn -js->clj+
   "For cases when built-in js->clj doesn't work. Source: https://stackoverflow.com/a/32583549/4839573"
@@ -15,9 +15,9 @@
 (defn get [setting]
   (case setting
     :region (:REGION env)
-    :stage (or (:STAGE env) (heraldry.charge-library.config/get-static :stage))
+    :stage (or (:STAGE env) (heraldry.config/get-static :stage))
     :table-charges (:TABLE_CHARGES env)
     :table-users (:TABLE_USERS env)
-    :api-endpoint (heraldry.charge-library.config/get-static :api-endpoint)
+    :charge-library-api-endpoint (heraldry.config/get-static :charge-library-api-endpoint)
     :dynamodb-endpoint (:DYNAMODB_ENDPOINT env)
     nil))
