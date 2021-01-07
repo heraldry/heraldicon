de76f5d183398ee795c5d3a44a5aec811fd8ddc9
Oliver Runge <oliver.runge@gmail.com>
2021-01-07 22:46:02 +0100

    Move user/auth related forms into own module
    Also add a logout button. This is terribly placed, just need a button
    for now. It will all become pretty once the main functionality is added.

--- src/heraldry/charge_library/client.cljs
+++ src/heraldry/charge_library/client.cljs
@@ -6,15 +6,10 @@
             [clojure.walk :as walk]
             [com.wsscode.common.async-cljs :refer [<? go-catch]]
             [heraldry.charge-library.api.request :as api-request]
-            [heraldry.user.auth :as auth]
+            [heraldry.charge-library.user :as user :refer [form-field]]
             [hickory.core :as hickory]
-            [hodgepodge.core :refer [local-storage get-item
-                                     set-item]]
             [re-frame.core :as rf]
             [reagent.dom :as r]))
-
-(def local-storage-token-name
-  "cl-jwt-token")
 
 ;; subs
 
@@ -149,16 +144,6 @@
                                        (load-svg-file form-id key raw-data))))
         (.readAsText reader file)))))
 
-(defn form-field [form-id key function]
-  (let [{value key} @(rf/subscribe [:get-form-data form-id])
-        error       @(rf/subscribe [:get-form-error form-id key])]
-    (-> [:div.field {:class (when error "error")}]
-        (cond->
-            error (conj [:p.error-message error]))
-        (conj (function :value value
-                        :on-change #(let [new-value (-> % .-target .-value)]
-                                      (rf/dispatch [:set-form-data-key form-id key new-value])))))))
-
 (defn save-charge-form [form-id]
   (let [payload @(rf/subscribe [:get-form-data form-id])]
     (go
@@ -210,172 +195,17 @@
                    :id        "upload"
                    :on-change #(upload-file % form-id :charge/data)}]])]
       [:div.buttons
-       [:button.save {:on-click #(save-charge-form form-id)} "Save"]]]]))
-
-(defn login-clicked [form-id]
-  (let [{:keys [username password]} @(rf/subscribe [:get-form-data form-id])]
-    (rf/dispatch [:clear-form-errors])
-    (auth/login username password
-                :on-success (fn [result]
-                              (println "login success" result)
-                              (rf/dispatch [:clear-form form-id])
-                              (let [jwt-token (-> result
-                                                  .getAccessToken
-                                                  .getJwtToken)]
-                                (set-item local-storage local-storage-token-name jwt-token)
-                                (rf/dispatch [:set [:user-data] {:jwt-token  jwt-token
-                                                                 :logged-in? true}])))
-                :on-failure (fn [error]
-                              (println "login failure" error)
-                              (rf/dispatch [:set-form-error-message form-id (:message error)])))))
-
-(defn login-form []
-  (let [form-id       :login-form
-        error-message @(rf/subscribe [:get-form-error-message form-id])]
-    [:div.form.login
-     [:h4 {:style {:margin        0
-                   :margin-bottom "0.5em"}} "Login"]
-     (when error-message
-       [:div.error-top
-        [:p.error-message error-message]])
-     [form-field form-id :username
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "username"} "Username"]
-         [:input {:id        "username"
-                  :value     value
-                  :on-change on-change
-                  :type      "text"}]])]
-     [form-field form-id :password
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "password"} "Password:"]
-         [:input {:id        "password"
-                  :value     value
-                  :on-change on-change
-                  :type      "password"}]])]
-     [:div.buttons
-      [:button.login {:on-click #(login-clicked form-id)} "Login"]]
-     [:span {:style {:text-align "center"}}
-      [:a {:on-click #(rf/dispatch [:set [:sign-up?] true])} "Sign up"]]]))
-
-(defn sign-up-clicked [form-id]
-  (let [{:keys [username email password password-again]} @(rf/subscribe [:get-form-data form-id])]
-    (rf/dispatch [:clear-form-errors])
-    (if (not= password password-again)
-      (rf/dispatch [:set-form-error-key form-id :password-again "Passwords don't match."])
-      (auth/sign-up username password email
-                    :on-success (fn [result]
-                                  ;; data:
-                                  ;; {:user #object[CognitoUser [object Object]]
-                                  ;;  :userConfirmed false
-                                  ;;  :userSub <uuid>
-                                  ;;  :codeDeliveryDetails {:AttributeName email,
-                                  ;;                        :DeliveryMedium EMAIL,
-                                  ;;                        :Destination <masked-email>}}
-                                  (println "sign-up success" result)
-                                  (rf/dispatch [:clear-form form-id])
-                                  (rf/dispatch [:set [:user-data] {:user                 (:user result)
-                                                                   :confirmation-needed? true
-                                                                   :confirmation-email   (-> result
-                                                                                             :codeDeliveryDetails
-                                                                                             :Destination)}]))
-                    :on-failure (fn [error]
-                                  (println "sign-up failure" error)
-                                  (rf/dispatch [:set-form-error-message form-id (:message error)]))))))
-
-(defn sign-up-form []
-  (let [form-id       :sign-up-form
-        error-message @(rf/subscribe [:get-form-error-message form-id])]
-    [:div.form.login
-     [:h4 {:style {:margin        0
-                   :margin-bottom "0.5em"}} "Sign-up"]
-     (when error-message
-       [:div.error-top
-        [:p.error-message error-message]])
-     [form-field form-id :username
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "username"} "Username"]
-         [:input {:id        "username"
-                  :value     value
-                  :on-change on-change
-                  :type      "text"}]])]
-     [form-field form-id :email
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "email"} "Email"]
-         [:input {:id        "email"
-                  :value     value
-                  :on-change on-change
-                  :type      "text"}]])]
-     [form-field form-id :password
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "password"} "Password:"]
-         [:input {:id        "password"
-                  :value     value
-                  :on-change on-change
-                  :type      "password"}]])]
-     [form-field form-id :password-again
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "password-again"} "Password again:"]
-         [:input {:id        "password-again"
-                  :value     value
-                  :on-change on-change
-                  :type      "password"}]])]
-     [:div.buttons
-      [:button.sign-up {:on-click #(sign-up-clicked form-id)} "Sign-up"]]
-     [:span {:style {:text-align "center"}}
-      [:a {:on-click #(rf/dispatch [:set [:sign-up?] false])} "Back to login-clicked"]]]))
-
-(defn confirm-clicked [form-id]
-  (let [{:keys [code]} @(rf/subscribe [:get-form-data form-id])
-        user-data      @(rf/subscribe [:get [:user-data]])
-        user           (:user user-data)]
-    (rf/dispatch [:clear-form-errors form-id])
-    (auth/confirm user code
-                  :on-success (fn [result]
-                                (println "confirm success" result)
-                                (rf/dispatch [:clear-form form-id])
-                                (rf/dispatch [:set [:sign-up?] false])
-                                (rf/dispatch [:set [:user-data] {:user       user
-                                                                 :logged-in? true}]))
-                  :on-failure (fn [error]
-                                (println "confirm error" error)
-                                (rf/dispatch [:set-form-error-message form-id (:message error)])))))
-
-(defn confirmation-form []
-  (let [form-id       :confirmation-form
-        user-data     @(rf/subscribe [:get [:user-data]])
-        error-message @(rf/subscribe [:get-form-error-message form-id])]
-    [:div.form.login
-     [:h4 {:style {:margin        0
-                   :margin-bottom "0.5em"}} "Confirmation"]
-     "A confirmation code was sent to your email address: " (:confirmation-email user-data)
-     (when error-message
-       [:div.error-top
-        [:p.error-message error-message]])
-     [form-field form-id :code
-      (fn [& {:keys [value on-change]}]
-        [:<>
-         [:label {:for "code"} "Confirmation code"]
-         [:input {:id        "code"
-                  :value     value
-                  :on-change on-change
-                  :type      "text"}]])]
-
-     [:div.buttons
-      [:button.confirm {:on-click #(confirm-clicked form-id)} "Confirm"]]]))
+       [:button.save {:on-click #(save-charge-form form-id)} "Save"]]
+      [:div.buttons
+       [:button.logout {:on-click #(user/logout)} "Logout"]]]]))
 
 (defn not-logged-in []
   (let [confirmation-needed? @(rf/subscribe [:get [:user-data :confirmation-needed?]])
         sign-up?             @(rf/subscribe [:get [:sign-up?]])]
     (cond
-      confirmation-needed? [confirmation-form]
-      sign-up?             [sign-up-form]
-      :else                [login-form])))
+      confirmation-needed? [user/confirmation-form]
+      sign-up?             [user/sign-up-form]
+      :else                [user/login-form])))
 
 (defn app []
   (let [user-data @(rf/subscribe [:get [:user-data]])]
@@ -388,7 +218,7 @@
 
 (defn start []
   (rf/dispatch-sync [:initialize-db])
-  (when-let [jwt-token (get-item local-storage local-storage-token-name)]
+  (when-let [jwt-token (user/load-session)]
     (rf/dispatch-sync [:set [:user-data] {:jwt-token  jwt-token
                                           :logged-in? true}]))
   (r/render
