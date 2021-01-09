3fecacd6e4e80fb08dcad0da224a7f08e6601533
Oliver Runge <oliver.runge@gmail.com>
2021-01-09 19:09:01 +0100

    Move credentials namespace into AWS namespace

--- src/heraldry/credentials.cljs
+++
@@ -1,7 +0,0 @@
-(ns heraldry.credentials
-  (:require ["@aws-sdk/credential-provider-ini" :refer [fromIni]]
-            [heraldry.config :as config]))
-
-(defn load []
-  (when (= (config/get :stage)
-           "local") (fromIni #js {:profile "heraldry-local-user"})))
