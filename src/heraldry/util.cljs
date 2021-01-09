ec9607aa487e82e72c86b31fb0a35363572d575a
Oliver Runge <oliver.runge@gmail.com>
2021-01-09 12:19:54 +0100

    Move to AWS-SDK v3 for DynamoDB as well
    This required a lot of changes in the DDB layer, but now at least it
    uses the latest SDK, the internal API didn't change noticeably.

--- src/heraldry/util.cljs
+++ src/heraldry/util.cljs
@@ -1,4 +1,5 @@
-(ns heraldry.util)
+(ns heraldry.util
+  (:require [clojure.pprint :refer [pprint]]))
 
 (defn promise
   [resolver]
@@ -11,3 +12,21 @@
                   (if (nil? error)
                     (resolve data)
                     (reject error)))))))
+
+(defn spy [value msg]
+  (println msg)
+  (pprint value)
+  value)
+
+(defn map-keys
+  "Applies f to each key of m. Also to keys of m's vals and so on."
+  [f m]
+  (zipmap
+   (map (fn [k]
+          (f k))
+        (keys m))
+   (map (fn [v]
+          (if (map? v)
+            (map-keys f v)
+            v))
+        (vals m))))
