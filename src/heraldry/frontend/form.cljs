a9f749bdc75ce119f2d5fb2e04dcaf7d26980962
Oliver Runge <oliver.runge@gmail.com>
2021-01-12 22:09:53 +0100

    Add attitude and facing settings, also first attributes
    These can be filtered based on the type of charge later on, so only the
    relevant ones are shown.

--- src/heraldry/frontend/form.cljs
+++ src/heraldry/frontend/form.cljs
@@ -1,5 +1,6 @@
 (ns heraldry.frontend.form
-  (:require [re-frame.core :as rf]))
+  (:require [heraldry.coat-of-arms.util :as util]
+            [re-frame.core :as rf]))
 
 (defn field [db-path function]
   (let [value @(rf/subscribe [:get db-path])
@@ -10,3 +11,80 @@
      (function :value value
                :on-change #(let [new-value (-> % .-target .-value)]
                              (rf/dispatch [:set db-path new-value])))]))
+
+(defn checkbox [path label]
+  (let [component-id (util/id "checkbox")
+        checked?     (-> (and path
+                              @(rf/subscribe [:get path]))
+                         boolean)]
+    [:label {:for   component-id
+             :style {:text-align "left"
+                     :width      "6em"}}
+     [:input {:type      "checkbox"
+              :id        component-id
+              :checked   checked?
+              :on-change #(let [new-checked? (-> % .-target .-checked)]
+                            (rf/dispatch [:set path new-checked?]))}]
+     (str " " label)]))
+
+(defn select [path label choices & {:keys [grouped? value on-change default]}]
+  (let [component-id  (util/id "select")
+        current-value @(rf/subscribe [:get path])]
+    [:div.pure-control-group
+     [:label {:for component-id} label]
+     [:select {:id        component-id
+               :value     (name (or value
+                                    current-value
+                                    default
+                                    :none))
+               :on-change #(let [checked (keyword (-> % .-target .-value))]
+                             (if on-change
+                               (on-change checked)
+                               (rf/dispatch [:set path checked])))}
+      (if grouped?
+        (for [[group-name & group-choices] choices]
+          (if (and (-> group-choices count (= 1))
+                   (-> group-choices first keyword?))
+            (let [key (-> group-choices first)]
+              ^{:key key}
+              [:option {:value (name key)} group-name])
+            ^{:key group-name}
+            [:optgroup {:label group-name}
+             (for [[display-name key] group-choices]
+               ^{:key key}
+               [:option {:value (name key)} display-name])]))
+        (for [[display-name key] choices]
+          ^{:key key}
+          [:option {:value (name key)} display-name]))]]))
+
+(defn multi-select [path label choices & {:keys [grouped? value on-change default]}]
+  (let [component-id  (util/id "select")
+        current-value @(rf/subscribe [:get path])]
+    [:div.pure-control-group
+     [:label {:for component-id} label]
+     [:select {:id        component-id
+               :value     (-> value
+                              (or current-value default [])
+                              (->> (map name))
+                              vec)
+               :multiple  true
+               :on-change #(let [checked (keyword (-> % .-target .-value))]
+                             (println "checked" (-> % .-target .-value))
+                             (if on-change
+                               (on-change checked)
+                               (rf/dispatch [:set path ["coward"]])))}
+      (if grouped?
+        (for [[group-name & group-choices] choices]
+          (if (and (-> group-choices count (= 1))
+                   (-> group-choices first keyword?))
+            (let [key (-> group-choices first)]
+              ^{:key key}
+              [:option {:value (name key)} group-name])
+            ^{:key group-name}
+            [:optgroup {:label group-name}
+             (for [[display-name key] group-choices]
+               ^{:key key}
+               [:option {:value (name key)} display-name])]))
+        (for [[display-name key] choices]
+          ^{:key key}
+          [:option {:value (name key)} display-name]))]]))
