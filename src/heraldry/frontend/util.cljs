2462fdaabe6b4c961147c1f0660982e191bbf4b6
Oliver Runge <oliver.runge@gmail.com>
2021-01-15 15:01:20 +0100

    Split up heraldry.api.util again
    Some of those functions needed re-frame/reagent, which don't exist in
    the API project.

--- src/heraldry/frontend/util.cljs
+++ src/heraldry/frontend/util.cljs
@@ -1,51 +1,7 @@
-(ns heraldry.util
-  (:require ["crypto" :as crypto]
-            [cljs-time.core :as time]
-            [cljs-time.format :as format]
-            [clojure.string :as s]
+(ns heraldry.frontend.util
+  (:require [clojure.string :as s]
             [clojure.walk :as walk]
-            [clojure.pprint :refer [pprint]]
             [re-frame.core :as rf]))
-
-(defn promise
-  [resolver]
-  (js/Promise. resolver))
-
-(defn promise-from-callback
-  [f]
-  (promise (fn [resolve reject]
-             (f (fn [error data]
-                  (if (nil? error)
-                    (resolve data)
-                    (reject error)))))))
-
-(defn spy [value msg]
-  (println msg)
-  (pprint value)
-  value)
-
-(defn map-keys
-  "Applies f to each key of m. Also to keys of m's vals and so on."
-  [f m]
-  (zipmap
-   (map (fn [k]
-          (f k))
-        (keys m))
-   (map (fn [v]
-          (if (map? v)
-            (map-keys f v)
-            v))
-        (vals m))))
-
-(defn iso-now []
-  (->> (time/time-now)
-       (format/unparse (:date-time format/formatters))))
-
-(defn sha1 [data]
-  (-> crypto
-      (.createHash "sha1")
-      (.update data)
-      (.digest "hex")))
 
 (defn dispatch [event effect]
   (rf/dispatch effect)
