0f35cd371b1986e8cd3c8b77b03123685ca6ed20
Oliver Runge <oliver.runge@gmail.com>
2021-01-17 10:50:10 +0100

    Move heraldry.api.request and heraldry.config into frontend code

---
+++ src/heraldry/api/request.cljs
@@ -0,0 +1,19 @@
+(ns heraldry.api.request
+  (:require [cljs-http.client :as http]
+            [com.wsscode.common.async-cljs :refer [<? go-catch]]
+            [heraldry.frontend.config :as config]))
+
+(defn call [name payload user-data]
+  (println "calling API" name)
+  (go-catch
+   (let [response (<? (http/post (config/get :armory-api-endpoint)
+                                 {:headers {"Session-Id" (:session-id user-data)}
+                                  :edn-params {:call name
+                                               :data payload}}))
+         status (:status response)
+         body (:body response)]
+     (if (= status 200)
+       (:success body)
+       (if (:error body)
+         body
+         {:error (str "HTTP: " status " " (:body response))})))))
