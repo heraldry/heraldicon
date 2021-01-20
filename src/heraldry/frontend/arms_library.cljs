20f39b7236035440a817306717e1504ba76c2019
Oliver Runge <oliver.runge@gmail.com>
2021-01-20 22:11:18 +0100

    Fix component selection for charge-library
    Also improve deselection of all selected components and closing of
    submenus when clicking anywhere else.

--- src/heraldry/frontend/arms_library.cljs
+++ src/heraldry/frontend/arms_library.cljs
@@ -80,11 +80,8 @@
                                      {:render-options render-options
                                       :db-path        [:arms-form :coat-of-arms]}))
             {:keys [width height]} environment]
-        [:div {:on-click #(do (rf/dispatch [:ui-component-deselect-all])
-                              (rf/dispatch [:ui-submenu-close-all])
-                              (.stopPropagation %))
-               :style    {:margin-left  "10px"
-                          :margin-right "10px"}}
+        [:div {:style {:margin-left  "10px"
+                       :margin-right "10px"}}
          [:svg {:id                  "svg"
                 :style               {:width "25em"}
                 :viewBox             (str "0 0 " (-> width (* 5) (+ 20)) " " (-> height (* 5) (+ 20) (+ 30)))
@@ -133,7 +130,9 @@
                         (.preventDefault event)
                         (.stopPropagation event)
                         (save-arms-clicked db-path))]
-    [:div.pure-g
+    [:div.pure-g {:on-click #(do (rf/dispatch [:ui-component-deselect-all])
+                                 (rf/dispatch [:ui-submenu-close-all])
+                                 (.stopPropagation %))}
      [:div.pure-u-1-2 {:style {:position "fixed"}}
       [render-coat-of-arms]]
      [:div.pure-u-1-2 {:style {:margin-left "50%"
