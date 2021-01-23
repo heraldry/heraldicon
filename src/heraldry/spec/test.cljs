014109d9ffcfed898f4cfad836ac453869170e76
Oliver Runge <oliver.runge@gmail.com>
2021-01-23 19:25:08 +0100

    Add ordinary spec and tests
    Also use "are" in tests to reduce boiler plate code.

--- src/heraldry/spec/test.cljs
+++ src/heraldry/spec/test.cljs
@@ -1,26 +1,83 @@
 (ns heraldry.spec.test
   (:require [cljs.spec.alpha :as s]
-            [cljs.test :refer-macros [deftest is]]
+            [cljs.test :refer-macros [deftest is are]]
             [heraldry.spec.core]))
 
-(deftest spec-field-content
-  (let [form {:component :field
-              :content   {:tincture :azure}}]
-    (s/explain :heraldry/field form)
-    (is (s/valid? :heraldry/field form))))
+(deftest valid-fields
+  (are [spec form] (do
+                     (s/explain spec form)
+                     (s/valid? spec form))
 
-(deftest spec-field-division
-  (let [form {:component :field
-              :division  {:type   :per-pale
-                          :line   {:type :invected}
-                          :fields [{:component :field
-                                    :content   {:tincture :azure}}
-                                   {:component :field
-                                    :content   {:tincture :or}}]
-                          :hints  {:outline? true}}}]
-    (s/explain :heraldry/field form)
-    (is (s/valid? :heraldry/field form))))
+    :heraldry/field {:component :field
+                     :content   {:tincture :azure}}
 
-(deftest spec-field-invalid-no-content
-  (let [form {:component :field}]
-    (is (not (s/valid? :heraldry/field form)))))
+    :heraldry/field {:component :field
+                     :division  {:type   :per-pale
+                                 :line   {:type         :invected
+                                          :eccentricity 1.3
+                                          :width        2
+                                          :offset       0.2
+                                          :flipped?     false}
+                                 :fields [{:component :field
+                                           :content   {:tincture :azure}}
+                                          {:component :field
+                                           :content   {:tincture :or}}]
+                                 :hints  {:outline? true}}}))
+
+(deftest invalid-fields
+  (are [spec form] (not (s/valid? spec form))
+
+    :heraldry/field {:component :field}
+
+    :heraldry/field {:component :something-else}
+
+    :heraldry/field {:component :field
+                     :division  {:type :does-not-exist}}))
+
+(deftest valid-ordinaries
+  (are [spec form] (do
+                     (s/explain spec form)
+                     (s/valid? spec form))
+
+    :heraldry/ordinary {:component :ordinary
+                        :type      :pale
+                        :field     {:component :field
+                                    :content   {:tincture :azure}}}
+
+    :heraldry/ordinary {:component     :ordinary
+                        :type          :fess
+                        :field         {:component :field
+                                        :content   {:tincture :azure}}
+                        :geometry      {:size 50}
+                        :origin        {:point    :fess
+                                        :offset-x -5
+                                        :offset-y 5}
+                        :line          {:type         :engrailed
+                                        :eccentricity 1.3
+                                        :width        2
+                                        :offset       0.2
+                                        :flipped?     false}
+                        :opposite-line {:type         :engrailed
+                                        :eccentricity 1.3
+                                        :width        2
+                                        :offset       0.2
+                                        :flipped?     true}}))
+
+(deftest invalid-ordinaries
+  (are [spec form] (not (s/valid? spec form))
+
+    :heraldry/ordinary {:component :ordinary}
+
+    :heraldry/ordinary {:component :something-else
+                        :type      :pale
+                        :field     {:component :field
+                                    :content   {:tincture :azure}}}
+
+    :heraldry/ordinary {:component :ordinary
+                        :type      :does-not-exist
+                        :field     {:component :field
+                                    :content   {:tincture :azure}}}
+
+    :heraldry/ordinary {:component :ordinary
+                        :type      :fess
+                        :field     {:component :field}}))
