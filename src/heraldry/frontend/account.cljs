f96943b0a5c2535211dbf7777bca57536dc62a57
Oliver Runge <oliver.runge@gmail.com>
2021-01-30 11:38:58 +0100

    Make account page, just listing own arms/charges for now

--- src/heraldry/frontend/account.cljs
+++ src/heraldry/frontend/account.cljs
@@ -1,4 +1,13 @@
-(ns heraldry.frontend.account)
+(ns heraldry.frontend.account
+  (:require [heraldry.frontend.user :as user]
+            [heraldry.frontend.user-library :as user-library]))
+
+(defn not-logged-in []
+  [:div {:style {:padding "15px"}}
+   "You need to be logged in."])
 
 (defn view []
-  [:div "hello!"])
+  (let [user-data (user/data)]
+    (if (:logged-in? user-data)
+      [user-library/view-user (:username user-data)]
+      [not-logged-in])))
