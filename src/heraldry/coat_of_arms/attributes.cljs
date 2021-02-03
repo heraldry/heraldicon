d6053eb5ccb69818285080e0eb99828f9aa23c90
Oliver Runge <oliver.runge@gmail.com>
2021-02-03 20:38:08 +0100

    Move attitudes into attributes module
    This will be a place where all such supported keywords are stored, but
    only in one place.

---
+++ src/heraldry/coat_of_arms/attributes.cljs
@@ -0,0 +1,50 @@
+(ns heraldry.coat-of-arms.attributes)
+
+(def attitude-options
+  [["None" :none]
+   ["Beasts"
+    ["Couchant" :couchant]
+    ["Courant" :courant]
+    ["Dormant" :dormant]
+    ["Pascuant" :pascuant]
+    ["Passant" :passant]
+    ["Rampant" :rampant]
+    ["Salient" :salient]
+    ["Sejant" :sejant]
+    ["Statant" :statant]
+    ["Segreant" :segreant]]
+   ["Two beasts"
+    ["Combatant" :combatant]
+    ["Respectant" :respectant]
+    ["Addorsed" :addorsed]]
+   ["Winged creatures"
+    ["Displayed" :displayed]
+    ["Overt" :overt]
+    ["Close" :close]
+    ["Issuant" :issuant]
+    ["Rising" :rising]
+    ["Volant" :volant]
+    ["Recursant" :recursant]
+    ["Vigilant" :vigilant]
+    ["Vulning herself" :vulning]
+    ["In her piety" :in-her-piety]]
+   ["Sea creatures"
+    ["Naiant" :naiant]
+    ["Urinant" :urinant]
+    ["Hauriant" :hauriant]]
+   ["Serpents"
+    ["Nowed" :nowed]
+    ["Glissant" :glissant]]])
+
+(def attitude-map
+  (->> attitude-options
+       (map (fn [[group-name & items]]
+              (if (and (-> items count (= 1))
+                       (-> items first keyword?))
+                ;; in this case there is no group, treat the first element of "items" as key
+                ;; and "group-name" as display-name
+                [[(first items) group-name]]
+                (->> items
+                     (map (comp vec reverse))))))
+       (apply concat)
+       (into {})))
