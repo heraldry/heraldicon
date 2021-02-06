6b1925925a14c7951fd4ec236e60c2ee408b9d14
Oliver Runge <oliver.runge@gmail.com>
2021-02-06 18:04:39 +0100

    Start work on a better charge map/tree
    Fetching the charges and building the map/tree client-side allows for
    more flexible uses of this component.

--- src/heraldry/frontend/charge_map.cljs
+++ src/heraldry/frontend/charge_map.cljs
@@ -1,5 +1,7 @@
 (ns heraldry.frontend.charge-map
   (:require [cljs.core.async :refer [go]]
+            [clojure.set :as set]
+            [clojure.walk :as walk]
             [com.wsscode.common.async-cljs :refer [<?]]
             [heraldry.api.request :as api-request]
             [heraldry.frontend.http :as http]
@@ -45,3 +47,115 @@
       (when (= status :done)
         charge-data))
     (println "error fetching charge data, variant:" variant)))
+
+(def group-map
+  {:node-type :_root
+   :groups    {:beasts {:node-type :group
+                        :name      "beasts"
+                        :groups    {:predators {:node-type :group
+                                                :name      "predators"
+                                                :charges   #{:lion :wolf :bear}}
+                                    :ungulates {:node-type :group
+                                                :name      "ungulates"
+                                                :charges   #{:antelope :deer :boar}}
+                                    :reptiles  {:node-type :group
+                                                :name      "reptiles"
+                                                :charges   #{:lizard :serpent}}
+                                    :insects   {:node-type :group
+                                                :name      "insects"
+                                                :charges   #{:bee :butterfly}}
+                                    :hybrids   {:node-type :group
+                                                :name      "hybrids"
+                                                :charges   #{:sphinx :griffin :unicorn}}
+                                    :birds     {:node-type :group
+                                                :name      "birds"
+                                                :charges   #{:dove :crane :eagle :owl :corvus}}}}}})
+
+(def known-charge-types
+  (walk/postwalk (fn [data]
+                   (cond
+                     (:charges data) (:charges data)
+                     (:groups data)  (:groups data)
+                     (map? data)     (->> data
+                                          (map second)
+                                          (apply set/union))
+                     :else           data)) group-map))
+
+(defn build-charge-variants [node groupable-types charges]
+  (let [type            (first groupable-types)
+        remaining-types (drop 1 groupable-types)
+        groups          (if type
+                          (group-by type charges)
+                          {})
+        nil-group       (if type
+                          (get groups nil)
+                          charges)
+        non-nil-groups  (when type
+                          (into {}
+                                (filter first groups)))]
+    (-> node
+        (cond->
+            non-nil-groups (assoc (-> type
+                                      name
+                                      (str "s")
+                                      keyword)
+                                  (->> non-nil-groups
+                                       (map (fn [[value grouped-charges]]
+                                              [value (build-charge-variants
+                                                      {:node-type type
+                                                       :type      value
+                                                       :name      (name value)}
+                                                      remaining-types
+                                                      grouped-charges)]))
+                                       (into {})))
+            (and nil-group
+                 (-> remaining-types
+                     seq))    (build-charge-variants remaining-types nil-group)
+            (and nil-group
+                 (-> remaining-types
+                     empty?)) (assoc :variants (->> nil-group
+                                                    (map (fn [charge]
+                                                           [(:id charge) {:node-type :variant
+                                                                          :name      (:name charge)
+                                                                          :data      charge}]))
+                                                    (into {})))))))
+
+(defn build-map [group-map charges-by-type]
+  (let [charge-map (walk/postwalk
+                    (fn [data]
+                      (if (and (-> data seqable?)
+                               (-> data first (= :charges)))
+                        (let [charge-types (second data)]
+                          [:charges (->> charge-types
+                                         (map (fn [type]
+                                                [type (build-charge-variants
+                                                       {:node-type :charge
+                                                        :type      type
+                                                        :name      (name type)}
+                                                       [:attitude]
+                                                       (get charges-by-type type))]))
+
+                                         (into {}))])
+                        data)) group-map)
+        ungrouped-types (-> charges-by-type
+                            keys
+                            set
+                            (set/difference known-charge-types))]
+    (cond-> charge-map
+      (seq ungrouped-types) (assoc-in [:groups :ungrouped]
+                                      {:node-type :group
+                                       :name      "ungrouped"
+                                       :charges   (->> ungrouped-types
+                                                       (map (fn [type]
+                                                              [type (build-charge-variants
+                                                                     {:node-type :charge
+                                                                      :type      type
+                                                                      :name      (name type)}
+                                                                     [:attitude]
+                                                                     (get charges-by-type type))]))
+                                                       (into {}))}))))
+
+(defn build-charge-map [charges]
+  (->> charges
+       (group-by :type)
+       (build-map group-map)))
