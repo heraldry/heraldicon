8405077e9bee12d7211b018bced5c0ee46b151b2
Oliver Runge <oliver.runge@gmail.com>
2021-02-07 16:06:23 +0100

    Move charge-fetching functions into frontend.charge
    frontend.charge-map remains for anything involving the charge-map.

--- src/heraldry/frontend/charge.cljs
+++ src/heraldry/frontend/charge.cljs
@@ -3,6 +3,7 @@
             [com.wsscode.common.async-cljs :refer [<?]]
             [heraldry.api.request :as api-request]
             [heraldry.frontend.http :as http]
+            [heraldry.frontend.state :as state]
             [heraldry.frontend.user :as user]))
 
 (defn fetch-charges-for-user [user-id]
@@ -31,12 +32,21 @@
   (go
     (try
       (let [user-data (user/data)
-            response (<? (api-request/call :fetch-charge {:id charge-id
-                                                          :version version} user-data))
-            edn-data (<? (http/fetch (:edn-data-url response)))
-            svg-data (<? (http/fetch (:svg-data-url response)))]
-        (-> response
-            (assoc-in [:data :edn-data] edn-data)
-            (assoc-in [:data :svg-data] svg-data)))
+            charge-data (<? (api-request/call :fetch-charge {:id charge-id
+                                                             :version version} user-data))
+            edn-data (<? (http/fetch (:edn-data-url charge-data)))]
+        (-> charge-data
+            (assoc-in [:data :edn-data] edn-data)))
       (catch :default e
         (println "fetch-charge error:" e)))))
+
+(defn fetch-charge-data [{:keys [id version] :as variant}]
+  (if (and id version)
+    (let [db-path [:charge-data variant]
+          [status charge-data] (state/async-fetch-data
+                                db-path
+                                variant
+                                #(fetch-charge id version))]
+      (when (= status :done)
+        charge-data))
+    (println "error fetching charge data, variant:" variant)))
