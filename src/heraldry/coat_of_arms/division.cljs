a6e05184c204b67051de39289566de5ac0983e3b
Oliver Runge <oliver.runge@gmail.com>
2021-02-15 23:05:01 +0100

    Improve chequy

--- src/heraldry/coat_of_arms/division.cljs
+++ src/heraldry/coat_of_arms/division.cljs
@@ -182,11 +182,7 @@
                               [:layout :num-fields-y]
                               [:layout :offset-y]
                               [:layout :stretch-y]
-                              [:origin :point]
-                              [:origin :offset-x]
-                              [:origin :offset-y]]
-                             {[:layout :num-fields-y :default] nil
-                              [:origin :point :choices] position/point-choices-y})
+                              {[:layout :num-fields-y :default] 7}])
        :bendy (pick-options [[:line]
                              [:layout :num-base-fields]
                              [:layout :num-fields-y]
@@ -287,6 +283,13 @@
                                              (nth defaults (mod (+ i 2) (count defaults)))) (range (- num-base-fields 2))))
                                 (into (map (fn [i]
                                              {:ref (mod i num-base-fields)}) (range (- num-fields-y num-base-fields)))))
+            (= :chequy type) (-> []
+                                 (into (map (fn [i]
+                                              (nth defaults (mod (+ i 2) (count defaults)))) (range (- num-base-fields 2))))
+                                 (into (->> (for [j (range num-fields-y)
+                                                  i (range num-fields-x)]
+                                              {:ref (mod (+ i j) num-base-fields)})
+                                            (drop num-base-fields))))
             (#{:bendy
                :bendy-sinister} type) (-> []
                                           (into (map (fn [i]
@@ -630,13 +633,90 @@
                      (let [x1 (+ x0 (* i part-width))
                            x2 (+ x1 part-width)
                            y1 (+ y0 (* j part-height))
-                           y2 (+ y1 part-height)]
-                       [["M" [x1 y1]
-                         "L" [x2 y1]
-                         "L" [x2 y2]
-                         "L" [x1 y2]
-                         "z"]
-                        [(v/v x1 y1) (v/v x2 y2)]]))
+                           y2 (+ y1 part-height)
+                           first-x? (zero? i)
+                           first-y? (zero? j)
+                           last-x? (-> num-fields-x dec (= i))
+                           last-y? (-> num-fields-y dec (= j))]
+                       (cond
+                         (and first-x?
+                              first-y?) [["M" [x1 y2]
+                                          "L" [x2 y2]
+                                          "L" [x2 y1]
+                                          (infinity/path :counter-clockwise
+                                                         [:top :left]
+                                                         [(v/v x2 y1) (v/v x1 y2)])
+                                          "z"]
+                                         [(v/v x1 y1) (v/v x2 y2)]]
+                         (and last-x?
+                              first-y?) [["M" [x1 y1]
+                                          "L" [x1 y2]
+                                          "L" [x2 y2]
+                                          (infinity/path :counter-clockwise
+                                                         [:right :top]
+                                                         [(v/v x2 y2) (v/v x1 y1)])
+                                          "z"]
+                                         [(v/v x1 y1) (v/v x2 y2)]]
+                         (and first-x?
+                              last-y?) [["M" [x1 y1]
+                                         "L" [x2 y1]
+                                         "L" [x2 y2]
+                                         (infinity/path :clockwise
+                                                        [:bottom :left]
+                                                        [(v/v x2 y2) (v/v x1 y1)])
+                                         "z"]
+                                        [(v/v x1 y1) (v/v x2 y2)]]
+                         (and last-x?
+                              last-y?) [["M" [x1 y2]
+                                         "L" [x1 y1]
+                                         "L" [x2 y1]
+                                         (infinity/path :clockwise
+                                                        [:right :bottom]
+                                                        [(v/v x2 y1) (v/v x1 y2)])
+                                         "z"]
+                                        [(v/v x1 y1) (v/v x2 y2)]]
+                         first-x? [["M" [x1 y1]
+                                    "L" [x2 y1]
+                                    "L" [x2 y2]
+                                    "L" [x1 y2]
+                                    (infinity/path :counter-clockwise
+                                                   [:left :left]
+                                                   [(v/v x1 y2) (v/v x1 y1)])
+                                    "z"]
+                                   [(v/v x1 y1) (v/v x2 y2)]]
+                         first-y? [["M" [x1 y1]
+                                    "L" [x1 y2]
+                                    "L" [x2 y2]
+                                    "L" [x2 y1]
+                                    (infinity/path :counter-clockwise
+                                                   [:top :top]
+                                                   [(v/v x2 y1) (v/v x1 y1)])
+                                    "z"]
+                                   [(v/v x1 y1) (v/v x2 y2)]]
+                         last-x? [["M" [x2 y2]
+                                   "L" [x1 y2]
+                                   "L" [x1 y1]
+                                   "L" [x2 y1]
+                                   (infinity/path :clockwise
+                                                  [:right :right]
+                                                  [(v/v x2 y1) (v/v x2 y2)])
+                                   "z"]
+                                  [(v/v x1 y1) (v/v x2 y2)]]
+                         last-y? [["M" [x1 y2]
+                                   "L" [x1 y1]
+                                   "L" [x2 y1]
+                                   "L" [x2 y2]
+                                   (infinity/path :clockwise
+                                                  [:bottom :bottom]
+                                                  [(v/v x2 y2) (v/v x1 y2)])
+                                   "z"]
+                                  [(v/v x1 y1) (v/v x2 y2)]]
+                         :else [["M" [x1 y1]
+                                 "L" [x2 y1]
+                                 "L" [x2 y2]
+                                 "L" [x1 y2]
+                                 "z"]
+                                [(v/v x1 y1) (v/v x2 y2)]])))
                    vec)
         overlap (->> (for [j (range num-fields-y)
                            i (range num-fields-x)]
@@ -648,19 +728,20 @@
                                           "L" [x2 y2]
                                           "L" [x1 y2]])]))
                      vec)
+        outline-extra 50
         outlines (when (or (:outline? render-options)
                            (:outline? hints))
                    [:g outline-style
-                    (for [i (range num-fields-x)]
+                    (for [i (range 1 num-fields-x)]
                       (let [x1 (+ x0 (* i part-width))]
-
                         ^{:key [:x i]}
-                        [:path {:d (svg/make-path ["M" [x1 y0] "L" [x1 (+ y0 height)]])}]))
-                    (for [j (range num-fields-y)]
+                        [:path {:d (svg/make-path ["M" [x1 (- y0 outline-extra)]
+                                                   "L" [x1 (+ y0 required-height outline-extra)]])}]))
+                    (for [j (range 1 num-fields-y)]
                       (let [y1 (+ y0 (* j part-height))]
-
                         ^{:key [:y j]}
-                        [:path {:d (svg/make-path ["M" [x0 y1] "L" [(+ x0 width) y1]])}]))])]
+                        [:path {:d (svg/make-path ["M" [(- x0 outline-extra) y1]
+                                                   "L" [(+ x0 required-width outline-extra) y1]])}]))])]
     [parts overlap outlines]))
 
 (defn per-pale
