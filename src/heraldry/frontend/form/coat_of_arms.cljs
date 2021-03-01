71cbb4f975210be55589978cbdddf7a0eabaf495
Oliver Runge <oliver.runge@gmail.com>
2021-03-01 18:29:54 +0100

    Move coat-of-arms form into own module

--- src/heraldry/frontend/form/coat_of_arms.cljs
+++ src/heraldry/frontend/form/coat_of_arms.cljs
@@ -1,9 +1,9 @@
-(ns heraldry.frontend.form.component
+(ns heraldry.frontend.form.coat-of-arms
   (:require [heraldry.frontend.form.element :as element]
             [heraldry.frontend.form.escutcheon :as escutcheon]
             [heraldry.frontend.form.field :as field]))
 
-(defn form-for-coat-of-arms [db-path]
+(defn form [db-path]
   [element/component db-path :coat-of-arms "Coat of Arms" nil
    [escutcheon/form (conj db-path :escutcheon) "Default Escutcheon" :label-width "11em"]
    [field/form (conj db-path :field)]])
