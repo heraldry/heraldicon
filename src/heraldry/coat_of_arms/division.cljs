b1ffa1ead183fc6fbf8162af4055096065b37c18
Oliver Runge <oliver.runge@gmail.com>
2021-03-01 10:16:58 +0100

    Move to-roman to heraldry.util

--- src/heraldry/coat_of_arms/division.cljs
+++ src/heraldry/coat_of_arms/division.cljs
@@ -2505,31 +2505,9 @@
 (def division-map
   (util/choices->map choices))
 
-;; https://gist.github.com/jimweirich/1388782
-(def roman-reductions
-  '((1000 "M")
-    (900 "CM") (500 "D") (400 "CD") (100 "C")
-    (90 "XC") (50 "L") (40 "XL") (10 "X")
-    (9 "IX") (5 "V") (4 "IV") (1 "I")))
-
-(defn to-roman [number]
-  (let [counts
-        (map first
-             (drop 1
-                   (reductions (fn [[_ r] v]
-                                 (list (int (/ r v))
-                                       (- r (* v (int (/ r v))))))
-                               (list 0 number)
-                               (map first roman-reductions))))
-        glyphs (map second roman-reductions)]
-    (apply str
-           (flatten
-            (map (fn [[c g]] (take c (repeat g)))
-                 (map vector counts glyphs))))))
-
 (defn part-name [type index]
   (let [function (get kinds-function-map type)]
-    (-> function meta :parts (get index) (or (to-roman (inc index))))))
+    (-> function meta :parts (get index) (or (util/to-roman (inc index))))))
 
 (defn render [{:keys [type] :as division} environment context]
   (let [function (get kinds-function-map type)]
--- src/heraldry/util.cljs
+++ src/heraldry/util.cljs
@@ -70,3 +70,25 @@
        (apply merge-with m maps)
        (apply f maps)))
    maps))
+
+;; https://gist.github.com/jimweirich/1388782
+(def roman-reductions
+  '((1000 "M")
+    (900 "CM") (500 "D") (400 "CD") (100 "C")
+    (90 "XC") (50 "L") (40 "XL") (10 "X")
+    (9 "IX") (5 "V") (4 "IV") (1 "I")))
+
+(defn to-roman [number]
+  (let [counts
+        (map first
+             (drop 1
+                   (reductions (fn [[_ r] v]
+                                 (list (int (/ r v))
+                                       (- r (* v (int (/ r v))))))
+                               (list 0 number)
+                               (map first roman-reductions))))
+        glyphs (map second roman-reductions)]
+    (apply str
+           (flatten
+            (map (fn [[c g]] (take c (repeat g)))
+                 (map vector counts glyphs))))))
