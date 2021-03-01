9b401217d6e882a8af315f12015e9f781a870521
Oliver Runge <oliver.runge@gmail.com>
2021-03-01 18:24:24 +0100

    Move attribution form into own module

---
+++ src/heraldry/frontend/form/attribution.cljs
@@ -0,0 +1,26 @@
+(ns heraldry.frontend.form.attribution
+  (:require [heraldry.frontend.form.element :as element]
+            [re-frame.core :as rf]))
+
+(defn form [db-path]
+  (let [attribution-options [["None (No sharing)" :none]
+                             ["CC Attribution" :cc-attribution]
+                             ["CC Attribution-ShareAlike" :cc-attribution-share-alike]
+                             ["Public Domain" :public-domain]]
+        license-nature @(rf/subscribe [:get (conj db-path :nature)])]
+    [element/component db-path :attribution "Attribution / License" nil
+     [element/select (conj db-path :license) "License" attribution-options]
+     [element/radio-select (conj db-path :nature) [["Own work" :own-work]
+                                                   ["Derivative" :derivative]]
+      :default :own-work]
+     (when (= license-nature :derivative)
+       [:<>
+        [element/select (conj db-path :source-license) "Source license" (assoc-in
+                                                                         attribution-options
+                                                                         [0 0]
+                                                                         "None")]
+        [element/text-field (conj db-path :source-name) "Source name"]
+        [element/text-field (conj db-path :source-link) "Source link"]
+        [element/text-field (conj db-path :source-creator-name) "Creator name"]
+        [element/text-field (conj db-path :source-creator-link) "Creator link"]])
+     [:div {:style {:margin-bottom "1em"}} " "]]))
--- src/heraldry/frontend/form/component.cljs
+++ src/heraldry/frontend/form/component.cljs
@@ -242,26 +242,3 @@
                                (-> component :type keyword? not)))
                  [:a {:on-click #(state/dispatch-on-event % [:remove-component component-path])}
                   [:i.far.fa-trash-alt]])]])))]]]))
-
-(defn form-attribution [db-path]
-  (let [attribution-options [["None (No sharing)" :none]
-                             ["CC Attribution" :cc-attribution]
-                             ["CC Attribution-ShareAlike" :cc-attribution-share-alike]
-                             ["Public Domain" :public-domain]]
-        license-nature @(rf/subscribe [:get (conj db-path :nature)])]
-    [element/component db-path :attribution "Attribution / License" nil
-     [element/select (conj db-path :license) "License" attribution-options]
-     [element/radio-select (conj db-path :nature) [["Own work" :own-work]
-                                                   ["Derivative" :derivative]]
-      :default :own-work]
-     (when (= license-nature :derivative)
-       [:<>
-        [element/select (conj db-path :source-license) "Source license" (assoc-in
-                                                                         attribution-options
-                                                                         [0 0]
-                                                                         "None")]
-        [element/text-field (conj db-path :source-name) "Source name"]
-        [element/text-field (conj db-path :source-link) "Source link"]
-        [element/text-field (conj db-path :source-creator-name) "Creator name"]
-        [element/text-field (conj db-path :source-creator-link) "Creator link"]])
-     [:div {:style {:margin-bottom "1em"}} " "]]))
