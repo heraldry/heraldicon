c8b2a9a8a99277c19732c049a29944cd4bec7462
Oliver Runge <oliver.runge@gmail.com>
2021-03-05 09:12:46 +0100

    Add new fimbriation approach to lines, use it for ordinary fess
    This works for the "outside" mode, "inside" and "even" still need to be
    added.

--- src/heraldry/coat_of_arms/line/fimbriation.cljs
+++ src/heraldry/coat_of_arms/line/fimbriation.cljs
@@ -1,6 +1,7 @@
 (ns heraldry.coat-of-arms.line.fimbriation
   (:require [clojure.walk :as walk]
             [heraldry.coat-of-arms.infinity :as infinity]
+            [heraldry.coat-of-arms.outline :as outline]
             [heraldry.coat-of-arms.svg :as svg]
             [heraldry.coat-of-arms.tincture.core :as tincture]
             [heraldry.coat-of-arms.vector :as v]
@@ -108,23 +109,26 @@
                                    (svg/stitch fimbriation-2))])}])]]
     [elements outlines]))
 
-(defn dilate-and-fill-path [shape negate-shape thickness color {:keys [svg-export?]}]
+(defn dilate-and-fill-path [shape negate-shape thickness color {:keys [svg-export?]}
+                            & {:keys [linejoin fill?] :or {linejoin "round"
+                                                           fill?    true}}]
   (let [mask-id (util/id "mask")]
     [:<>
      [:defs
       [:mask {:id mask-id}
        [:path {:d     shape
-               :fill  "#ffffff"
+               :fill  (when fill? "#ffffff")
                :style {:stroke-width      thickness
                        :stroke            "#ffffff"
-                       :stroke-linejoin   "round"
+                       :stroke-linejoin   linejoin
                        :stroke-miterlimit 10}}]
-       [:path {:d     negate-shape
-               :fill  "#000000"
-               :style {:stroke-width      thickness
-                       :stroke            "#ffffff"
-                       :stroke-linejoin   "round"
-                       :stroke-miterlimit 10}}]]]
+       (when negate-shape
+         [:path {:d     negate-shape
+                 :fill  "#000000"
+                 :style {:stroke-width      thickness
+                         :stroke            "#ffffff"
+                         :stroke-linejoin   linejoin
+                         :stroke-miterlimit 10}}])]]
 
      [:rect {:x      -500
              :y      -500
@@ -135,7 +139,7 @@
              :style  (when (not svg-export?)
                        {:pointer-events "none"})}]]))
 
-(defn dilate-recursively [data stroke-width color]
+(defn dilate-recursively [data stroke-width color linejoin]
   (walk/postwalk #(cond
                     (and (vector? %)
                          (-> % first (= :stroke-width)))    [(first %) stroke-width]
@@ -144,13 +148,14 @@
                                                                            second
                                                                            (conj [:stroke-width stroke-width]))]
                     (and (vector? %)
-                         (-> % first (= :stroke-linejoin))) [(first %) "round"]
+                         (-> % first (= :stroke-linejoin))) [(first %) linejoin]
                     (and (vector? %)
                          (-> % first #{:stroke :fill}))     [(first %) color]
                     :else                                   %)
                  data))
 
-(defn dilate-and-fill [shape thickness color {:keys [svg-export?]} & {:keys [transform]}]
+(defn dilate-and-fill [shape thickness color {:keys [svg-export?]}
+                       & {:keys [transform linejoin] :or {linejoin "round"}}]
   (let [mask-id (util/id "mask")]
     [:<>
      [:defs
@@ -158,9 +163,9 @@
        [:g {:fill  "#ffffff"
             :style {:stroke-width      thickness
                     :stroke            "#ffffff"
-                    :stroke-linejoin   "round"
+                    :stroke-linejoin   linejoin
                     :stroke-miterlimit 10}}
-        (dilate-recursively shape thickness "#ffffff")]]]
+        (dilate-recursively shape thickness "#ffffff" linejoin)]]]
      [:g {:mask (str "url(#" mask-id ")")}
       [:g {:transform transform}
        [:rect {:x      -500
@@ -170,3 +175,53 @@
                :fill   color
                :style  (when (not svg-export?)
                          {:pointer-events "none"})}]]]]))
+
+(defn draw-line [line line-data start outline? render-options]
+  (let [{:keys [fimbriation]} line
+        line-path             (svg/make-path ["M" (v/+ start
+                                                       (:line-start line-data))
+                                              (svg/stitch (:line line-data))])
+        {:keys [thickness-1
+                thickness-2]} fimbriation
+        thickness-2           (+ thickness-1 thickness-2)]
+    [:<>
+     (when (-> fimbriation :mode #{:double})
+       [:<>
+        (when outline?
+          [dilate-and-fill-path
+           line-path
+           nil
+           (+ thickness-2 outline/stroke-width)
+           outline/color render-options
+           :linejoin "miter"
+           :fill? false])
+        [dilate-and-fill-path
+         line-path
+         nil
+         (cond-> thickness-2
+           outline? (- outline/stroke-width))
+         (-> fimbriation
+             :tincture-2
+             (tincture/pick render-options)) render-options
+         :linejoin "miter"
+         :fill? false]])
+     (when (-> fimbriation :mode #{:single :double})
+       [:<>
+        (when outline?
+          [dilate-and-fill-path
+           line-path
+           nil
+           (+ thickness-1 outline/stroke-width)
+           outline/color render-options
+           :linejoin "miter"
+           :fill? false])
+        [dilate-and-fill-path
+         line-path
+         nil
+         (cond-> thickness-1
+           outline? (- outline/stroke-width))
+         (-> fimbriation
+             :tincture-1
+             (tincture/pick render-options)) render-options
+         :linejoin "miter"
+         :fill? false]])]))
--- src/heraldry/coat_of_arms/ordinary/type/fess.cljs
+++ src/heraldry/coat_of_arms/ordinary/type/fess.cljs
@@ -16,91 +16,78 @@
   {:display-name "Fess"
    :value        :fess}
   [{:keys [field hints] :as ordinary} parent environment {:keys [render-options] :as context}]
-  (let [{:keys [line origin geometry]}                            (options/sanitize ordinary (ordinary-options/options ordinary))
-        {:keys [size]}                                            geometry
-        opposite-line                                             (ordinary-options/sanitize-opposite-line ordinary line)
-        points                                                    (:points environment)
-        origin-point                                              (position/calculate origin environment :fess)
-        left                                                      (assoc (:left points) :y (:y origin-point))
-        right                                                     (assoc (:right points) :y (:y origin-point))
-        height                                                    (:height environment)
-        band-height                                               (-> size
-                                                                      ((util/percent-of height)))
-        row1                                                      (- (:y origin-point) (/ band-height 2))
-        row2                                                      (+ row1 band-height)
-        line                                                      (-> line
-                                                                      (update-in [:fimbriation :thickness-1] (util/percent-of height))
-                                                                      (update-in [:fimbriation :thickness-2] (util/percent-of height)))
-        opposite-line                                             (-> opposite-line
-                                                                      (update-in [:fimbriation :thickness-1] (util/percent-of height))
-                                                                      (update-in [:fimbriation :thickness-2] (util/percent-of height)))
+  (let [{:keys [line origin geometry]}           (options/sanitize ordinary (ordinary-options/options ordinary))
+        {:keys [size]}                           geometry
+        opposite-line                            (ordinary-options/sanitize-opposite-line ordinary line)
+        points                                   (:points environment)
+        origin-point                             (position/calculate origin environment :fess)
+        left                                     (assoc (:left points) :y (:y origin-point))
+        right                                    (assoc (:right points) :y (:y origin-point))
+        height                                   (:height environment)
+        band-height                              (-> size
+                                                     ((util/percent-of height)))
+        row1                                     (- (:y origin-point) (/ band-height 2))
+        row2                                     (+ row1 band-height)
+        line                                     (-> line
+                                                     (update-in [:fimbriation :thickness-1] (util/percent-of height))
+                                                     (update-in [:fimbriation :thickness-2] (util/percent-of height)))
+        opposite-line                            (-> opposite-line
+                                                     (update-in [:fimbriation :thickness-1] (util/percent-of height))
+                                                     (update-in [:fimbriation :thickness-2] (util/percent-of height)))
         {line-one       :line
          line-one-start :line-start
-         :as            line-one-data}                            (line/create line
-                                                                               (:x (v/- right left))
-                                                                               :render-options render-options)
+         :as            line-one-data}           (line/create line
+                                                              (:x (v/- right left))
+                                                              :render-options render-options)
         {line-reversed       :line
          line-reversed-start :line-start
-         :as                 line-reversed-data}                  (line/create opposite-line
-                                                                               (:x (v/- right left))
-                                                                               :reversed? true
-                                                                               :angle 180
-                                                                               :render-options render-options)
-        first-left                                                (v/v (:x left) row1)
-        first-right                                               (v/v (:x right) row1)
-        second-left                                               (v/v (:x left) row2)
-        second-right                                              (v/v (:x right) row2)
-        parts                                                     [[["M" (v/+ first-left
-                                                                              line-one-start)
-                                                                     (svg/stitch line-one)
-                                                                     (infinity/path :clockwise
-                                                                                    [:right :right]
-                                                                                    [(v/+ first-right
-                                                                                          line-one-start)
-                                                                                     (v/+ second-right
-                                                                                          line-reversed-start)])
-                                                                     (svg/stitch line-reversed)
-                                                                     (infinity/path :clockwise
-                                                                                    [:left :left]
-                                                                                    [(v/+ second-left
-                                                                                          line-reversed-start)
-                                                                                     (v/+ first-left
-                                                                                          line-one-start)])
-                                                                     "z"]
-                                                                    [(v/+ first-right
-                                                                          line-one-start)
-                                                                     (v/+ second-left
-                                                                          line-reversed-start)]]]
-        field                                                     (if (counterchange/counterchangable? field parent)
-                                                                    (counterchange/counterchange-field field parent)
-                                                                    field)
-        [fimbriation-elements-top fimbriation-outlines-top]       (fimbriation/render
-                                                                   [first-left :left]
-                                                                   [first-right :right]
-                                                                   [line-one-data]
-                                                                   (:fimbriation line)
-                                                                   render-options)
-        [fimbriation-elements-bottom fimbriation-outlines-bottom] (fimbriation/render
-                                                                   [second-right :right]
-                                                                   [second-left :left]
-                                                                   [line-reversed-data]
-                                                                   (:fimbriation opposite-line)
-                                                                   render-options)]
+         :as                 line-reversed-data} (line/create opposite-line
+                                                              (:x (v/- right left))
+                                                              :reversed? true
+                                                              :angle 180
+                                                              :render-options render-options)
+        first-left                               (v/v (:x left) row1)
+        first-right                              (v/v (:x right) row1)
+        second-left                              (v/v (:x left) row2)
+        second-right                             (v/v (:x right) row2)
+        parts                                    [[["M" (v/+ first-left
+                                                             line-one-start)
+                                                    (svg/stitch line-one)
+                                                    (infinity/path :clockwise
+                                                                   [:right :right]
+                                                                   [(v/+ first-right
+                                                                         line-one-start)
+                                                                    (v/+ second-right
+                                                                         line-reversed-start)])
+                                                    (svg/stitch line-reversed)
+                                                    (infinity/path :clockwise
+                                                                   [:left :left]
+                                                                   [(v/+ second-left
+                                                                         line-reversed-start)
+                                                                    (v/+ first-left
+                                                                         line-one-start)])
+                                                    "z"]
+                                                   [(v/+ first-right
+                                                         line-one-start)
+                                                    (v/+ second-left
+                                                         line-reversed-start)]]]
+        field                                    (if (counterchange/counterchangable? field parent)
+                                                   (counterchange/counterchange-field field parent)
+                                                   field)
+        outline?                                 (or (:outline? render-options)
+                                                     (:outline? hints))]
     [:<>
-     fimbriation-elements-top
-     fimbriation-elements-bottom
+     (fimbriation/draw-line line line-one-data first-left outline? render-options)
+     (fimbriation/draw-line opposite-line line-reversed-data second-right outline? render-options)
      [division-shared/make-division
       :ordinary-fess [field] parts
       [:all]
       environment ordinary context]
-     (when (or (:outline? render-options)
-               (:outline? hints))
+     (when outline?
        [:g outline/style
         [:path {:d (svg/make-path
                     ["M" (v/+ first-left line-one-start)
                      (svg/stitch line-one)])}]
         [:path {:d (svg/make-path
                     ["M" (v/+ second-right line-reversed-start)
-                     (svg/stitch line-reversed)])}]
-        fimbriation-outlines-top
-        fimbriation-outlines-bottom])]))
+                     (svg/stitch line-reversed)])}]])]))
