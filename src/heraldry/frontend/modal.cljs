7bef6b71e9acc5e42e9ddbbaf7071cde2539169f
Oliver Runge <oliver.runge@gmail.com>
2021-03-09 20:58:32 +0100

    Add loading modal, use it for some backend-interaction buttons

--- src/heraldry/frontend/modal.cljs
+++ src/heraldry/frontend/modal.cljs
@@ -1,23 +1,39 @@
 (ns heraldry.frontend.modal
   (:require [re-frame.core :as rf]))
 
-(def db-path
-  [:modal])
+(def dialog-db-path
+  [:modal :dialog])
+
+(def loader-db-path
+  [:modal :loader])
 
 (defn create [title content & {:keys [on-cancel]}]
-  (rf/dispatch [:set db-path {:title title
-                              :content content
-                              :on-cancel on-cancel}]))
+  (rf/dispatch [:set dialog-db-path {:title title
+                                     :content content
+                                     :on-cancel on-cancel}]))
 (defn clear []
-  (when-let [on-cancel @(rf/subscribe [:get (conj db-path :on-cancel)])]
+  (when-let [on-cancel @(rf/subscribe [:get (conj dialog-db-path :on-cancel)])]
     (on-cancel))
-  (rf/dispatch [:remove db-path]))
+  (rf/dispatch [:remove dialog-db-path]))
+
+(defn start-loading []
+  (rf/dispatch-sync [:set loader-db-path true]))
+
+(defn stop-loading []
+  (rf/dispatch-sync [:set loader-db-path nil]))
 
 (defn render []
-  (let [{:keys [title content]} @(rf/subscribe [:get db-path])]
-    (when content
-      [:<>
-       [:div.modal-background {:on-click #(clear)}]
-       [:div.modal
-        [:div.header title]
-        [:div.content content]]])))
+  (let [{:keys [title content]} @(rf/subscribe [:get dialog-db-path])
+        loader @(rf/subscribe [:get loader-db-path])]
+    [:<>
+     (when content
+       [:<>
+        [:div.modal-background {:on-click #(clear)}]
+        [:div.modal.dialog
+         [:div.header title]
+         [:div.content content]]])
+     (when loader
+       [:<>
+        [:div.modal-background]
+        [:div.modal
+         [:div.loader]]])]))
