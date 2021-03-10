7d0411d622abdc647a15cee46f3e3f3460c97e17
Oliver Runge <oliver.runge@gmail.com>
2021-03-10 15:46:18 +0100

    Allow options/merge to overwrite values
    Previously options had to exist in the base options in order to be
    merge-able. Furthermore, min/max values would be clamped based on the
    base option values. This is not necessary at all and complects things a
    lot, so throw it out.

--- src/heraldry/coat_of_arms/options.cljs
+++ src/heraldry/coat_of_arms/options.cljs
@@ -38,21 +38,10 @@
       nil)))
 
 #_{:clj-kondo/ignore [:redefined-var]}
-(defn merge
-  [x other]
-  (let [x (if (nil? x) {} x)]
-    (into x
-          (for [[key value] other]
-            (let [other-value (get x key)]
-              (cond
-                (nil? value)             [key nil]
-                (and (contains? x key)
-                     (nil? other-value)) [key nil]
-                (map? value)             [key (merge other-value value)]
-                :else                    [key (cond
-                                                (= key :min) (max value other-value)
-                                                (= key :max) (min value other-value)
-                                                :else        value)]))))))
+(defn merge [x other]
+  (util/deep-merge-with (fn [_current-value new-value]
+                          new-value)
+                        x (or other {})))
 
 (defn sanitize [values given-options]
   (into {}
