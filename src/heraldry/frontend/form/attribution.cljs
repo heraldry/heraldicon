179e5fef0eff724e1c4212cb554874a5ca02795b
Oliver Runge <oliver.runge@gmail.com>
2021-03-11 12:40:37 +0100

    Refactor licensing/attribution options
    Foremost, move them into a separate module. Then add CC license
    versioning and the NonCommercial version of the license.

--- src/heraldry/frontend/form/attribution.cljs
+++ src/heraldry/frontend/form/attribution.cljs
@@ -1,24 +1,32 @@
 (ns heraldry.frontend.form.attribution
   (:require [heraldry.frontend.form.element :as element]
+            [heraldry.license :as license]
             [re-frame.core :as rf]))
 
 (defn form [db-path]
-  (let [attribution-options [["None (No sharing)" :none]
-                             ["CC Attribution" :cc-attribution]
-                             ["CC Attribution-ShareAlike" :cc-attribution-share-alike]
-                             ["Public Domain" :public-domain]]
-        license-nature @(rf/subscribe [:get (conj db-path :nature)])]
+  (let [license-nature @(rf/subscribe [:get (conj db-path :nature)])
+        current-license @(rf/subscribe [:get (conj db-path :license)])
+        current-source-license @(rf/subscribe [:get (conj db-path :source-license)])]
     [element/component db-path :attribution "Attribution / License" nil
-     [element/select (conj db-path :license) "License" attribution-options]
-     [element/radio-select (conj db-path :nature) [["Own work" :own-work]
-                                                   ["Derivative" :derivative]]
+     [element/select (conj db-path :license) "License"
+      (assoc-in
+       license/license-choices
+       [0 0]
+       "None (no sharing)")]
+     (when (license/cc-license? current-license)
+       [element/select (conj db-path :license-version) "License version"
+        license/cc-license-version-choices
+        :default :v4])
+     [element/radio-select (conj db-path :nature) license/nature-choices
       :default :own-work]
      (when (= license-nature :derivative)
        [:<>
-        [element/select (conj db-path :source-license) "Source license" (assoc-in
-                                                                         attribution-options
-                                                                         [0 0]
-                                                                         "None")]
+        [element/select (conj db-path :source-license) "Source license"
+         license/license-choices]
+        (when (license/cc-license? current-source-license)
+          [element/select (conj db-path :source-license-version) "License version"
+           license/cc-license-version-choices
+           :default :v4])
         [element/text-field (conj db-path :source-name) "Source name"]
         [element/text-field (conj db-path :source-link) "Source link"]
         [element/text-field (conj db-path :source-creator-name) "Creator name"]
