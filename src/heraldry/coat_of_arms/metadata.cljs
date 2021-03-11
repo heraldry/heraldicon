9aec456377491528e5392bebc6c96655cec54eba
Oliver Runge <oliver.runge@gmail.com>
2021-03-11 12:40:37 +0100

    Refactor licensing/attribution options
    Foremost, move them into a separate module. Then add CC license
    versioning and the NonCommercial version of the license.

--- src/heraldry/coat_of_arms/metadata.cljs
+++ src/heraldry/coat_of_arms/metadata.cljs
@@ -1,14 +1,12 @@
-(ns heraldry.coat-of-arms.metadata)
+(ns heraldry.coat-of-arms.metadata
+  (:require [heraldry.license :as license]))
 
 (defn attribution [title creator creator-url url
-                   {:keys [license nature
+                   {:keys [license license-version nature
                            source-name source-link
+                           source-license source-license-version
                            source-creator-name source-creator-link]}]
-  (let [license-url (cond
-                      (= license :cc-attribution)             "https://creativecommons.org/licenses/by/4.0"
-                      (= license :cc-attribution-share-alike) "https://creativecommons.org/licenses/by-sa/4.0"
-                      (= license :public-domain)              "https://creativecommons.org/publicdomain/mark/1.0/"
-                      :else                                   "private")]
+  (let [license-url (license/url license license-version)]
     [:metadata {:xmlns:rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
                 :xmlns:cc  "http://creativecommons.org/ns#"
                 :xmlns:dc  "http://purl.org/dc/elements/1.1/"}
@@ -20,30 +18,35 @@
        [:cc:attributionName (str title " by " creator)]
        [:cc:attributionURL {:rdf:resource creator-url}]]
       [:cc:License {:rdf:about license-url}
-       (cond
-         (= license :cc-attribution)             [:<>
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]
-                                                  [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Notice"}]
-                                                  [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Attribution"}]]
-         (= license :cc-attribution-share-alike) [:<>
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]
-                                                  [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Notice"}]
-                                                  [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Attribution"}]
-                                                  [:cc:requires {:rdf:resource "http://creativecommons.org/ns#ShareAlike"}]]
-         (= license :public-domain)              [:<>
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
-                                                  [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]])]]
+       (case license
+         :cc-attribution                            [:<>
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Notice"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Attribution"}]]
+         :cc-attribution-share-alike                [:<>
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Notice"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Attribution"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#ShareAlike"}]]
+         :cc-attribution-non-commercial-share-alike [:<>
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Notice"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#Attribution"}]
+                                                     [:cc:requires {:rdf:resource "http://creativecommons.org/ns#ShareAlike"}]
+                                                     [:cc:prohibits {:rdf:resource "http://creativecommons.org/ns#CommercialUse"}]]
+         :publjc-domain                             [:<>
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Reproduction"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#DerivativeWorks"}]
+                                                     [:cc:permits {:rdf:resource "http://creativecommons.org/ns#Distribution"}]]
+         nil)]]
      (when (= nature :derivative)
-       (let [source-license-url (cond
-                                  (= license :cc-attribution)             "https://creativecommons.org/licenses/by/4.0"
-                                  (= license :cc-attribution-share-alike) "https://creativecommons.org/licenses/by-sa/4.0"
-                                  (= license :public-domain)              "https://creativecommons.org/publicdomain/mark/1.0/"
-                                  :else                                   "private")]
+       (let [source-license-url (license/url source-license source-license-version)]
          [:rdf:RDF {:rdf:about source-link}
           [:rdf:Description {:dc:title   source-name
                              :dc:creator source-creator-name}]
