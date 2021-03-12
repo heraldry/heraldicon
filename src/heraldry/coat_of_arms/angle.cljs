28e76201f83df7ee323c420086cd1e5a95eac84a
Oliver Runge <oliver.runge@gmail.com>
2021-03-12 16:36:02 +0100

    Add position alignment
    This allows aligning a band of a certain width to align with
    origin/anchor either in the middle, its left edge, or right edge in
    direction of origin -> anchor.

--- src/heraldry/coat_of_arms/angle.cljs
+++ src/heraldry/coat_of_arms/angle.cljs
@@ -1,5 +1,6 @@
 (ns heraldry.coat-of-arms.angle
-  (:require [heraldry.coat-of-arms.position :as position]
+  (:require [clojure.set :as set]
+            [heraldry.coat-of-arms.position :as position]
             [heraldry.coat-of-arms.vector :as v]))
 
 (defn angle-to-point [p1 p2]
@@ -28,7 +29,31 @@
          (-> dir :y Math/abs))))
 
 (defn calculate-origin-and-anchor [environment origin anchor width base-angle]
-  (let [real-origin (position/calculate origin environment)
-        real-anchor (position/calculate-anchor anchor environment real-origin base-angle)]
+  (let [target-origin (position/calculate origin environment)
+        target-anchor (position/calculate-anchor anchor environment target-origin base-angle)
+        origin-align (:alignment origin)
+        anchor-align (if (-> anchor :point (= :angle))
+                       (:alignment origin)
+                       (:alignment anchor))
+        r (/ width 2)
+        alignments (set [origin-align anchor-align])
+        outer-tangent? (or (set/subset? alignments #{:middle :left})
+                           (set/subset? alignments #{:middle :right}))
+        [real-origin real-anchor] (if outer-tangent?
+                                    (v/outer-tangent-between-circles target-origin (case origin-align
+                                                                                     :middle 0
+                                                                                     r)
+                                                                     target-anchor (case anchor-align
+                                                                                     :middle 0
+                                                                                     r)
+                                                                     (or (:left alignments)
+                                                                         (:right alignments)))
+                                    (v/inner-tangent-between-circles target-origin (case origin-align
+                                                                                     :middle 0
+                                                                                     r)
+                                                                     target-anchor (case anchor-align
+                                                                                     :middle 0
+                                                                                     r)
+                                                                     anchor-align))]
     {:real-origin real-origin
      :real-anchor real-anchor}))
