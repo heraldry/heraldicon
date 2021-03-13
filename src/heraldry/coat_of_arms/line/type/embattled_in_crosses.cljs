1516f2d60bfa0d03bb146124c3d36334305b28eb
Oliver Runge <oliver.runge@gmail.com>
2021-03-13 22:40:49 +0100

    Add embattled-in-crosses line style

---
+++ src/heraldry/coat_of_arms/line/type/embattled_in_crosses.cljs
@@ -0,0 +1,30 @@
+(ns heraldry.coat-of-arms.line.type.embattled-in-crosses
+  (:require [heraldry.util :as util]))
+
+(defn pattern
+  {:display-name "Embattled in crosses"
+   :value        :embattled-in-crosses}
+  [{:keys [height
+           eccentricity
+           width]}
+   _line-options]
+  (let [l (-> width (/ 4) (* (util/map-to-interval eccentricity 0.6 1.4)))
+        t (-> width (/ 2) (- l))]
+    ["l"
+     [(- (/ l 2)) 0]
+     [0 (- (* t height))]
+     [l 0]
+     [0 (- (* l height))]
+     [t 0]
+     [0 (* l height)]
+     [l 0]
+     [0 (* t height)]
+     [(- l) 0]
+     [0 (* t height)]
+     [l 0]
+     [0 (* l height)]
+     [t 0]
+     [0 (- (* l height))]
+     [l 0]
+     [0 (- (* t height))]
+     [(- (/ l 2)) 0]]))
