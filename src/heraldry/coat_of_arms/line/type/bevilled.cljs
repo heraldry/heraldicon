65818a91f8684e945364241244f61dfc3f423175
Oliver Runge <oliver.runge@gmail.com>
2021-03-26 15:52:07 +0100

    Add bevilled line-style
    This is the first line style that is not pattern based. It requires the
    knowledge of the visible section of the line, other line styles will use
    the same structure.

---
+++ src/heraldry/coat_of_arms/line/type/bevilled.cljs
@@ -0,0 +1,28 @@
+(ns heraldry.coat-of-arms.line.type.bevilled)
+
+(defn full
+  {:display-name "Bevilled"
+   :value :bevilled
+   :full? true}
+  [{:keys [height width
+           eccentricity] :as _line-data}
+   length
+   {:keys [real-start real-end] :as _line-options}]
+  (let [real-start (or real-start 0)
+        real-end (or real-end length)
+        relevant-length (- real-end real-start)
+        half-width (/ width 2)
+        height (* half-width height)
+        pos-x (-> relevant-length
+                  (- width)
+                  (* eccentricity)
+                  (+ half-width))]
+    ["h" real-start
+     "h" (+ pos-x
+            half-width)
+     "l" [(- width) height]
+     "h" (-> relevant-length
+             (- pos-x)
+             (+ half-width))
+     "h" (- length real-end)
+     "v" (- height)]))
