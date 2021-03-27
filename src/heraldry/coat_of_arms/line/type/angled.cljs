7fa9be355a41548a7c763dd428adbe85436855f5
Oliver Runge <oliver.runge@gmail.com>
2021-03-27 18:38:57 +0100

    Add angled line-style

---
+++ src/heraldry/coat_of_arms/line/type/angled.cljs
@@ -0,0 +1,21 @@
+(ns heraldry.coat-of-arms.line.type.angled)
+
+(defn full
+  {:display-name "Angled"
+   :value        :angled
+   :full? true}
+  [{:keys [height eccentricity] :as _line-data}
+   length
+   {:keys [real-start real-end] :as _line-options}]
+  (let [real-start (or real-start 0)
+        real-end (or real-end length)
+        relevant-length (- real-end real-start)
+        pos-x (-> relevant-length
+                  (* eccentricity))]
+    ["h" real-start
+     "h" pos-x
+     "v" height
+     "h" (- relevant-length pos-x)
+     "h" (- length real-end)
+     "v" (- height)]))
+
