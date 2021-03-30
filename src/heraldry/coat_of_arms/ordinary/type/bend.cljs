5a73c368585150fc930e3520a9600d33ac795d55
Oliver Runge <oliver.runge@gmail.com>
2021-03-30 20:06:58 +0200

    Make bend and bend-sinister ordinaries work with full line styles
    Also rewrite them entirely to not be rotated fess bands anymore. This is
    necessary to find the intersections with the environment.
    This also requires a change to the transform passed down the field, as
    it must not transform the selection overlay.
    
    Finally, the environment intersections now more correctly find all
    intersections without the ray approach, the first and last then should
    be the outer intersections the code needs.

--- src/heraldry/coat_of_arms/ordinary/type/bend.cljs
+++ src/heraldry/coat_of_arms/ordinary/type/bend.cljs
@@ -18,9 +18,6 @@
         {:keys [size]} geometry
         opposite-line (ordinary-options/sanitize-opposite-line ordinary line)
         points (:points environment)
-        top-left (:top-left points)
-        left (:left points)
-        right (:right points)
         top (:top points)
         bottom (:bottom points)
         height (:height environment)
@@ -36,32 +33,51 @@
         center-point (v/line-intersection origin-point anchor-point
                                           top bottom)
         direction (v/- anchor-point origin-point)
-        direction (v/v (-> direction :x Math/abs)
-                       (-> direction :y Math/abs))
+        direction (-> (v/v (-> direction :x Math/abs)
+                           (-> direction :y Math/abs))
+                      v/normal)
         direction-orthogonal (v/orthogonal direction)
-        diagonal-start (v/project-x center-point (v/dot direction (v/v -1 -1)) (:x left))
-        diagonal-end (v/project-x center-point (v/dot direction (v/v 1 1)) (:x right))
-        angle (angle/angle-to-point diagonal-start diagonal-end)
-        required-half-length (v/distance-point-to-line top-left center-point (v/+ center-point direction-orthogonal))
-        bend-length (* required-half-length 2)
-        line-length (-> diagonal-end
-                        (v/- diagonal-start)
+        [middle-real-start
+         middle-real-end] (v/environment-intersections
+                           origin-point
+                           (v/+ origin-point direction)
+                           environment)
+        band-length (-> (v/- middle-real-start center-point)
                         v/abs
-                        (* 4))
-        period (-> line
-                   :width
-                   (or 1))
-        offset (-> period
-                   (* (-> line-length
-                          (/ 4)
-                          (/ period)
-                          Math/ceil
-                          inc))
-                   -)
-        row1 (- (/ band-height 2))
-        row2 (+ row1 band-height)
-        first-left (v/v offset row1)
-        second-right (v/v (+ offset line-length) row2)
+                        (* 2))
+        middle-start (-> direction
+                         (v/* -30)
+                         (v/+ middle-real-start))
+        middle-end (-> direction
+                       (v/* 30)
+                       (v/+ middle-real-end))
+        width-offset (-> direction-orthogonal
+                         (v/* band-height)
+                         (v// 2))
+        ordinary-top-left (v/+ middle-real-start width-offset)
+        first-start (v/+ middle-start width-offset)
+        first-end (v/+ middle-end width-offset)
+        second-start (v/- middle-start width-offset)
+        second-end (v/- middle-end width-offset)
+        [first-real-start
+         first-real-end] (v/environment-intersections
+                          first-start
+                          first-end
+                          environment)
+        [second-real-start
+         second-real-end] (v/environment-intersections
+                           second-start
+                           second-end
+                           environment)
+        real-start (min (-> (v/- first-real-start first-start)
+                            (v/abs))
+                        (-> (v/- second-real-start second-start)
+                            (v/abs)))
+        real-end (max (-> (v/- first-real-start first-start)
+                          (v/abs))
+                      (-> (v/- second-real-end second-start)
+                          (v/abs)))
+        angle (v/angle-to-point middle-start middle-end)
         line (-> line
                  (update-in [:fimbriation :thickness-1] (util/percent-of height))
                  (update-in [:fimbriation :thickness-2] (util/percent-of height)))
@@ -70,45 +86,54 @@
                           (update-in [:fimbriation :thickness-2] (util/percent-of height)))
         {line-one :line
          line-one-start :line-start
-         :as line-one-data} (line/create line
-                                         line-length
-                                         :render-options render-options)
+         :as line-one-data} (line/create2 line
+                                          first-start
+                                          first-end
+                                          :real-start real-start
+                                          :real-end real-end
+                                          :render-options render-options
+                                          :environment environment)
         {line-reversed :line
          line-reversed-start :line-start
-         :as line-reversed-data} (line/create opposite-line
-                                              line-length
-                                              :reversed? true
-                                              :angle 180
-                                              :render-options render-options)
-        parts [[["M" (v/+ first-left
+         :as line-reversed-data} (line/create2 opposite-line
+                                               second-start
+                                               second-end
+                                               :reversed? true
+                                               :real-start real-start
+                                               :real-end real-end
+                                               :render-options render-options
+                                               :environment environment)
+        counterchanged? (counterchange/counterchangable? field parent)
+        use-parent-environment? (or counterchanged?
+                                    (:inherit-environment? field))
+        parts [[["M" (v/+ first-start
                           line-one-start)
                  (svg/stitch line-one)
-                 "L" (v/+ second-right
+                 "L" (v/+ second-end
                           line-reversed-start)
                  (svg/stitch line-reversed)
-                 "L" (v/+ first-left
+                 "L" (v/+ first-start
                           line-one-start)
                  "z"]
-                [(v/v 0 row1) (v/v bend-length row2)]]]
-        counterchanged? (counterchange/counterchangable? field parent)
+                (if use-parent-environment?
+                  [first-real-start first-real-end
+                   second-real-start second-real-end]
+                  [(v/v 0 0)
+                   (v/v band-length band-height)])]]
         field (if counterchanged?
                 (counterchange/counterchange-field field parent)
                 field)
         outline? (or (:outline? render-options)
                      (:outline? hints))]
-    [:g {:transform (str "translate(" (:x center-point) "," (:y center-point) ")"
-                         "rotate(" angle ")"
-                         "translate(" (- required-half-length) "," 0 ")")}
+    [:<>
      [division-shared/make-division
       :ordinary-fess [field] parts
       [:all]
       environment
       ordinary
       (-> context
-          (assoc :transform (when (or counterchanged?
-                                      (:inherit-environment? field))
-                              (str
-                               "rotate(" (- angle) ") "
-                               "translate(" (-> diagonal-start :x -) "," (-> diagonal-start :y -) ")"))))]
-     (line/render line [line-one-data] first-left outline? render-options)
-     (line/render opposite-line [line-reversed-data] second-right outline? render-options)]))
+          (assoc :transform (when (not use-parent-environment?)
+                              (str "translate(" (v/->str ordinary-top-left) ")"
+                                   "rotate(" angle ")"))))]
+     (line/render line [line-one-data] first-start outline? render-options)
+     (line/render opposite-line [line-reversed-data] second-end outline? render-options)]))
