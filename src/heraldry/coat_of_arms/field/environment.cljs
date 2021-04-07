1e2488b7b8c0ea391afa175fcc382ef268dc287a
Oliver Runge <oliver.runge@gmail.com>
2021-04-07 19:15:25 +0200

    Move coat-of-arms.field-environment into coat-of-arms.field.environment

--- src/heraldry/coat_of_arms/field/environment.cljs
+++ src/heraldry/coat_of_arms/field/environment.cljs
@@ -1,4 +1,4 @@
-(ns heraldry.coat-of-arms.field-environment
+(ns heraldry.coat-of-arms.field.environment
   (:require ["svgpath" :as svgpath]
             [heraldry.coat-of-arms.svg :as svg]
             [heraldry.coat-of-arms.vector :as v]))
