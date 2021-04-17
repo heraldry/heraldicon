fec14d00441c17ce7ca30b975f4f4b170e71ab3e
Oliver Runge <oliver.runge@gmail.com>
2021-04-17 23:22:00 +0200

    Add wolf-toothed line style

---
+++ src/heraldry/coat_of_arms/line/type/wolf_toothed.cljs
@@ -0,0 +1,22 @@
+(ns heraldry.coat-of-arms.line.type.wolf-toothed
+  (:require [heraldry.util :as util]))
+
+(defn pattern
+  {:display-name "Wolf toothed"
+   :value        :wolf-toothed}
+  [{:keys [eccentricity
+           height
+           width]}
+   _line-options]
+  (let [half-width (/ width 2)
+        height     (* width height)
+        rf         (util/map-to-interval eccentricity 1.5 1.2)
+        r          (* half-width rf rf)
+        shift      (* (util/map-to-interval eccentricity -0.5 0.5)
+                      half-width)]
+    {:pattern ["a" (* r 0.9) (* r 0.9) 0 0 0 [(- half-width shift) (- height)]
+               "a" r r 0 0 1 [(+ half-width
+                                 shift) height]]
+     :min     (- height)
+     :max     0}))
+
