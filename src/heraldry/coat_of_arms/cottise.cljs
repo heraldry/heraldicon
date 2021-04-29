c3717a84fd8a8b436566d26230c1fabd3ca53c77
Oliver Runge <oliver.runge@gmail.com>
2021-04-29 20:44:08 +0200

    Add first draft of cottising options and form (WIP)

---
+++ src/heraldry/coat_of_arms/cottise.cljs
@@ -0,0 +1,23 @@
+(ns heraldry.coat-of-arms.cottise
+  (:require [heraldry.coat-of-arms.line.core :as line]
+            [heraldry.coat-of-arms.options :as options]
+            [heraldry.coat-of-arms.tincture.core :as tincture]))
+
+(def base-options
+  {:line line/default-options
+   :opposite-line line/default-options
+   :distance {:type :range
+              :min 1
+              :max 10
+              :default 4}
+   :thickness {:type :range
+               :min 1
+               :max 10
+               :default 4}
+   :tincture {:type :choice
+              :choices (-> [["None" :none]]
+                           (into tincture/choices))
+              :default :none}})
+
+(def options
+  (assoc base-options :cottise base-options))
