c12680c924c646a76c34d4ed9267af6805332490
Oliver Runge <oliver.runge@gmail.com>
2021-04-30 11:21:40 +0200

    Use yet another different approach to cottising options

--- src/heraldry/coat_of_arms/cottising.cljs
+++ src/heraldry/coat_of_arms/cottising.cljs
@@ -1,10 +1,11 @@
 (ns heraldry.coat-of-arms.cottising
   (:require [heraldry.coat-of-arms.line.core :as line]
-            [heraldry.coat-of-arms.options :as options]
             [heraldry.coat-of-arms.tincture.core :as tincture]))
 
 (def cottise-options
-  {:line          line/default-options
+  {:enabled?      {:type    :boolean
+                   :default false}
+   :line          line/default-options
    :opposite-line line/default-options
    :distance      {:type    :range
                    :min     1
@@ -19,17 +20,11 @@
                                 (into tincture/choices))
                    :default :none}})
 
-(def cottising-choices
-  [["None" :none]
-   ["Single" :single]
-   ["Double" :double]])
-
 (def options
-  {:mode               {:type    :choice
-                        :choices cottising-choices
-                        :default :none}
-   :cottise-1          cottise-options
+  {:cottise-1          cottise-options
    :cottise-opposite-1 cottise-options
    :cottise-2          cottise-options
    :cottise-opposite-2 cottise-options})
+
+(defn render [{:keys [] :as cottise} parent environment {:keys [render-options] :as context}])
 
--- src/heraldry/coat_of_arms/ordinary/type/fess.cljs
+++ src/heraldry/coat_of_arms/ordinary/type/fess.cljs
@@ -15,7 +15,7 @@
    :value        :heraldry.ordinary.type/fess}
   [{:keys [field hints] :as ordinary} parent environment {:keys [render-options] :as context}]
   (let [{:keys [line origin geometry
-                cottise opposite-cottise]}       (options/sanitize ordinary (ordinary-options/options ordinary))
+                cottising]}                      (options/sanitize ordinary (ordinary-options/options ordinary))
         {:keys [size]}                           geometry
         opposite-line                            (ordinary-options/sanitize-opposite-line ordinary line)
         points                                   (:points environment)
@@ -94,7 +94,7 @@
                                                    field)
         outline?                                 (or (:outline? render-options)
                                                      (:outline? hints))]
-    (js/console.log "cot" cottise)
+    (js/console.log "toc" cottising)
     [:<>
      [field-shared/make-subfields
       :ordinary-fess [field] parts
