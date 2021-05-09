8002b10a998283af5828d296e8c1e7e9d4e88c08
Oliver Runge <oliver.runge@gmail.com>
2021-05-09 15:12:00 +0200

    Fix opposite cottising line inheritance swap
    Passing along the fully sanitized line/opposite-line fixes this issue.
    It also solves some inconsistencies with the opposite cottising of
    chevrons.

--- src/heraldry/coat_of_arms/ordinary/type/bend_sinister.cljs
+++ src/heraldry/coat_of_arms/ordinary/type/bend_sinister.cljs
@@ -204,6 +204,12 @@
               (assoc :override-real-end real-end))]))
      (when (:enabled? cottise-opposite-1)
        (let [cottise-opposite-1-data (options/sanitize cottise-opposite-1 cottising/cottise-options)
+             bend-sinister-base {:type :heraldry.ordinary.type/bend-sinister
+                                 :line (:line cottise-opposite-1)
+                                 :opposite-line (:opposite-line cottise-opposite-1)}
+             bend-sinister-options (ordinary-options/options bend-sinister-base)
+             {:keys [line]} (options/sanitize bend-sinister-base bend-sinister-options)
+             opposite-line (ordinary-options/sanitize-opposite-line bend-sinister-base line)
              dist (-> (+ (:distance cottise-opposite-1-data))
                       (+ (/ (:thickness cottise-opposite-1-data) 2))
                       (/ 100)
@@ -228,8 +234,9 @@
                          :angle (- angle)}]
          [render (-> ordinary
                      (assoc :cottising {:cottise-opposite-1 cottise-opposite-2})
-                     (assoc :line (:opposite-line cottise-opposite-1))
-                     (assoc :opposite-line (:line cottise-opposite-1))
+                     ;; swap line/opposite-line because the cottise fess is upside down
+                     (assoc :line opposite-line)
+                     (assoc :opposite-line line)
                      (assoc :field (:field cottise-opposite-1))
                      (assoc-in [:geometry :size] (:thickness cottise-opposite-1-data))
                      (assoc :origin new-origin)
