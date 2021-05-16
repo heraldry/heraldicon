930f3a6761ac160f111059cd47308f266f7a359f
Oliver Runge <oliver.runge@gmail.com>
2021-05-16 17:06:09 +0200

    Move header into own namespace

---
+++ src/heraldry/frontend/header.cljs
@@ -0,0 +1,36 @@
+(ns heraldry.frontend.header
+  (:require [heraldry.frontend.route :as route]
+            [heraldry.frontend.user :as user]))
+
+(defn view []
+  (let [user-data (user/data)]
+    [:div.header
+     [:div.home-menu.pure-menu.pure-menu-horizontal.pure-menu-fixed
+      [:span [route/link {:to :home
+                          :class "pure-menu-heading pure-float-right"
+                          :style {:padding-right "5px"}} "Heraldry"]
+       [:sup {:style {:color "#d82"}} "beta"]]
+      [:ul.pure-menu-list
+       [route/nav-link {:to :home} "Home"]
+       #_[route/nav-link {:to :collections} "Collections"]
+       [route/nav-link {:to :arms} "Arms"]
+       [route/nav-link {:to :charges} "Charges"]
+       [route/nav-link {:to :about} "About"]
+       [:span.horizontal-spacer {:style {:width "5em"}}]
+       (if (:logged-in? user-data)
+         [:li.pure-menu-item.pure-menu-has-children.pure-menu-allow-hover
+          {:style {:min-width "6em"}}
+          [:<>
+           [:a.pure-menu-link {:href "#"} (str "@" (:username user-data))]
+           [:ul.pure-menu-children
+            [route/nav-link {:to :account} "Account"]
+            [:li.pure-menu-item
+             [:a.pure-menu-link {:href "#"
+                                 :on-click #(user/logout)} "Logout"]]]]]
+         [:<>
+          [:li.pure-menu-item
+           [:a.pure-menu-link {:href "#"
+                               :on-click #(user/login-modal)} "Login"]]
+          [:li.pure-menu-item
+           [:a.pure-menu-link {:href "#"
+                               :on-click #(user/sign-up-modal)} "Register"]]])]]]))
--- src/heraldry/frontend/main.cljs
+++ src/heraldry/frontend/main.cljs
@@ -1,47 +1,15 @@
 (ns heraldry.frontend.main
   (:require [devtools.core :as devtools]
+            [heraldry.frontend.header :as header]
             [heraldry.frontend.modal :as modal]
             [heraldry.frontend.route :as route]
             [heraldry.frontend.user :as user]
             [re-frame.core :as rf]
             [reagent.dom :as r]))
 
-(defn header []
-  (let [user-data (user/data)]
-    [:div.header
-     [:div.home-menu.pure-menu.pure-menu-horizontal.pure-menu-fixed
-      [:span [route/link {:to :home
-                          :class "pure-menu-heading pure-float-right"
-                          :style {:padding-right "5px"}} "Heraldry"]
-       [:sup {:style {:color "#d82"}} "beta"]]
-      [:ul.pure-menu-list
-       [route/nav-link {:to :home} "Home"]
-       #_[route/nav-link {:to :collections} "Collections"]
-       [route/nav-link {:to :arms} "Arms"]
-       [route/nav-link {:to :charges} "Charges"]
-       [route/nav-link {:to :about} "About"]
-       [:span.horizontal-spacer {:style {:width "5em"}}]
-       (if (:logged-in? user-data)
-         [:li.pure-menu-item.pure-menu-has-children.pure-menu-allow-hover
-          {:style {:min-width "6em"}}
-          [:<>
-           [:a.pure-menu-link {:href "#"} (str "@" (:username user-data))]
-           [:ul.pure-menu-children
-            [route/nav-link {:to :account} "Account"]
-            [:li.pure-menu-item
-             [:a.pure-menu-link {:href "#"
-                                 :on-click #(user/logout)} "Logout"]]]]]
-         [:<>
-          [:li.pure-menu-item
-           [:a.pure-menu-link {:href "#"
-                               :on-click #(user/login-modal)} "Login"]]
-          [:li.pure-menu-item
-           [:a.pure-menu-link {:href "#"
-                               :on-click #(user/sign-up-modal)} "Register"]]])]]]))
-
 (defn app []
   [:<>
-   [header]
+   [header/view]
    [:div.main-content
     (if-let [view (route/view)]
       view
