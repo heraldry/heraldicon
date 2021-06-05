843f8189792738f0b5f45cb767f6b1c77873e428
Oliver Runge <oliver.runge@gmail.com>
2021-06-05 09:55:52 +0200

    Separate collection element from arms reference
    Also add field for the name of collection back.

--- src/heraldry/frontend/form/arms_reference.cljs
+++ src/heraldry/frontend/form/arms_reference.cljs
@@ -5,19 +5,14 @@
             [heraldry.frontend.user :as user]
             [re-frame.core :as rf]))
 
-(defn link-to-arms [db-path]
+(defn link-to-arms [path]
   (fn [arms]
-    [:a {:on-click #(rf/dispatch [:set db-path (select-keys arms [:id :version])])}
+    [:a {:on-click #(rf/dispatch [:set path (select-keys arms [:id :version])])}
      (:name arms)]))
 
-(defn form [db-path]
-  (let [index (last db-path)
-        data @(rf/subscribe [:get db-path])
-        title (-> data
-                  :name
-                  (or "None"))
-        {arms-id :id
-         version :version} (:reference data)
+(defn form [path]
+  (let [{arms-id :id
+         version :version} @(rf/subscribe [:get path])
         [_status arms-data] (when arms-id
                               (state/async-fetch-data
                                [:arms-references arms-id version]
@@ -26,12 +21,8 @@
         arms-title (-> arms-data
                        :name
                        (or "None"))]
-    [element/component db-path :arms-reference (str (inc index) ": " title) nil
-     [:div.setting {:style {:margin-bottom "1em"}}
-      [element/text-field (conj db-path :name) "Name" :style {:width "19em"}]]
-
-     [:div.setting {:style {:margin-bottom "0.5em"}}
-      [:label "Arms"]
-      " "
-      [element/submenu (conj db-path :reference) "Select Arms" arms-title {}
-       [arms-select/list-arms-for-user (:user-id (user/data)) (link-to-arms (conj db-path :reference))]]]]))
+    [:div.setting {:style {:margin-bottom "0.5em"}}
+     [:label "Arms"]
+     " "
+     [element/submenu path "Select Arms" arms-title {}
+      [arms-select/list-arms-for-user (:user-id (user/data)) (link-to-arms path)]]]))
--- src/heraldry/frontend/form/collection.cljs
+++ src/heraldry/frontend/form/collection.cljs
@@ -3,10 +3,25 @@
             [heraldry.frontend.form.element :as element]
             [re-frame.core :as rf]))
 
+(defn collection-element [path]
+  (let [index (last path)
+        data @(rf/subscribe [:get path])
+        element-name (-> data
+                         :name
+                         (or "None"))]
+    [element/component path :collection-element (str (inc index) ": " element-name) nil
+     [:div.setting {:style {:margin-bottom "1em"}}
+      [element/text-field (conj path :name) "Name" :style {:width "19em"}]]
+
+     [arms-reference/form (conj path :reference)]]))
+
 (defn form [path selected-arms-path]
   (let [data @(rf/subscribe [:get path])
         selected-arms @(rf/subscribe [:get selected-arms-path])]
     [element/component path :collection (:name data) "Collection"
+     [:div.setting {:style {:margin-bottom "1em"}}
+      [element/text-field (conj path :name) "Name" :style {:width "19em"}]]
+
      [:div.settings
       [element/range-input (conj path :collection :num-columns) "Columns"
        1
@@ -15,4 +30,4 @@
 
      (when selected-arms
        [:div.selected-arms
-        [arms-reference/form (conj path :collection :elements selected-arms)]])]))
+        [collection-element (conj path :collection :elements selected-arms)]])]))
