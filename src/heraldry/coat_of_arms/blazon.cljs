d24a6b027e0334ff9c6eb0ac6583b160f044e424
Oliver Runge <oliver.runge@gmail.com>
2021-06-12 17:09:19 +0200

    Improve charge-group blazoning

--- src/heraldry/coat_of_arms/blazon.cljs
+++ src/heraldry/coat_of_arms/blazon.cljs
@@ -15,14 +15,18 @@
                   "a")]
     (util/combine " " [article rest])))
 
-(defn encode-charge [{:keys [type attitude field tincture variant]}]
+(defn encode-charge [{:keys [type attitude field tincture variant]}
+                     & {:keys [part-of-charge-group? pluralize?]}]
   (let [charge-data (when variant
                       (charge/fetch-charge-data variant))
         fixed-tincture (-> charge-data
                            :fixed-tincture
                            (or :none)
                            (#(when (not= :none %) %)))]
-    (util/combine " " ["a" (util/translate type)
+    (util/combine " " [(when-not part-of-charge-group?
+                         "a") (str (util/translate type)
+                                   (when pluralize?
+                                     "s"))
                        (util/translate attitude)
                        (if fixed-tincture
                          (util/translate fixed-tincture)
@@ -36,6 +40,30 @@
                                                       keys
                                                       sort)))])))
 
+(defn encode-charge-group [charge-group]
+  (let [used-indices (->> charge-group
+                          (tree-seq #(or (map? %)
+                                         (vector? %)
+                                         (seq? %))
+                                    (fn [data]
+                                      (cond
+                                        (and (map? data)
+                                             (:slots data)) [data]
+                                        (map? data) (:strips data))
+                                      (seq data)))
+                          (filter :slots)
+                          (mapcat :slots)
+                          (filter identity)
+                          (group-by identity))
+        charges (:charges charge-group)]
+    (util/combine
+     ", "
+     (map (fn [[k v]]
+            (str (count v) " "
+                 (encode-charge (get charges k)
+                                :part-of-charge-group? true
+                                :pluralize? (> (count v) 1)))) used-indices))))
+
 (defn encode-semy [component]
   (str "semy of " (-> component :charge :type util/translate)))
 
@@ -43,7 +71,7 @@
   (case (-> component :type namespace)
     "heraldry.ordinary.type" (encode-ordinary component)
     "heraldry.charge.type" (encode-charge component)
-    "heraldry.charge-group.type" (str "charge group of " (encode-charge (-> component :charges first)))
+    "heraldry.charge-group.type" (encode-charge-group component)
     "heraldry.component" (encode-semy component)))
 
 (defn encode-field [{:keys [components counterchanged?] :as field} & {:keys [root?]}]
