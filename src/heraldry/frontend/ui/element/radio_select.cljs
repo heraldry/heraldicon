d8f2c1d2e00284830217416a5c9d5a08e4bb1792
Oliver Runge <oliver.runge@gmail.com>
2021-06-21 23:41:09 +0200

    Move radio-select choices into own component as well
    This avoids re-rendering them, now only the two choices that actually
    changed will be re-rendered.

--- src/heraldry/frontend/ui/element/radio_select.cljs
+++ src/heraldry/frontend/ui/element/radio_select.cljs
@@ -3,6 +3,20 @@
             [heraldry.util :as util]
             [re-frame.core :as rf]))
 
+(defn radio-choice [path key display-name & {:keys [selected? on-change]}]
+  (let [component-id (util/id "radio")]
+    [:<>
+     [:input {:id component-id
+              :type "radio"
+              :value (util/keyword->str key)
+              :checked selected?
+              :on-change #(let [value (keyword (-> % .-target .-value))]
+                            (if on-change
+                              (on-change value)
+                              (rf/dispatch [:set path value])))}]
+     [:label {:for component-id
+              :style {:margin-right "10px"}} display-name]]))
+
 (defn radio-select [path choices & {:keys [default label on-change]}]
   [:div.ui-setting
    (when label
@@ -11,19 +25,10 @@
     (let [current-value (or @(rf/subscribe [:get-value path])
                             default)]
       (for [[display-name key] choices]
-        (let [component-id (util/id "radio")]
-          ^{:key key}
-          [:<>
-           [:input {:id component-id
-                    :type "radio"
-                    :value (util/keyword->str key)
-                    :checked (= key current-value)
-                    :on-change #(let [value (keyword (-> % .-target .-value))]
-                                  (if on-change
-                                    (on-change value)
-                                    (rf/dispatch [:set path value])))}]
-           [:label {:for component-id
-                    :style {:margin-right "10px"}} display-name]])))]])
+        ^{:key key}
+        [radio-choice path key display-name
+         :selected? (= key current-value)
+         :on-change on-change]))]])
 
 (defmethod interface/form-element :radio-select [path {:keys [ui default choices] :as option}]
   (when option
