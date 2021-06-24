d0564d39a6d9830de11448f665b6d232562ae1cd
Oliver Runge <oliver.runge@gmail.com>
2021-06-24 22:51:27 +0200

    Improve line and fimbriation titles in UI

--- src/heraldry/frontend/ui/element/fimbriation.cljs
+++ src/heraldry/frontend/ui/element/fimbriation.cljs
@@ -1,14 +1,30 @@
 (ns heraldry.frontend.ui.element.fimbriation
-  (:require [heraldry.frontend.ui.element.submenu :as submenu]
-            [heraldry.frontend.ui.interface :as interface]))
+  (:require [heraldry.coat-of-arms.line.fimbriation :as fimbriation]
+            [heraldry.coat-of-arms.options :as options]
+            [heraldry.frontend.ui.element.submenu :as submenu]
+            [heraldry.frontend.ui.interface :as interface]
+            [heraldry.frontend.util :as util]
+            [re-frame.core :as rf]))
+
+(rf/reg-sub :fimbriation-title
+  (fn [[_ path] _]
+    (rf/subscribe [:get path]))
+
+  (fn [fimbriation [_ _path]]
+    (let [effective-data (options/sanitize fimbriation (fimbriation/options fimbriation))]
+      (case (:mode effective-data)
+        :none "None"
+        :single (str "Fimbriated " (-> effective-data :tincture-1 util/translate-tincture))
+        :double (str "Fimbriated " (-> effective-data :tincture-1 util/translate-tincture)
+                     " and " (-> effective-data :tincture-2 util/translate-tincture))))))
 
 (defn fimbriation-submenu [path options & {:keys [label]}]
-  (let [representation "fimbriation"]
+  (let [title @(rf/subscribe [:fimbriation-title path])]
     [:div.ui-setting
      (when label
        [:label label])
      [:div.option
-      [submenu/submenu path label representation {:width "30em"}
+      [submenu/submenu path label title {:width "30em"}
        (for [option [:mode
                      :alignment
                      :corner
--- src/heraldry/frontend/ui/element/line.cljs
+++ src/heraldry/frontend/ui/element/line.cljs
@@ -1,14 +1,44 @@
 (ns heraldry.frontend.ui.element.line
-  (:require [heraldry.frontend.ui.element.submenu :as submenu]
-            [heraldry.frontend.ui.interface :as interface]))
+  (:require [clojure.string :as s]
+            [heraldry.coat-of-arms.line.core :as line]
+            [heraldry.coat-of-arms.options :as options]
+            [heraldry.frontend.ui.element.submenu :as submenu]
+            [heraldry.frontend.ui.interface :as interface]
+            [heraldry.frontend.util :as util]
+            [re-frame.core :as rf]))
+
+(rf/reg-sub :line-title
+  (fn [[_ path] _]
+    [(rf/subscribe [:get path])
+     (rf/subscribe [:fimbriation-title (conj path :fimbriation)])])
+
+  (fn [[line fimbriation-title] [_ _path]]
+    (let [effective-data (options/sanitize line (line/options line))]
+      (-> (util/combine
+           ", "
+           [(or (-> effective-data :type line/line-map)
+                "Inherit")
+            (when (and (-> effective-data :offset)
+                       (-> effective-data :offset zero? not))
+              (str "shifted"))
+            (when (and (-> effective-data :spacing)
+                       (-> effective-data :spacing zero? not))
+              (str "spaced"))
+            (when (and (-> effective-data :rotation)
+                       (-> effective-data :rotation zero? not))
+              (str "rotated"))
+            (when (not= fimbriation-title "None")
+              fimbriation-title)])
+          s/lower-case
+          util/upper-case-first))))
 
 (defn line-submenu [path options & {:keys [label]}]
-  (let [representation "line"]
+  (let [title @(rf/subscribe [:line-title path])]
     [:div.ui-setting
      (when label
        [:label label])
      [:div.option
-      [submenu/submenu path label representation {:width "30em"}
+      [submenu/submenu path label title {:width "30em"}
        (for [option [:type
                      :eccentricity
                      :height
