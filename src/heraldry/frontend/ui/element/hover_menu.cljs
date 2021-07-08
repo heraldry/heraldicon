f949157920a6ffaddbc633d72f53e9046c5659c7
Oliver Runge <oliver.runge@gmail.com>
2021-07-08 12:09:41 +0200

    Use mouse-enter and mouse-leave for hover-menu
    Also put them on different elements, avoiding event ping-pong when the
    mouse moves to li elements.

--- src/heraldry/frontend/ui/element/hover_menu.cljs
+++ src/heraldry/frontend/ui/element/hover_menu.cljs
@@ -2,31 +2,39 @@
   (:require [re-frame.core :as rf]))
 
 (defn hover-menu [path title menu trigger-element & {:keys [disabled?]}]
-  [:span.node-icon.ui-hover-menu
-   {:class (when disabled? "disabled")
-    :on-mouse-over #(rf/dispatch [:ui-hover-menu-open path])
-    :on-mouse-out #(rf/dispatch [:ui-hover-menu-close path])}
-   trigger-element
-   [:ul.ui-menu {:style {:padding 0
-                         :font-weight 400
-                         :display (if @(rf/subscribe [:ui-hover-menu-open? path])
-                                    "block"
-                                    "none")
-                         :min-width "7em"}}
-    [:li.ui-menu-header title]
-    (for [{:keys [icon title handler]} menu]
-      (let [handler (when handler
-                      #(do
-                         (rf/dispatch [:ui-hover-menu-close path])
-                         (handler %)))]
-        ^{:key title}
-        [:li.ui-menu-item
-         {:style {:color "#000"}
-          :on-click (when-not disabled? handler)}
-         (when icon
-           [:i.ui-icon {:class icon
-                        :style {:margin-right "5px"
-                                :color (if disabled?
-                                         "#ccc"
-                                         "#777")}}])
-         title]))]])
+  (let [menu-open? @(rf/subscribe [:ui-hover-menu-open? path])]
+    [:span.node-icon.ui-hover-menu
+     {:class (when disabled? "disabled")
+      :on-mouse-enter (when-not disabled?
+                        (fn [event]
+                          (when-not menu-open?
+                            (rf/dispatch [:ui-hover-menu-open path]))
+                          (.stopPropagation event)))}
+     trigger-element
+     [:ul.ui-menu {:style {:padding 0
+                           :font-weight 400
+                           :display (if menu-open?
+                                      "block"
+                                      "none")
+                           :min-width "7em"}
+                   :on-mouse-leave (fn [event]
+                                     (when menu-open?
+                                       (rf/dispatch [:ui-hover-menu-close path]))
+                                     (.stopPropagation event))}
+      [:li.ui-menu-header title]
+      (for [{:keys [icon title handler]} menu]
+        (let [handler (when handler
+                        #(do
+                           (rf/dispatch [:ui-hover-menu-close path])
+                           (handler %)))]
+          ^{:key title}
+          [:li.ui-menu-item
+           {:style {:color "#000"}
+            :on-click (when-not disabled? handler)}
+           (when icon
+             [:i.ui-icon {:class icon
+                          :style {:margin-right "5px"
+                                  :color (if disabled?
+                                           "#ccc"
+                                           "#777")}}])
+           title]))]]))
