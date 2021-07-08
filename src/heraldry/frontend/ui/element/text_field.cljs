30742a338938c20a8799105f98780db10ed14110
Oliver Runge <oliver.runge@gmail.com>
2021-07-08 22:09:21 +0200

    Simplify UI text-field

--- src/heraldry/frontend/ui/element/text_field.cljs
+++ src/heraldry/frontend/ui/element/text_field.cljs
@@ -2,23 +2,24 @@
   (:require [heraldry.frontend.ui.interface :as interface]
             [re-frame.core :as rf]))
 
-(defn text-field [path & {:keys [default label on-change]}]
-  (let [value (or @(rf/subscribe [:get-value path])
-                  default)]
-    [:div.ui-setting
-     (when label
-       [:label label])
-     [:div.option
-      [:input {:type "text"
-               :value value
-               :on-change #(let [value (-> % .-target .-value)]
-                             (if on-change
-                               (on-change value)
-                               (rf/dispatch-sync [:set path value])))}]]]))
+(defn text-field [path & {:keys [on-change]}]
+  (when-let [option @(rf/subscribe [:get-relevant-options path])]
+    (let [{:keys [ui inherited default]} option
+          current-value @(rf/subscribe [:get-value path])
+          value (or current-value
+                    inherited
+                    default)
+          label (:label ui)]
+      [:div.ui-setting
+       (when label
+         [:label label])
+       [:div.option
+        [:input {:type "text"
+                 :value value
+                 :on-change #(let [value (-> % .-target .-value)]
+                               (if on-change
+                                 (on-change value)
+                                 (rf/dispatch-sync [:set path value])))}]]])))
 
 (defmethod interface/form-element :text-field [path _]
-  (when-let [option @(rf/subscribe [:get-relevant-options path])]
-    (let [{:keys [ui default]} option]
-      [text-field path
-       :default default
-       :label (:label ui)])))
+  [text-field path])
