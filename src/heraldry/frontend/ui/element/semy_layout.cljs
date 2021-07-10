37820505e140691738ca05a81a21ecfaf6a66238
Oliver Runge <oliver.runge@gmail.com>
2021-07-10 11:55:34 +0200

    Refactor semy layout submenu link name

--- src/heraldry/frontend/ui/element/semy_layout.cljs
+++ src/heraldry/frontend/ui/element/semy_layout.cljs
@@ -1,40 +1,42 @@
 (ns heraldry.frontend.ui.element.semy-layout
   (:require [heraldry.coat-of-arms.options :as options]
-            [heraldry.coat-of-arms.semy.options :as semy-options]
             [heraldry.frontend.ui.element.submenu :as submenu]
             [heraldry.frontend.ui.interface :as interface]
             [heraldry.frontend.util :as util]
             [re-frame.core :as rf]))
 
-(rf/reg-sub :semy-layout-title
+(rf/reg-sub :semy-layout-submenu-link-name
   (fn [[_ path] _]
-    (rf/subscribe [:get (drop-last path)]))
+    [(rf/subscribe [:get path])
+     (rf/subscribe [:get-relevant-options path])])
 
-  (fn [semy [_ _path]]
-    (let [effective-data (:layout (options/sanitize semy semy-options/default-options))]
-      (util/combine
-       ", "
-       [(str (:num-fields-x effective-data) "x"
-             (:num-fields-y effective-data))
-        (when (or (-> effective-data :offset-x zero? not)
-                  (-> effective-data :offset-y zero? not))
-          (str "shifted"))
-        (when (or (-> effective-data :stretch-x (not= 1))
-                  (-> effective-data :stretch-y (not= 1)))
-          (str "stretched"))
-        (when (-> effective-data :rotation zero? not)
-          (str "rotated"))]))))
+  (fn [[layout options] [_ _path]]
+    (let [sanitized-layout (options/sanitize layout options)
+          main-name (str (:num-fields-x sanitized-layout) "x"
+                         (:num-fields-y sanitized-layout)
+                         " fields")
+          changes [main-name
+                   (when (some #(options/changed? % sanitized-layout options)
+                               [:offset-x :offset-y])
+                     "shifted")
+                   (when (some #(options/changed? % sanitized-layout options)
+                               [:stretch-x :stretch-y])
+                     "stretched")
+                   (when (options/changed? :rotation sanitized-layout options)
+                     "rotated")]]
+      (-> (util/combine ", " changes)
+          util/upper-case-first))))
 
 (defn layout-submenu [path]
   (when-let [options @(rf/subscribe [:get-relevant-options path])]
     (let [{:keys [ui]} options
           label (:label ui)
-          title @(rf/subscribe [:semy-layout-title path])]
+          link-name @(rf/subscribe [:semy-layout-submenu-link-name path])]
       [:div.ui-setting
        (when label
          [:label label])
        [:div.option
-        [submenu/submenu path label title {:width "30em"}
+        [submenu/submenu path label link-name {:width "30em"}
          (for [option [:num-fields-x
                        :num-fields-y
                        :offset-x
