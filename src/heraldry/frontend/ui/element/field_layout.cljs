c824a0a4f2cf041239bae83ce5d03892597f95d8
Oliver Runge <oliver.runge@gmail.com>
2021-07-10 11:48:26 +0200

    Refactor field layout submenu link name

--- src/heraldry/frontend/ui/element/field_layout.cljs
+++ src/heraldry/frontend/ui/element/field_layout.cljs
@@ -1,55 +1,61 @@
 (ns heraldry.frontend.ui.element.field-layout
-  (:require [heraldry.coat-of-arms.field.options :as field-options]
-            [heraldry.coat-of-arms.options :as options]
+  (:require [heraldry.coat-of-arms.options :as options]
             [heraldry.frontend.ui.element.submenu :as submenu]
             [heraldry.frontend.ui.interface :as interface]
             [heraldry.frontend.util :as util]
             [re-frame.core :as rf]))
 
-(rf/reg-sub :field-layout-title
+(rf/reg-sub :field-layout-submenu-link-name
   (fn [[_ path] _]
-    (rf/subscribe [:get (drop-last path)]))
+    [(rf/subscribe [:get (drop-last path)])
+     (rf/subscribe [:get-relevant-options (drop-last path)])])
 
-  (fn [field [_ _path]]
+  (fn [[field options] [_ _path]]
     (let [stripped-field-type (-> field :type name keyword)
-          effective-data (:layout (options/sanitize field (field-options/options field)))]
-      (util/combine
-       ", "
-       [(cond
-          (= stripped-field-type :paly) (str (:num-fields-x effective-data) " fields")
-          (#{:barry
-             :bendy
-             :bendy-sinister} stripped-field-type) (str (:num-fields-y effective-data) " fields")
-          (#{:quarterly
-             :chequy
-             :lozengy
-             :vairy
-             :potenty
-             :papellony
-             :masonry
-             :bendy} stripped-field-type) (str (:num-fields-x effective-data) "x"
-                                               (:num-fields-y effective-data) " fields"))
-        (when (-> effective-data :num-base-fields (not= 2))
-          (str (:num-base-fields effective-data) " base fields"))
-        (when (or (-> effective-data :offset-x zero? not)
-                  (-> effective-data :offset-y zero? not))
-          (str "shifted"))
-        (when (or (-> effective-data :stretch-x (not= 1))
-                  (-> effective-data :stretch-y (not= 1)))
-          (str "stretched"))
-        (when (-> effective-data :rotation zero? not)
-          (str "rotated"))]))))
+          sanitized-field (options/sanitize field options)
+          sanitized-layout (:layout sanitized-field)
+          layout-options (:layout options)
+          main-name (cond
+                      (#{:paly}
+                       stripped-field-type) (str (:num-fields-x sanitized-layout) " fields")
+                      (#{:barry
+                         :bendy
+                         :bendy-sinister}
+                       stripped-field-type) (str (:num-fields-y sanitized-layout) " fields")
+                      (#{:quarterly
+                         :chequy
+                         :lozengy
+                         :vairy
+                         :potenty
+                         :papellony
+                         :masonry
+                         :bendy}
+                       stripped-field-type) (str (:num-fields-x sanitized-layout) "x"
+                                                 (:num-fields-y sanitized-layout) " fields"))
+          changes [main-name
+                   (when (options/changed? :num-base-fields sanitized-layout layout-options)
+                     (str (:num-base-fields sanitized-layout) " base fields"))
+                   (when (some #(options/changed? % sanitized-layout layout-options)
+                               [:offset-x :offset-y])
+                     "shifted")
+                   (when (some #(options/changed? % sanitized-layout layout-options)
+                               [:stretch-x :stretch-y])
+                     "stretched")
+                   (when (options/changed? :rotation sanitized-layout layout-options)
+                     "rotated")]]
+      (-> (util/combine ", " changes)
+          util/upper-case-first))))
 
 (defn layout-submenu [path]
   (when-let [options @(rf/subscribe [:get-relevant-options path])]
     (let [{:keys [ui]} options
           label (:label ui)
-          title @(rf/subscribe [:field-layout-title path])]
+          link-name @(rf/subscribe [:field-layout-submenu-link-name path])]
       [:div.ui-setting
        (when label
          [:label label])
        [:div.option
-        [submenu/submenu path label title {:width "30em"}
+        [submenu/submenu path label link-name {:width "30em"}
          (for [option [:num-base-fields
                        :num-fields-x
                        :num-fields-y
