16e7c3c8503f30c4544905dff9ea0b72d3cf7cf3
Oliver Runge <oliver.runge@gmail.com>
2021-07-11 22:36:50 +0200

    Simplify CSS for arms list view

--- src/heraldry/frontend/arms_library.cljs
+++ src/heraldry/frontend/arms_library.cljs
@@ -335,9 +335,8 @@
 
 (defn view-list-arms []
   [:div {:style {:padding "15px"}}
-   [:div.pure-u-1-2 {:style {:display "block"
-                             :text-align "justify"
-                             :min-width "30em"}}
+   [:div {:style {:text-align "justify"
+                  :min-width "30em"}}
     [:p
      "Here you can create and view coats of arms. You explicitly have to save your coat of arms as "
      [:b "public"] " and add a license, if you want to share the link and allow others to view it."]
