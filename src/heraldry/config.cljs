d3dd7853d743cc1da13fcc1055dfe2e2298b6bec
Oliver Runge <oliver.runge@gmail.com>
2021-07-23 21:18:45 +0200

    Adjust formatting

--- src/heraldry/config.cljs
+++ src/heraldry/config.cljs
@@ -18,14 +18,14 @@
     "local" {:heraldry-api-endpoint "http://localhost:4000/local/api"
              :heraldry-url "http://localhost:8081"
              :cognito-pool-config {:UserPoolId "eu-central-1_eHwF2byeJ"
-                                   :ClientId   "2v90eij0l4aluf2amqumqh9gko"
-                                   :jwksUri    "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_eHwF2byeJ/.well-known/jwks.json"}
+                                   :ClientId "2v90eij0l4aluf2amqumqh9gko"
+                                   :jwksUri "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_eHwF2byeJ/.well-known/jwks.json"}
              :fleur-de-lis-charge-id "charge:RnHzw8"}
     "prod" {:heraldry-api-endpoint "https://2f1yb829vl.execute-api.eu-central-1.amazonaws.com/api"
             :heraldry-url "https://heraldry.digital"
             :cognito-pool-config {:UserPoolId "eu-central-1_WXqnJUEOT"
-                                  :ClientId   "21pvp6cc4l3gptoj4bl3jc9s7r"
-                                  :jwksUri    "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_WXqnJUEOT/.well-known/jwks.json"}
+                                  :ClientId "21pvp6cc4l3gptoj4bl3jc9s7r"
+                                  :jwksUri "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_WXqnJUEOT/.well-known/jwks.json"}
             :fleur-de-lis-charge-id "charge:ZfqrIl"
             :bucket-data "data.heraldry.digital"}))
 
