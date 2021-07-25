df7e44f994e84d140590831f92ee0d082e10489e
Oliver Runge <oliver.runge@gmail.com>
2021-07-25 22:54:53 +0200

    Make range input fields work with "incomplete" values
    Now any typing is allowed, but the value is only parsed when the field
    is blurred.
    
    This allows typing "1." without the value being parsed back to "1" right
    away, making it impossible to type a decimal number if the . isn't
    already there.
    
    This approach also allows updating the value when the hover-menu is used
    to reset it, because tmp-value is only relevant while the field is
    focussed.

--- src/heraldry/frontend/ui/element/range.cljs
+++ src/heraldry/frontend/ui/element/range.cljs
@@ -2,60 +2,70 @@
   (:require [heraldry.frontend.ui.element.value-mode-select :as value-mode-select]
             [heraldry.frontend.ui.interface :as interface]
             [heraldry.util :as util]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [reagent.core :as r]))
 
-(defn range-input [path & {:keys [value on-change disabled?]}]
-  (when-let [option @(rf/subscribe [:get-relevant-options path])]
-    (let [component-id (util/id "range")
-          current-value @(rf/subscribe [:get-value path])
-          {:keys [ui inherited default min max]} option
-          step (or (:step ui) 1)
-          label (:label ui)
-          tooltip (:tooltip ui)
-          value (or value
-                    current-value
-                    inherited
-                    default
-                    min)]
-      [:div.ui-setting
-       [:label {:for component-id} label]
-       (when tooltip
-         [:div.tooltip.info {:style {:display "inline-block"
-                                     :margin-left "0.2em"}}
-          [:i.fas.fa-question-circle]
-          [:div.bottom
-           [:h3 {:style {:text-align "center"}} tooltip]
-           [:i]]])
-       [:div.option
-        [:input {:type "range"
-                 :id component-id
-                 :min min
-                 :max max
-                 :step step
-                 :value value
-                 :disabled disabled?
-                 :on-change #(let [value (-> % .-target .-value js/parseFloat)
-                                   value (if (js/isNaN value)
-                                           nil
-                                           value)]
-                               (if on-change
-                                 (on-change value)
-                                 (rf/dispatch [:set path value])))}]
-        [:input {:type "text"
-                 :value value
-                 :on-change #(let [value (-> % .-target .-value js/parseFloat)
-                                   value (if (js/isNaN value)
-                                           nil
-                                           value)]
-                               (if on-change
-                                 (on-change value)
-                                 (rf/dispatch-sync [:set path value])))
-                 :style {:display "inline-block"
-                         :width "2em"
-                         :margin-left "0.5em"}}]
-        [value-mode-select/value-mode-select path
-         :disabled? disabled?
-         :on-change on-change]]])))
+(defn range-input [_]
+  (let [tmp-value (r/atom nil)
+        focused? (r/atom false)]
+    (fn [path & {:keys [value on-change disabled?]}]
+      (when-let [option @(rf/subscribe [:get-relevant-options path])]
+        (let [component-id (util/id "range")
+              current-value @(rf/subscribe [:get-value path])
+              {:keys [ui inherited default min max]} option
+              step (or (:step ui) 1)
+              label (:label ui)
+              tooltip (:tooltip ui)
+              value (or value
+                        current-value
+                        inherited
+                        default
+                        min)]
+          [:div.ui-setting
+           [:label {:for component-id} label]
+           (when tooltip
+             [:div.tooltip.info {:style {:display "inline-block"
+                                         :margin-left "0.2em"}}
+              [:i.fas.fa-question-circle]
+              [:div.bottom
+               [:h3 {:style {:text-align "center"}} tooltip]
+               [:i]]])
+           [:div.option
+            [:input {:type "range"
+                     :id component-id
+                     :min min
+                     :max max
+                     :step step
+                     :value value
+                     :disabled disabled?
+                     :on-change #(let [value (-> % .-target .-value js/parseFloat)
+                                       value (if (js/isNaN value)
+                                               nil
+                                               value)]
+                                   (if on-change
+                                     (on-change value)
+                                     (rf/dispatch [:set path value])))}]
+            [:input {:type "text"
+                     :value (if @focused?
+                              @tmp-value
+                              value)
+                     :on-focus #(do
+                                  (swap! tmp-value (fn [_] (str value)))
+                                  (swap! focused? (fn [_] true)))
+                     :on-change (when @focused?
+                                  #(swap! tmp-value (fn [_] (-> % .-target .-value))))
+                     :on-blur #(let [parsed-value (js/parseFloat @tmp-value)]
+                                 (when-not (js/isNaN parsed-value)
+                                   (if on-change
+                                     (on-change parsed-value)
+                                     (rf/dispatch-sync [:set path parsed-value])))
+                                 (swap! focused? (fn [_] false)))
+                     :style {:display "inline-block"
+                             :width "2em"
+                             :margin-left "0.5em"}}]
+            [value-mode-select/value-mode-select path
+             :disabled? disabled?
+             :on-change on-change]]])))))
 
 (defmethod interface/form-element :range [path]
   [range-input path])
