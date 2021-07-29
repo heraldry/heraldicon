68f805b039c9ffe54beaf33d81236977f5fbeba2
Oliver Runge <oliver.runge@gmail.com>
2021-07-29 22:03:56 +0200

    WIP: start drafting rule of tincture validation logic

--- src/heraldry/frontend/validation.cljs
+++ src/heraldry/frontend/validation.cljs
@@ -13,11 +13,53 @@
     :error "#b30000"
     "#ccc"))
 
+(rf/reg-sub :field-tinctures-for-validation
+  (fn [[_ path] _]
+    [(rf/subscribe [:get-sanitized-data (conj path :tincture)])
+     (rf/subscribe [:get-sanitized-data (conj path :fields 0 :tincture)])
+     (rf/subscribe [:get-sanitized-data (conj path :fields 1 :tincture)])])
+
+  (fn [[tincture
+        subfield-1-tincture
+        subfield-2-tincture] [_ _path]]
+    (-> #{tincture}
+        (conj subfield-1-tincture)
+        (conj subfield-2-tincture)
+        (cond->
+         ;; at least one of the subfields is not a plain field, but we'll stop here, only
+         ;; report that there's more with the magical :mixed tincture
+         (and (not tincture)
+              (or (not subfield-1-tincture)
+                  (not subfield-2-tincture))) (conj :mixed)))))
+
+(rf/reg-sub :fimbriation-tinctures-for-validation
+  (fn [[_ path] _]
+    [(rf/subscribe [:get-data (conj path :tincture-1)])
+     (rf/subscribe [:get-data (conj path :tincture-2)])])
+
+  (fn [[tincture-1 tincture-2] [_ _path]]
+    [tincture-1 tincture-2]))
+
 (rf/reg-sub :validate-ordinary
   (fn [[_ path] _]
-    (rf/subscribe [:get path]))
+    (let [parent-field-path (->> path
+                                 (drop-last 2)
+                                 vec)
+          own-field-path (conj path :field)]
+      [(rf/subscribe [:field-tinctures-for-validation parent-field-path])
+       (rf/subscribe [:field-tinctures-for-validation own-field-path])
+       (rf/subscribe [:fimbriation-tinctures-for-validation (conj own-field-path :fimbriation)])
+       (rf/subscribe [:fimbriation-tinctures-for-validation (conj own-field-path :line :fimbriation)])
+       (rf/subscribe [:fimbriation-tinctures-for-validation (conj own-field-path :opposite-line :fimbriation)])
+       (rf/subscribe [:fimbriation-tinctures-for-validation (conj own-field-path :extra-line :fimbriation)])]))
 
-  (fn [value [_ _path]]
+  (fn [[parent-field-tinctures
+        own-field-tinctures
+        fimbriation-tinctures
+        line-fimbriation-tinctures
+        opposite-line-fimbriation-tinctures
+        extra-line-fimbriation-tinctures] [_ _path]]
+
     (->> [{:level :error
            :message "An error"}
           {:level :warning
