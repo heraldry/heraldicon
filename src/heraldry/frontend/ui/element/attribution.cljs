710ae1a0e0587f4a83a50314b5af15a98698b1fc
Oliver Runge <oliver.runge@gmail.com>
2021-07-31 22:36:14 +0200

    Add attribution presets again
    Looks like they got lost during the refactoring. Back they are, with the
    additional preset for Sodacan.

--- src/heraldry/frontend/ui/element/attribution.cljs
+++ src/heraldry/frontend/ui/element/attribution.cljs
@@ -1,5 +1,6 @@
 (ns heraldry.frontend.ui.element.attribution
   (:require [heraldry.attribution :as attribution]
+            [heraldry.frontend.ui.element.select :as select]
             [heraldry.frontend.ui.element.submenu :as submenu]
             [heraldry.frontend.ui.interface :as interface]
             [heraldry.options :as options]
@@ -24,7 +25,11 @@
       (-> (util/combine ", " changes)
           util/upper-case-first))))
 
-(defn attribution-submenu [path]
+(rf/reg-event-db :merge-attribution
+  (fn [db [_ path data]]
+    (update-in db path merge data)))
+
+(defn attribution-submenu [path & {:keys [charge-presets?]}]
   (when-let [options @(rf/subscribe [:get-relevant-options path])]
     (let [{:keys [ui]} options
           label (:label ui)
@@ -36,6 +41,55 @@
         [submenu/submenu path label link-name {:style {:width "29em"}
                                                :class "submenu-attribution"}
          [:<>
+          (when charge-presets?
+            [select/raw-select
+             nil
+             :none
+             "Presets"
+             [["Autofill for known sources" :none]
+              ["WappenWiki" :wappenwiki]
+              ["Wikimedia" :wikimedia]
+              ["Wikimedia (Sodacan)" :wikimedia-sodacan]
+              ["Encyclopedia Heraldica" :encyclopedia-heraldica]]
+             :on-change (fn [value]
+                          (case value
+                            :wappenwiki (rf/dispatch [:merge-attribution
+                                                      path
+                                                      {:nature :derivative
+                                                       :license :cc-attribution-non-commercial-share-alike
+                                                       :license-version :v4
+                                                       :source-license :cc-attribution-non-commercial-share-alike
+                                                       :source-license-version :v3
+                                                       :source-creator-name "WappenWiki"
+                                                       :source-creator-link "http://wappenwiki.org"}])
+                            :wikimedia (rf/dispatch [:merge-attribution
+                                                     path
+                                                     {:nature :derivative
+                                                      :license :cc-attribution-share-alike
+                                                      :license-version :v4
+                                                      :source-license :cc-attribution-share-alike
+                                                      :source-license-version :v3}])
+                            :wikimedia-sodacan (rf/dispatch [:merge-attribution
+                                                             path
+                                                             {:nature :derivative
+                                                              :license :cc-attribution-share-alike
+                                                              :license-version :v4
+                                                              :source-license :cc-attribution-share-alike
+                                                              :source-license-version :v3
+                                                              :source-creator-name "Sodacan"
+                                                              :source-creator-link "https://commons.wikimedia.org/wiki/User:Sodacan"}])
+                            :encyclopedia-heraldica (rf/dispatch [:set
+                                                                  path
+                                                                  {:nature :derivative
+                                                                   :license :cc-attribution-share-alike
+                                                                   :license-version :v4
+                                                                   :source-license :cc-attribution-share-alike
+                                                                   :source-license-version :v4
+                                                                   :source-link "https://1drv.ms/u/s!Anj4BrtS8clIaQi3EIOCPpnfKQE?e=AkQ8lW"
+                                                                   :source-creator-name "Encyclopedia Heraldica"
+                                                                   :source-creator-link "https://1drv.ms/u/s!Anj4BrtS8clIaQi3EIOCPpnfKQE?e=AkQ8lW"}])
+                            nil))])
+
           (for [option [:nature
                         :license
                         :license-version]]
@@ -52,4 +106,6 @@
             ^{:key option} [interface/form-element (conj path option)])]]]])))
 
 (defmethod interface/form-element :attribution [path]
-  [attribution-submenu path])
+  [attribution-submenu path
+   :charge-presets? (-> path drop-last last #{:charge-form
+                                              :charge-data})])
--- src/heraldry/frontend/ui/element/select.cljs
+++ src/heraldry/frontend/ui/element/select.cljs
@@ -4,38 +4,41 @@
             [heraldry.util :as util]
             [re-frame.core :as rf]))
 
+(defn raw-select [path value label choices & {:keys [on-change]}]
+  (let [component-id (util/id "select")]
+    [:div.ui-setting
+     (when label
+       [:label {:for component-id} label])
+     [:div.option
+      [:select {:id component-id
+                :value (util/keyword->str value)
+                :on-change #(let [selected (keyword (-> % .-target .-value))]
+                              (if on-change
+                                (on-change selected)
+                                (rf/dispatch [:set path selected])))}
+       (for [[group-name & group-choices] choices]
+         (if (and (-> group-choices count (= 1))
+                  (-> group-choices first keyword?))
+           (let [key (-> group-choices first)]
+             ^{:key key}
+             [:option {:value (util/keyword->str key)} group-name])
+           ^{:key group-name}
+           [:optgroup {:label group-name}
+            (for [[display-name key] group-choices]
+              ^{:key key}
+              [:option {:value (util/keyword->str key)} display-name])]))]
+      [value-mode-select/value-mode-select path]]]))
+
 (defn select [path & {:keys [on-change]}]
   (when-let [option @(rf/subscribe [:get-relevant-options path])]
     (let [current-value @(rf/subscribe [:get-value path])
           {:keys [ui default inherited choices]} option
           label (:label ui)
-          component-id (util/id "select")
           value (or current-value
                     inherited
                     default
                     :none)]
-      [:div.ui-setting
-       (when label
-         [:label {:for component-id} label])
-       [:div.option
-        [:select {:id component-id
-                  :value (util/keyword->str value)
-                  :on-change #(let [selected (keyword (-> % .-target .-value))]
-                                (if on-change
-                                  (on-change selected)
-                                  (rf/dispatch [:set path selected])))}
-         (for [[group-name & group-choices] choices]
-           (if (and (-> group-choices count (= 1))
-                    (-> group-choices first keyword?))
-             (let [key (-> group-choices first)]
-               ^{:key key}
-               [:option {:value (util/keyword->str key)} group-name])
-             ^{:key group-name}
-             [:optgroup {:label group-name}
-              (for [[display-name key] group-choices]
-                ^{:key key}
-                [:option {:value (util/keyword->str key)} display-name])]))]
-        [value-mode-select/value-mode-select path]]])))
+      [raw-select path value label choices :on-change on-change])))
 
 (defmethod interface/form-element :select [path]
   [select path])
