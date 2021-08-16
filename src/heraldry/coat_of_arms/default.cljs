af9b11f3002aba853b577032bd9037b706238923
Oliver Runge <oliver.runge@gmail.com>
2021-08-16 23:48:14 +0200

    Improve default ribbon when creating a new one

--- src/heraldry/coat_of_arms/default.cljs
+++ src/heraldry/coat_of_arms/default.cljs
@@ -105,7 +105,9 @@
 
 (def ribbon
   {:ribbon {:thickness nil
-            :points [(v/v -150 0) (v/v 150 0)]}})
+            :points [(v/v -150 -20) (v/v -75 0) (v/v 0 5) (v/v 75 0) (v/v 150 -20)]
+            :segments [{:type :heraldry.ribbon.segment/foreground-with-text
+                        :text "* LOREM IPSUM *"}]}})
 
 (def motto
   {:type :heraldry.motto.type/motto})
