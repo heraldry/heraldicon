a24a0b70ec751907f7af459cce847ef166878de4
Oliver Runge <oliver.runge@gmail.com>
2021-10-24 17:30:55 +0200

    Use the same charge IDs for special charges in prod and local/staging

--- src/heraldry/config.cljs
+++ src/heraldry/config.cljs
@@ -21,47 +21,44 @@
            :cognito-pool-config {:UserPoolId "eu-central-1_eHwF2byeJ"
                                  :ClientId "2v90eij0l4aluf2amqumqh9gko"
                                  :jwksUri "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_eHwF2byeJ/.well-known/jwks.json"}
-           :fleur-de-lis-charge-id "charge:RnHzw8"
-           :torse-charge-id "charge:6A691Q"
-           :helmet-charge-id "charge:Af7tBN"
            :static-files-url "http://localhost:8081"
            :base-font-dir "/Users/oliver/dev/armory/assets/font"
-           :compartment-charge-id "charge:lD7wLA"
-           :supporter-charge-id "charge:MTMStn"
-           :mantling-charge-id "charge:qGoikm"
-           :motto-ribbon-id "ribbon:5jsojj"
-           :slogan-ribbon-id "ribbon:gGZoHQ"}
+           :fleur-de-lis-charge-id "charge:ZfqrIl"
+           :torse-charge-id "charge:8vwlZ2"
+           :helmet-charge-id "charge:hlsnvP"
+           :compartment-charge-id "charge:SSLk9y"
+           :supporter-charge-id "charge:fxOk19"
+           :mantling-charge-id "charge:gTrIM7"}
 
     "staging" {:heraldry-api-endpoint "https://ru73nh6ozg.execute-api.eu-central-1.amazonaws.com/api"
                :heraldry-url "https://staging.heraldry.digital"
                :cognito-pool-config {:UserPoolId "eu-central-1_eHwF2byeJ"
                                      :ClientId "2v90eij0l4aluf2amqumqh9gko"
                                      :jwksUri "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_eHwF2byeJ/.well-known/jwks.json"}
-               :fleur-de-lis-charge-id "charge:RnHzw8"
-               :torse-charge-id "charge:6A691Q"
-               :helmet-charge-id "charge:Af7tBN"
+               :bucket-data "data.heraldry.digital"
                :static-files-url "https://cdn.staging.heraldry.digital"
                :base-font-dir "./assets/font"
-               :compartment-charge-id "charge:lD7wLA"
-               :supporter-charge-id "charge:MTMStn"
-               :mantling-charge-id "charge:qGoikm"
-               :motto-ribbon-id "ribbon:5jsojj"
-               :slogan-ribbon-id "ribbon:gGZoHQ"}
+               :fleur-de-lis-charge-id "charge:ZfqrIl"
+               :torse-charge-id "charge:8vwlZ2"
+               :helmet-charge-id "charge:hlsnvP"
+               :compartment-charge-id "charge:SSLk9y"
+               :supporter-charge-id "charge:fxOk19"
+               :mantling-charge-id "charge:gTrIM7"}
 
     "prod" {:heraldry-api-endpoint "https://2f1yb829vl.execute-api.eu-central-1.amazonaws.com/api"
             :heraldry-url "https://heraldry.digital"
             :cognito-pool-config {:UserPoolId "eu-central-1_WXqnJUEOT"
                                   :ClientId "21pvp6cc4l3gptoj4bl3jc9s7r"
                                   :jwksUri "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_WXqnJUEOT/.well-known/jwks.json"}
+            :bucket-data "data.heraldry.digital"
+            :static-files-url "https://cdn.heraldry.digital"
+            :base-font-dir "./assets/font"
             :fleur-de-lis-charge-id "charge:ZfqrIl"
             :torse-charge-id "charge:8vwlZ2"
             :helmet-charge-id "charge:hlsnvP"
-            :bucket-data "data.heraldry.digital"
-            :static-files-url "https://cdn.heraldry.digital"
             :compartment-charge-id "charge:SSLk9y"
             :supporter-charge-id "charge:fxOk19"
-            :mantling-charge-id "charge:gTrIM7"
-            :base-font-dir "./assets/font"}))
+            :mantling-charge-id "charge:gTrIM7"}))
 
 #_{:clj-kondo/ignore [:redefined-var]}
 (defn get [setting]
