c042560c16b9ae4368375b419252ec6d9464a562
Oliver Runge <oliver.runge@gmail.com>
2021-11-04 18:55:34 +0100

    Make arms-reference-select context-only

--- src/heraldry/frontend/ui/element/arms_reference_select.cljs
+++ src/heraldry/frontend/ui/element/arms_reference_select.cljs
@@ -5,6 +5,7 @@
    [heraldry.frontend.ui.element.arms-select :as arms-select]
    [heraldry.frontend.ui.element.submenu :as submenu]
    [heraldry.frontend.ui.interface :as ui-interface]
+   [heraldry.interface :as interface]
    [re-frame.core :as rf]))
 
 (defn link-to-arms [path]
@@ -13,10 +14,10 @@
                                              :version 0}])}
      (:name arms)]))
 
-(defn arms-reference-select [path]
-  (when-let [option @(rf/subscribe [:get-relevant-options path])]
+(defn arms-reference-select [{:keys [path] :as context}]
+  (when-let [option (interface/get-relevant-options context)]
     (let [{arms-id :id
-           version :version} @(rf/subscribe [:get path])
+           version :version} (interface/get-raw-data context)
           {:keys [ui]} option
           label (:label ui)
           [_status arms-data] (when arms-id
@@ -36,5 +37,5 @@
                                :de "Wappen auswählen"} arms-title nil
          [arms-select/list-arms (link-to-arms path)]]]])))
 
-(defmethod ui-interface/form-element :arms-reference-select [{:keys [path]}]
-  [arms-reference-select path])
+(defmethod ui-interface/form-element :arms-reference-select [context]
+  [arms-reference-select context])
