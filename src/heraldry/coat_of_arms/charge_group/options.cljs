ee5758e6c9978e430d2ec979c4eb928f6f24b04f
Oliver Runge <oliver.runge@gmail.com>
2021-11-08 17:28:09 +0100

    Make charge-group options context-only

--- src/heraldry/coat_of_arms/charge_group/options.cljs
+++ src/heraldry/coat_of_arms/charge_group/options.cljs
@@ -1,8 +1,8 @@
 (ns heraldry.coat-of-arms.charge-group.options
   (:require
    [heraldry.coat-of-arms.position :as position]
+   [heraldry.context :as c]
    [heraldry.interface :as interface]
-   [heraldry.options :as options]
    [heraldry.strings :as strings]
    [heraldry.util :as util]))
 
@@ -17,115 +17,104 @@
 (def type-map
   (util/choices->map type-choices))
 
-(def default-options
-  {:type {:type :choice
-          :choices type-choices
-          :ui {:label strings/type
-               :form-type :charge-group-type-select}}
-   :origin (-> position/default-options
+(def type-option
+  {:type :choice
+   :choices type-choices
+   :ui {:label strings/type
+        :form-type :charge-group-type-select}})
+
+(def shared-options
+  {:origin (-> position/default-options
                (dissoc :alignment))
-   :anchor (-> position/anchor-default-options
-               (assoc-in [:point :default] :angle)
-               (update-in [:point :choices] (fn [choices]
-                                              (-> choices
-                                                  drop-last
-                                                  (conj (last choices))
-                                                  vec)))
-               (assoc :alignment nil)
-               (assoc-in [:angle :min] -180)
-               (assoc-in [:angle :max] 180)
-               (assoc-in [:angle :default] 0)
-               (assoc-in [:ui :label] strings/anchor))
-   :spacing {:type :range
-             :min 1
-             :max 100
-             :default 40
-             :ui {:label strings/spacing
-                  :step 0.1}}
-   :stretch {:type :range
-             :min 0
-             :max 5
-             :default 1
-             :ui {:label strings/stretch
-                  :step 0.01}}
-   :strip-angle {:type :range
-                 :min -90
-                 :max 90
-                 :default 0
-                 :ui {:label {:en "Strip angle"
-                              :de "Streifenwinkel"}
-                      :step 1}}
-   :start-angle {:type :range
-                 :min -180
-                 :max 180
-                 :default 0
-                 :ui {:label {:en "Start angle"
-                              :de "Startwinkel"}
-                      :step 1}}
-   :arc-angle {:type :range
-               :min 0
-               :max 360
-               :default 360
-               :ui {:label {:en "Arc angle"
-                            :de "Bogenwinkel"}
-                    :step 1}}
-   :slots {:type :range
-           :min 1
-           :max 20
-           :default 5
-           :integer? true
-           :ui {:label strings/number
-                :form-type :charge-group-slot-number}}
-   :radius {:type :range
-            :min 0
-            :max 100
-            :default 30
-            :ui {:label strings/radius
-                 :step 0.1}}
-   :arc-stretch {:type :range
-                 :min 0
-                 :max 5
-                 :default 1
-                 :ui {:label strings/stretch
-                      :step 0.01}}
-   :rotate-charges? {:type :boolean
-                     :default false
-                     :ui {:label {:en "Rotate charges"
-                                  :de "Wappenfiguren rotieren"}}}
    :manual-blazon {:type :text
                    :default nil
                    :ui {:label strings/manual-blazon}}})
 
-(defn options [charge-group]
-  (when charge-group
-    (-> (cond
-          (-> charge-group :type #{:heraldry.charge-group.type/rows
-                                   :heraldry.charge-group.type/columns}) (options/pick default-options
-                                                                                       [[:type]
-                                                                                        [:origin]
-                                                                                        [:spacing]
-                                                                                        [:stretch]
-                                                                                        [:strip-angle]])
-          (-> charge-group :type (= :heraldry.charge-group.type/arc)) (options/pick default-options
-                                                                                    [[:type]
-                                                                                     [:origin]
-                                                                                     [:arc-stretch]
-                                                                                     [:start-angle]
-                                                                                     [:arc-angle]
-                                                                                     [:slots]
-                                                                                     [:radius]
-                                                                                     [:rotate-charges?]]))
-        (assoc :manual-blazon (:manual-blazon default-options))
-        (update :origin (fn [position]
-                          (when position
-                            (-> position
-                                (position/adjust-options (-> charge-group :origin))))))
-        (update :anchor (fn [position]
-                          (when position
-                            (-> position
-                                (position/adjust-options (-> charge-group :anchor)))))))))
+(defn post-process [options context]
+  (-> options
+      (update :origin (fn [position]
+                        (when position
+                          (-> position
+                              (position/adjust-options (interface/get-raw-data (c/++ context :origin)))))))))
 
-(def strip-options
+(defn rows-or-columns [context]
+  (-> shared-options
+      (merge {:spacing {:type :range
+                        :min 1
+                        :max 100
+                        :default 40
+                        :ui {:label strings/spacing
+                             :step 0.1}}
+              :stretch {:type :range
+                        :min 0
+                        :max 5
+                        :default 1
+                        :ui {:label strings/stretch
+                             :step 0.01}}
+              :strip-angle {:type :range
+                            :min -90
+                            :max 90
+                            :default 0
+                            :ui {:label {:en "Strip angle"
+                                         :de "Streifenwinkel"}
+                                 :step 1}}})
+      (post-process context)))
+
+(defmethod interface/options :heraldry.charge-group.type/rows [context]
+  (rows-or-columns context))
+
+(defmethod interface/options :heraldry.charge-group.type/columns [context]
+  (rows-or-columns context))
+
+(defmethod interface/options :heraldry.charge-group.type/arc [context]
+  (-> shared-options
+      (merge {:start-angle {:type :range
+                            :min -180
+                            :max 180
+                            :default 0
+                            :ui {:label {:en "Start angle"
+                                         :de "Startwinkel"}
+                                 :step 1}}
+              :arc-angle {:type :range
+                          :min 0
+                          :max 360
+                          :default 360
+                          :ui {:label {:en "Arc angle"
+                                       :de "Bogenwinkel"}
+                               :step 1}}
+              :slots {:type :range
+                      :min 1
+                      :max 20
+                      :default 5
+                      :integer? true
+                      :ui {:label strings/number
+                           :form-type :charge-group-slot-number}}
+              :radius {:type :range
+                       :min 0
+                       :max 100
+                       :default 30
+                       :ui {:label strings/radius
+                            :step 0.1}}
+              :arc-stretch {:type :range
+                            :min 0
+                            :max 5
+                            :default 1
+                            :ui {:label strings/stretch
+                                 :step 0.01}}
+              :rotate-charges? {:type :boolean
+                                :default false
+                                :ui {:label {:en "Rotate charges"
+                                             :de "Wappenfiguren rotieren"}}}})
+      (post-process context)))
+
+(defmethod interface/options-dispatch-fn :heraldry.component/charge-group [context]
+  (interface/get-raw-data (c/++ context :type)))
+
+(defmethod interface/component-options :heraldry.component/charge-group [context]
+  (-> (interface/options context)
+      (assoc :type type-option)))
+
+(defmethod interface/component-options :heraldry.component/charge-group-strip [_context]
   {:slots {:type :range
            :min 0
            :max 10
@@ -145,9 +134,3 @@
             :default 0
             :ui {:label strings/offset
                  :step 0.01}}})
-
-(defmethod interface/component-options :heraldry.component/charge-group [context]
-  (options (interface/get-raw-data context)))
-
-(defmethod interface/component-options :heraldry.component/charge-group-strip [_context]
-  strip-options)
