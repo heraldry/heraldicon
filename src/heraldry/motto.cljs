4cb2b0357b511c7e1999787c806957dd5a1e77eb
Oliver Runge <oliver.runge@gmail.com>
2021-11-08 20:28:23 +0100

    Make motto options context-only

--- src/heraldry/motto.cljs
+++ src/heraldry/motto.cljs
@@ -3,6 +3,7 @@
    [heraldry.coat-of-arms.geometry :as geometry]
    [heraldry.coat-of-arms.position :as position]
    [heraldry.coat-of-arms.tincture.core :as tincture]
+   [heraldry.context :as c]
    [heraldry.interface :as interface]
    [heraldry.ribbon :as ribbon]
    [heraldry.strings :as strings]))
@@ -15,62 +16,58 @@
                           %))
       (->> (filterv #(when (-> % first :en (not= "Fur")) %)))))
 
-(def default-motto-options
-  {:type {:type :choice
-          :choices [[strings/motto :heraldry.motto.type/motto]
-                    [strings/slogan :heraldry.motto.type/slogan]]
-          :ui {:label strings/type}}
-   :origin (-> position/default-options
-               (assoc-in [:point :choices] [[strings/top :top]
-                                            [strings/bottom :bottom]])
-               (assoc-in [:point :default] :bottom)
-               (assoc-in [:offset-x :min] -100)
-               (assoc-in [:offset-x :max] 100)
-               (assoc-in [:offset-y :min] -100)
-               (assoc-in [:offset-y :max] 100)
-               (dissoc :alignment)
-               (assoc-in [:ui :label] strings/origin))
-   :geometry (-> geometry/default-options
-                 (select-keys [:size :ui])
-                 (assoc-in [:size :min] 5)
-                 (assoc-in [:size :max] 300)
-                 (assoc-in [:size :default] 100))
-   :ribbon-variant {:ui {:label strings/ribbon
-                         :form-type :ribbon-reference-select}}
-   :ribbon (ribbon/options nil)
-
-   :tincture-foreground {:type :choice
-                         :choices tinctures-without-furs
-                         :default :argent
-                         :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/foreground)
-                              :form-type :tincture-select}}
-
-   :tincture-background {:type :choice
-                         :choices (assoc tinctures-without-furs 0 [{:en "Other / Metal"
-                                                                    :de "Andere / Metall"}
-                                                                   [{:en "Darkened foreground"
-                                                                     :de "Vorderseite verdunkelt"} :none]
-                                                                   [(tincture/tincture-map :argent) :argent]
-                                                                   [(tincture/tincture-map :or) :or]])
-                         :default :none
-                         :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/background)
-                              :form-type :tincture-select}}
-
-   :tincture-text {:type :choice
-                   :choices tinctures-without-furs
-                   :default :helmet-dark
-                   :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/foreground-with-text)
-                        :form-type :tincture-select}}})
-
-(defn motto-options [data]
-  (let [ribbon-variant? (:ribbon-variant data)
-        motto-type (:type data)]
-    (-> default-motto-options
-        (cond->
-          ribbon-variant? (update :ribbon ribbon/options (:ribbon data))
-          (not ribbon-variant?) (dissoc :ribbon)
-          (= motto-type :heraldry.motto.type/slogan) (assoc-in [:origin :point :default] :top))
-        (update :origin position/adjust-options))))
+(def type-option
+  {:type :choice
+   :choices [[strings/motto :heraldry.motto.type/motto]
+             [strings/slogan :heraldry.motto.type/slogan]]
+   :ui {:label strings/type}})
 
 (defmethod interface/component-options :heraldry.component/motto [context]
-  (motto-options (interface/get-raw-data context)))
+  (let [ribbon-variant (interface/get-raw-data (c/++ context :ribbon-variant))
+        motto-type (interface/get-raw-data (c/++ context :type))]
+    (-> {:origin (-> position/default-options
+                     (assoc-in [:point :choices] [[strings/top :top]
+                                                  [strings/bottom :bottom]])
+                     (assoc-in [:point :default] :bottom)
+                     (assoc-in [:offset-x :min] -100)
+                     (assoc-in [:offset-x :max] 100)
+                     (assoc-in [:offset-y :min] -100)
+                     (assoc-in [:offset-y :max] 100)
+                     (dissoc :alignment)
+                     (assoc-in [:ui :label] strings/origin))
+         :geometry (-> geometry/default-options
+                       (select-keys [:size :ui])
+                       (assoc-in [:size :min] 5)
+                       (assoc-in [:size :max] 300)
+                       (assoc-in [:size :default] 100))
+         :ribbon-variant {:ui {:label strings/ribbon
+                               :form-type :ribbon-reference-select}}
+
+         :tincture-foreground {:type :choice
+                               :choices tinctures-without-furs
+                               :default :argent
+                               :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/foreground)
+                                    :form-type :tincture-select}}
+
+         :tincture-background {:type :choice
+                               :choices (assoc tinctures-without-furs 0 [{:en "Other / Metal"
+                                                                          :de "Andere / Metall"}
+                                                                         [{:en "Darkened foreground"
+                                                                           :de "Vorderseite verdunkelt"} :none]
+                                                                         [(tincture/tincture-map :argent) :argent]
+                                                                         [(tincture/tincture-map :or) :or]])
+                               :default :none
+                               :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/background)
+                                    :form-type :tincture-select}}
+
+         :tincture-text {:type :choice
+                         :choices tinctures-without-furs
+                         :default :helmet-dark
+                         :ui {:label (ribbon/segment-type-map :heraldry.ribbon.segment/foreground-with-text)
+                              :form-type :tincture-select}}}
+        (cond->
+          ribbon-variant (assoc :ribbon (ribbon/options (c/++ context :ribbon)))
+          (= motto-type :heraldry.motto.type/slogan) (assoc-in [:origin :point :default] :top))
+
+        (update :origin position/adjust-options)
+        (assoc :type type-option))))
