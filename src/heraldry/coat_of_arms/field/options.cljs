20114cd3a0a544e1bb0aa9d20a02315d04a49c4a
Oliver Runge <oliver.runge@gmail.com>
2021-11-13 19:23:05 +0100

    Make tierced-per-pall field option context-only

--- src/heraldry/coat_of_arms/field/options.cljs
+++ src/heraldry/coat_of_arms/field/options.cljs
@@ -208,56 +208,6 @@
                                (dissoc :fimbriation)
                                (assoc :ui (-> default-options :extra-line :ui)))]
       (-> (case (-> field :type name keyword)
-            :tierced-per-pall (options/pick default-options
-                                            [[:type]
-                                             [:inherit-environment?]
-                                             [:counterchanged?]
-                                             [:line]
-                                             [:opposite-line]
-                                             [:extra-line]
-                                             [:origin]
-                                             [:anchor]
-                                             [:direction-anchor]
-                                             [:outline?]]
-                                            {[:line] (-> line-style
-                                                         (options/override-if-exists [:offset :min] 0)
-                                                         (options/override-if-exists [:base-line] nil)
-                                                         (dissoc :fimbriation))
-                                             [:opposite-line] (-> opposite-line-style
-                                                                  (options/override-if-exists [:offset :min] 0)
-                                                                  (options/override-if-exists [:base-line] nil)
-                                                                  (dissoc :fimbriation))
-                                             [:extra-line] (-> extra-line-style
-                                                               (options/override-if-exists [:offset :min] 0)
-                                                               (options/override-if-exists [:base-line] nil)
-                                                               (dissoc :fimbriation))
-                                             [:direction-anchor :point :choices] (util/filter-choices
-                                                                                  position/anchor-point-choices
-                                                                                  [:top-left :top :top-right :left :right :bottom-left :bottom :bottom-right :angle])
-                                             [:direction-anchor :point :default] :top
-                                             [:anchor :point :choices] (util/filter-choices
-                                                                        position/anchor-point-choices
-                                                                        (case (-> field :direction-anchor :point (or :top))
-                                                                          :bottom [:bottom-left :bottom :bottom-right :left :right :angle]
-                                                                          :top [:top-left :top :top-right :left :right :angle]
-                                                                          :left [:top-left :left :bottom-left :top :bottom :angle]
-                                                                          :right [:top-right :right :bottom-right :top :bottom :angle]
-                                                                          :bottom-left [:bottom-left :bottom :bottom-right :top-left :left :angle]
-                                                                          :bottom-right [:bottom-left :bottom :bottom-right :right :top-right :angle]
-                                                                          :top-left [:top-left :top :top-right :left :bottom-left :angle]
-                                                                          :top-right [:top-left :top :top-right :left :bottom-right :angle]
-                                                                          [:top-left :top :top-right :left :right :bottom-left :bottom :bottom-right :angle]))
-                                             [:anchor :point :default] (case (-> field :direction-anchor :point (or :top))
-                                                                         :bottom :bottom-left
-                                                                         :top :top-right
-                                                                         :left :top-left
-                                                                         :right :bottom-right
-                                                                         :bottom-left :left
-                                                                         :bottom-right :bottom
-                                                                         :top-left :top
-                                                                         :top-right :right
-                                                                         :angle :angle
-                                                                         :bottom-left)})
             {})
           (assoc :manual-blazon (:manual-blazon default-options))
           (update :anchor (fn [anchor]
@@ -329,7 +279,8 @@
                        :masony
                        :fretty
                        :tierced-per-pale
-                       :tierced-per-fess})
+                       :tierced-per-fess
+                       :tierced-per-pall})
                (cond-> {:manual-blazon options/manual-blazon}
                  (not (or counterchanged?
                           plain?)) (assoc :outline? options/plain-outline?-option)
