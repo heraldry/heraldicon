0585fd9718b9cb74a17257cf45392f60e3d93a85
Oliver Runge <oliver.runge@gmail.com>
2021-11-18 21:09:15 +0100

    Make ornaments options subscription-based

--- src/heraldry/ornaments.cljs
+++ src/heraldry/ornaments.cljs
@@ -2,5 +2,8 @@
   (:require
    [heraldry.interface :as interface]))
 
+(defmethod interface/options-subscriptions :heraldry.component/ornaments [_context]
+  #{})
+
 (defmethod interface/options :heraldry.component/ornaments [_context]
   {})
