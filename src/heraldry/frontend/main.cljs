4a8deea2ae4a2c587eec275d576b8197eff1c24d
Oliver Runge <oliver.runge@gmail.com>
2021-11-18 21:32:16 +0100

    Use timbre for profile output, rather than console.log

--- src/heraldry/frontend/main.cljs
+++ src/heraldry/frontend/main.cljs
@@ -7,6 +7,7 @@
    [heraldry.util :as util]
    [re-frame.core :as rf]
    [reagent.dom :as r]
+   [taoensso.timbre :as log]
    [taoensso.tufte :as tufte]))
 
 (defn app []
@@ -30,13 +31,13 @@
   (rf/dispatch-sync [:set [:ui :timer] (js/setTimeout f 5000)]))
 
 (defn print-stats []
-  (js/console.log :print-stats)
+  (log/debug :print-stats)
   (when-let [timer @(rf/subscribe [:get [:ui :timer]])]
     (js/clearTimeout timer))
   (when-let [m (not-empty @stats-accumulator)]
-    (js/console.log "profile\n" (tufte/format-grouped-pstats
-                                 m
-                                 {:format-pstats-opts {:columns [:n-calls :min :p50 :p90 :max :mean :clock :total]}})))
+    (log/info "profile\n" (tufte/format-grouped-pstats
+                           m
+                           {:format-pstats-opts {:columns [:n-calls :min :p50 :p90 :max :mean :clock :total]}})))
 
   (start-stats-timer print-stats))
 (defn ^:export init []
