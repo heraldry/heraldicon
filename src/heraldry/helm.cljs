95f47ca6350cc00423185595e6db246101454aa7
Oliver Runge <oliver.runge@gmail.com>
2021-11-18 21:11:18 +0100

    Make helm options subscription-based

--- src/heraldry/helm.cljs
+++ src/heraldry/helm.cljs
@@ -2,8 +2,14 @@
   (:require
    [heraldry.interface :as interface]))
 
+(defmethod interface/options-subscriptions :heraldry.component/helm [_context]
+  #{})
+
 (defmethod interface/options :heraldry.component/helm [_context]
   {})
+
+(defmethod interface/options-subscriptions :heraldry.component/helms [_context]
+  #{})
 
 (defmethod interface/options :heraldry.component/helms [_context]
   {})
