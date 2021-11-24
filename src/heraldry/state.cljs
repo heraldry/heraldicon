0f7588fc6fee336f3fdc94ca132817755b47f11e
Oliver Runge <oliver.runge@gmail.com>
2021-11-24 20:33:04 +0100

    Fix options subscription again
    There still were two subscriptions that built their signal graph inputs
    based on the db state, which breaks things in unexpected ways, because
    options are fixed for paths for which data changes.
    
    This isn't as efficient, but at least it should make it work again.

--- src/heraldry/state.cljs
+++ src/heraldry/state.cljs
@@ -1,6 +1,5 @@
 (ns heraldry.state
   (:require
-   [heraldry.component :as component]
    [heraldry.context :as c]
    [heraldry.interface :as interface]
    [heraldry.options :as options]
@@ -19,62 +18,21 @@
   (fn [value [_ _path]]
     (count value)))
 
-(rf/reg-sub ::component-type
-  (fn [[_ context] _]
-    (rf/subscribe [:get (:path (c/++ context :type))]))
-
-  (fn [raw-type [_ context]]
-    (component/effective-type (:path context) raw-type)))
-
-(rf/reg-sub ::options-for-component
-  (fn [[_ context] _]
-    (let [entity-type (rf/subscribe [:get (:path (c/++ context :type))])
-          component-type (rf/subscribe [::component-type context])]
-      (-> {:component-type component-type
-           :entity-type entity-type}
-          (merge (->> (interface/options-subscriptions
-                       (-> context
-                           (assoc :dispatch-value @component-type)
-                           (assoc :entity-type @entity-type)))
-                      (map (fn [relative-path]
-                             [relative-path (rf/subscribe [:get
-                                                           (:path (apply c/++ context relative-path))])]))
-                      (into {}))))))
-
-  (fn [{:keys [component-type entity-type] :as subscription-data} [_ context]]
-    (when (or component-type entity-type)
-      (let [subscription-context (-> context
-                                     (assoc :dispatch-value component-type
-                                            :entity-type entity-type
-                                            :subscriptions {:base-path (:path context)
-                                                            :data subscription-data}))]
-        (interface/options subscription-context)))))
-
-(rf/reg-sub ::option-path
-  (fn [[_ {:keys [path] :as context}] _]
-    (->> (range (count path))
-         (mapv (fn [length]
-                 (rf/subscribe [::component-type (c/-- context length)])))))
-
-  (fn [component-types [_ {:keys [path]}]]
-    (->> component-types
-         (keep-indexed (fn [idx component-type]
-                         (when (and component-type
-                                    ;; some types are special in that they can't span their own
-                                    ;; option set, but they still have a component node representation
-                                    (not (#{:heraldry.component/cottise} component-type)))
-                           (subvec path 0 (-> path count (- idx))))))
-         first)))
-
 (rf/reg-sub ::options
-  (fn [[_ context] _]
-    (let [option-path (rf/subscribe [::option-path context])
-          component-options (rf/subscribe [::options-for-component (c/<< context :path @option-path)])]
-      [option-path component-options]))
-
-  (fn [[option-path component-options] [_ {:keys [path]}]]
-    (let [relative-path (-> option-path count (drop path) vec)]
-      (get-in component-options relative-path))))
+  (fn [db [_ {:keys [path] :as context}]]
+    (let [[options relative-path] (or (->> (range (count path) 0 -1)
+                                           (keep (fn [idx]
+                                                   (let [option-path (subvec path 0 idx)
+                                                         relative-path (subvec path idx)
+                                                         options (interface/options
+                                                                  (-> context
+                                                                      (c/<< :path option-path)
+                                                                      (assoc :direct-db db)))]
+                                                     (when options
+                                                       [options relative-path]))))
+                                           first)
+                                      [nil nil])]
+      (get-in options relative-path))))
 
 (rf/reg-sub ::sanitized-data
   (fn [[_ {:keys [path] :as context}] _]
