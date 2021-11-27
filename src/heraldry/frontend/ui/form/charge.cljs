d3d58c4aaee1eedddf76f7e6d4a4fa4a7e20be9e
Oliver Runge <oliver.runge@gmail.com>
2021-11-27 11:17:38 +0100

    Add preview icon for charges in component tree

--- src/heraldry/frontend/ui/form/charge.cljs
+++ src/heraldry/frontend/ui/form/charge.cljs
@@ -3,6 +3,8 @@
    [heraldry.coat-of-arms.charge.options :as charge-options]
    [heraldry.context :as c]
    [heraldry.frontend.ui.interface :as ui-interface]
+   [heraldry.interface :as interface]
+   [heraldry.static :as static]
    [re-frame.core :as rf]))
 
 (defn form [context]
@@ -21,11 +23,21 @@
     :ignore-layer-separator?]))
 
 (defmethod ui-interface/component-node-data :heraldry.component/charge [context]
-  ;; TODO: if the charge has a fixed tincture, then this should prevent field config,
-  ;; depends on charge data
-  {:title (charge-options/title context)
-   :validation @(rf/subscribe [:validate-charge context])
-   :nodes [{:context (c/++ context :field)}]})
+  (let [charge-type (interface/get-raw-data (c/++ context :type))]
+    ;; TODO: if the charge has a fixed tincture, then this should prevent field config,
+    ;; depends on charge data
+    {:title (charge-options/title context)
+     :icon (if (-> charge-type charge-options/choice-map)
+             {:default (static/static-url
+                        (str "/svg/charge-type-" (name charge-type) "-unselected.svg"))
+              :selected (static/static-url
+                         (str "/svg/charge-type-" (name charge-type) "-selected.svg"))}
+             {:default (static/static-url
+                        (str "/svg/charge-type-roundel-unselected.svg"))
+              :selected (static/static-url
+                         (str "/svg/charge-type-roundel-selected.svg"))})
+     :validation @(rf/subscribe [:validate-charge context])
+     :nodes [{:context (c/++ context :field)}]}))
 
 (defmethod ui-interface/component-form-data :heraldry.component/charge [_context]
   {:form form})
