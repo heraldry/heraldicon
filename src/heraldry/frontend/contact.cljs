eca27999f2f1d962b93b2e3d795d8c5af0ed523f
Oliver Runge <oliver.runge@gmail.com>
2021-11-28 14:48:51 +0100

    Remove r/heraldry Discord server from contacts page

--- src/heraldry/frontend/contact.cljs
+++ src/heraldry/frontend/contact.cljs
@@ -14,19 +14,6 @@
     "You can email me at oliver [at] heraldry.digital for feedback, questions, complaints, feature "
     "requests, or deep and meaningful friendship."]
 
-   [:p
-    "There also is a " [:a {:href "https://discord.gg/PfGrYSz8" :target "_blank"} "Discord server"]
-    " dedicated to development and help. A widget for it should show up here, if it doesn't, "
-    "then a browser extension like Privacy Badger might have hidden it."]
-
-   [:iframe
-    {:sandbox "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts",
-     :frameBorder "0",
-     :allowtransparency "true",
-     :height "500",
-     :width "350",
-     :src "https://discord.com/widget?id=847884812836012072&theme=dark"}]
-
    [:p {:style {:margin-top "1em"}}
     "The project is on " [:a {:href "https://github.com/heraldry" :target "_blank"} "github:heraldry"]
     " if you want to get involved in some way."]
@@ -34,5 +21,11 @@
    [:p "I can be found on:"]
    [:ul
     [:li [:a {:href "https://www.reddit.com/r/heraldry/" :target "_blank"} "reddit/heraldry"] ", where my username is " [:em "tierced"]]
-    [:li [:a {:href "https://discord.gg/EGbMW8dth2" :target "_blank"} "discord:heraldry.digital"] " (the server mentioned above), where my username is " [:em "or#5915"]]
-    [:li [:a {:href "https://discord.gg/QFrsdMkbbj" :target "_blank"} "discord:heraldry"] ", which is operated by the heraldry subreddit, there my username is " [:em "or#5915"]]]])
+    [:li [:a {:href "https://discord.gg/EGbMW8dth2" :target "_blank"} "discord:heraldry.digital"] ", where my username is " [:em "or#5915"]]]
+   [:iframe
+    {:sandbox "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts",
+     :frameBorder "0",
+     :allowtransparency "true",
+     :height "500",
+     :width "350",
+     :src "https://discord.com/widget?id=847884812836012072&theme=dark"}]])
