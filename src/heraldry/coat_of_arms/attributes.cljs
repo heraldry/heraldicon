f10e175663dd22aa1ac1d4ed5b133b23df61b1d6
Oliver Runge <oliver.runge@gmail.com>
2021-12-01 19:39:08 +0100

    Translate attributes-related strings

--- src/heraldry/coat_of_arms/attributes.cljs
+++ src/heraldry/coat_of_arms/attributes.cljs
@@ -2,157 +2,141 @@
   (:require
    [goog.string :as gstring]
    [heraldry.colour :as colour]
+   [heraldry.gettext :refer [string]]
    [heraldry.strings :as strings]
    [heraldry.util :as util]))
 
 (def attitude-choices
   [[strings/none :none]
-   [{:en "Beasts"
-     :de "Tiere"}
-    ["Cadent" :cadent]
-    ["Couchant" :couchant]
-    ["Courant" :courant]
-    ["Dormant" :dormant]
-    ["Pascuant" :pascuant]
-    ["Passant" :passant]
-    ["Rampant" :rampant]
-    ["Salient" :salient]
-    ["Segreant" :segreant]
-    ["Sejant" :sejant]
-    ["Sejant erect" :sejant-erect]
-    ["Statant" :statant]]
-   [{:en "Two beasts"
-     :de "Zwei Tiere"}
-    ["Addorsed" :addorsed]
-    ["Combatant" :combatant]
-    ["Respectant" :respectant]]
-   [{:en "Winged creatures"
-     :de "Geflügelte Kreaturen"}
-    ["Close" :close]
-    ["Displayed" :displayed]
-    ["In her piety" :in-her-piety]
-    ["Issuant" :issuant]
-    ["Overt" :overt]
-    ["Recursant" :recursant]
-    ["Rising" :rising]
-    ["Vigilant" :vigilant]
-    ["Volant" :volant]
-    ["Vulning herself" :vulning]]
-   [{:en "Sea creatures"
-     :de "Wasser-Kreaturen"}
-    ["Hauriant" :hauriant]
-    ["Naiant" :naiant]
-    ["Urinant" :urinant]]
-   [{:en "Serpents"
-     :de "Schlangen"}
-    ["Glissant" :glissant]
-    ["Nowed" :nowed]]])
+   [(string "Beasts")
+    [(string "Cadent") :cadent]
+    [(string "Couchant") :couchant]
+    [(string "Courant") :courant]
+    [(string "Dormant") :dormant]
+    [(string "Pascuant") :pascuant]
+    [(string "Passant") :passant]
+    [(string "Rampant") :rampant]
+    [(string "Salient") :salient]
+    [(string "Segreant") :segreant]
+    [(string "Sejant") :sejant]
+    [(string "Sejant erect") :sejant-erect]
+    [(string "Statant") :statant]]
+   [(string"Two beasts")
+    [(string "Addorsed") :addorsed]
+    [(string "Combatant") :combatant]
+    [(string "Respectant") :respectant]]
+   [(string "Winged creatures")
+    [(string "Close") :close]
+    [(string "Displayed") :displayed]
+    [(string "In her piety") :in-her-piety]
+    [(string "Issuant") :issuant]
+    [(string "Overt") :overt]
+    [(string "Recursant") :recursant]
+    [(string "Rising") :rising]
+    [(string "Vigilant") :vigilant]
+    [(string "Volant") :volant]
+    [(string "Vulning herself") :vulning]]
+   [(string "Sea creatures")
+    [(string "Hauriant") :hauriant]
+    [(string "Naiant") :naiant]
+    [(string "Urinant") :urinant]]
+   [(string "Serpents")
+    [(string "Glissant") :glissant]
+    [(string "Nowed") :nowed]]])
 
 (def attitude-map
   (util/choices->map attitude-choices))
 
 (def facing-choices
   [[strings/none :none]
-   ["To dexter (default)" :to-dexter]
-   ["To sinister" :to-sinister]
-   ["Affronté" :affronte]
-   ["En arrière" :en-arriere]
-   ["Guardant" :guardant]
-   ["In trian aspect" :in-trian-aspect]
-   ["Reguardant" :reguardant]
-   ["Salient" :salient]])
+   [(string "To dexter (default)") :to-dexter]
+   [(string "To sinister") :to-sinister]
+   [(string "Affronté") :affronte]
+   [(string "En arrière") :en-arriere]
+   [(string "Guardant") :guardant]
+   [(string "In trian aspect") :in-trian-aspect]
+   [(string "Reguardant") :reguardant]
+   [(string "Salient") :salient]])
 
 (def facing-map
   (util/choices->map facing-choices))
 
 (def attribute-choices
   [[strings/general
-    ["Erased" :erased]
-    ["Pierced" :pierced]
-    ["Voided" :voided]]
-   [{:en "Ornaments"
-     :de "Prachtstücke"}
+    [(string "Erased") :erased]
+    [(string "Pierced") :pierced]
+    [(string "Voided") :voided]]
+   [(string "Ornaments")
     [strings/mantling :mantling]
     [strings/compartment :compartment]
     [strings/supporter :supporter]]
-   [{:en "Tail"
-     :de "Schwanz"}
-    ["Coward" :coward]
-    ["Defamed" :defamed]
-    ["Double queued" :double-queued]
-    ["Tail nowed" :tail-nowed]
-    ["Queue fourché saltire reverse" :queue-fourche-saltire-reverse]
-    ["Queue fourché saltire" :queue-fourche-saltire]
-    ["Queue fourché" :queue-fourche]]])
+   [(string "Tail")
+    [(string "Coward") :coward]
+    [(string "Defamed") :defamed]
+    [(string "Double queued") :double-queued]
+    [(string "Tail nowed") :tail-nowed]
+    [(string "Queue fourché saltire reverse") :queue-fourche-saltire-reverse]
+    [(string "Queue fourché saltire") :queue-fourche-saltire]
+    [(string "Queue fourché") :queue-fourche]]])
 
 (def attribute-map
   (util/choices->map attribute-choices))
 
 (def tincture-modifier-choices
-  [["Fauna"
-    ["Armed" :armed]
-    ["Attired" :attired]
-    ["Beaked" :beaked]
-    ["Combed" :combed]
-    ["Eyed" :eyed]
-    ["Eyed (peacock)" :eyed-peacock]
-    ["Finned" :finned]
-    ["Incensed" :incensed]
-    ["Langued" :langued]
-    ["Legged" :legged]
-    ["Maned" :maned]
-    ["Orbed" :orbed]
-    ["Tailed" :tailed]
-    ["Toothed" :toothed]
-    ["Unguled" :unguled]
-    ["Wattled" :wattled]
-    ["Winged" :winged]]
-   ["Flora"
-    ["Barbed" :barbed]
-    ["Bladed" :bladed]
-    ["Fructed" :fructed]
-    ["Leaved" :leaved]
-    ["Seeded" :seeded]
-    ["Slipped" :slipped]]
-   [{:en "Helmet"
-     :de "Helm"}
-    ["Barred" :barred]
-    ["Trimmed" :trimmed]]
-   [{:en "Other"
-     :de "Andere"}
-    ["Caparisoned" :caparisoned]
-    ["Collared" :collared]
-    ["Erased" :erased]
-    ["Garnished" :garnished]
-    ["Handled" :handled]
-    ["Hilted" :hilted]
-    ["Illuminated" :illuminated]
-    ["Hooded" :hooded]
-    ["Masoned" :masoned]
-    ["Pommeled" :pommeled]
-    ["Stringed" :stringed]]])
+  [[(string "Fauna")
+    [(string "Armed") :armed]
+    [(string "Attired") :attired]
+    [(string "Beaked") :beaked]
+    [(string "Combed") :combed]
+    [(string "Eyed") :eyed]
+    [(string "Eyed (peacock)") :eyed-peacock]
+    [(string "Finned") :finned]
+    [(string "Incensed") :incensed]
+    [(string "Langued") :langued]
+    [(string "Legged") :legged]
+    [(string "Maned") :maned]
+    [(string "Orbed") :orbed]
+    [(string "Tailed") :tailed]
+    [(string "Toothed") :toothed]
+    [(string "Unguled") :unguled]
+    [(string "Wattled") :wattled]
+    [(string "Winged") :winged]]
+   [(string "Flora")
+    [(string "Barbed") :barbed]
+    [(string "Bladed") :bladed]
+    [(string "Fructed") :fructed]
+    [(string "Leaved") :leaved]
+    [(string "Seeded") :seeded]
+    [(string "Slipped") :slipped]]
+   [(string "Helmet")
+    [(string "Barred") :barred]
+    [(string "Trimmed") :trimmed]]
+   [(string "Other")
+    [(string "Caparisoned") :caparisoned]
+    [(string "Collared") :collared]
+    [(string "Erased") :erased]
+    [(string "Garnished") :garnished]
+    [(string "Handled") :handled]
+    [(string "Hilted") :hilted]
+    [(string "Illuminated") :illuminated]
+    [(string "Hooded") :hooded]
+    [(string "Masoned") :masoned]
+    [(string "Pommeled") :pommeled]
+    [(string "Stringed") :stringed]]])
 
 (def tincture-modifier-map
   (util/choices->map tincture-modifier-choices))
 
 (def tincture-modifier-for-charge-choices
-  (vec (concat [[{:en "Technical"
-                  :de "Technisch"}
-                 [{:en "Don't replace"
-                   :de "Nicht ersetzen"} :keep]
-                 [{:en "Primary"
-                   :de "Primär"} :primary]
-                 [{:en "Secondary"
-                   :de "Sekundär"} :secondary]
-                 [{:en "Tertiary"
-                   :de "Tertiär"} :tertiary]
+  (vec (concat [[(string "Technical")
+                 [(string "Don't replace") :keep]
+                 [(string "Primary") :primary]
+                 [(string "Secondary") :secondary]
+                 [(string "Tertiary") :tertiary]
                  [strings/outline :outline]
-                 [{:en "Shadow (alpha)"
-                   :de "Schatten (alpha)"} :shadow]
-                 [{:en "Highlight (alpha)"
-                   :de "Glanz (alpha)"} :highlight]
-                 [{:en "Layer separator"
-                   :de "Ebenentrenner"} :layer-separator]]]
+                 [(string "Shadow (alpha)") :shadow]
+                 [(string "Highlight (alpha)") :highlight]
+                 [(string "Layer separator") :layer-separator]]]
                tincture-modifier-choices)))
 
 (def tincture-modifier-for-charge-map
@@ -207,8 +191,7 @@
                     (map (fn [percentage]
                            (let [{:keys [name key]} (make-qualifier :highlight percentage)]
                              [name key]))))
-               [[{:en "None"
-                  :de "Keine"} :none]]
+               [[(string "None") :none]]
                (->> qualifier-percentages
                     (map (fn [percentage]
                            (let [{:keys [name key]} (make-qualifier :shadow percentage)]
