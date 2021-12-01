e28323069b8b67c0c3b81c3f07696227639954a0
Oliver Runge <oliver.runge@gmail.com>
2021-12-01 20:25:01 +0100

    Translate texture-related strings

--- src/heraldry/coat_of_arms/texture.cljs
+++ src/heraldry/coat_of_arms/texture.cljs
@@ -1,41 +1,27 @@
 (ns heraldry.coat-of-arms.texture
   (:require
    [heraldry.config :as config]
+   [heraldry.gettext :refer [string]]
    [heraldry.strings :as strings]
    [heraldry.util :as util]))
 
 (def textures
   [[strings/none :none nil 0]
-   [{:en "Cloth (rough)"
-     :de "Stoff (rau)"} :cloth-rough "/textures/cloth-rough.jpg" 1]
-   [{:en "Cloth (smooth)"
-     :de "Stoff (glatt)"} :cloth-smooth "/textures/cloth-smooth.jpg" 1]
-   [{:en "Felt"
-     :de "Filz"} :felt "/textures/felt.jpg" 1]
-   [{:en "Glass (frosted)"
-     :de "Glas (matt)"} :glass-frosted "/textures/glass-frosted.jpg" 1.5]
-   [{:en "Marble"
-     :de "Marmor"} :marble "/textures/marble.jpg" 1]
-   [{:en "Metal (brushed 1)"
-     :de "Metall (gebürstet 1)"} :metal-brushed-1 "/textures/metal-brushed-1.jpg" 1]
-   [{:en "Metal (brushed 2)"
-     :de "Metall (gebürstet 2)"} :metal-brushed-2 "/textures/metal-brushed-2.jpg" 1]
-   [{:en "Metal (rough)"
-     :de "Metall (rau)"} :metal-rough "/textures/metal-rough.jpg" 1]
-   [{:en "Metal (scraped)"
-     :de "Metall (zerkratzt)"} :metal-scraped "/textures/metal-scraped.jpg" 1]
-   [{:en "Metal (smooth)"
-     :de "Metall (glatt)"} :metal-smooth "/textures/metal-smooth.jpg" 1]
-   [{:en "Mosaic"
-     :de "Mosaik"} :mosaic "/textures/mosaic.jpg" 2]
-   [{:en "Satin"
-     :de "Satin"} :satin "/textures/satin.jpg" 10]
-   [{:en "Stone"
-     :de "Stein"} :stone "/textures/stone.jpg" 1]
-   [{:en "Stone (rough)"
-     :de "Stein (rau)"} :stone-rough "/textures/stone-rough.jpg" 1]
-   [{:en "Wood"
-     :de "Holz"} :wood "/textures/wood.jpg" 1]])
+   [(string "Cloth (rough)") :cloth-rough "/textures/cloth-rough.jpg" 1]
+   [(string "Cloth (smooth)") :cloth-smooth "/textures/cloth-smooth.jpg" 1]
+   [(string "Felt") :felt "/textures/felt.jpg" 1]
+   [(string "Glass (frosted)") :glass-frosted "/textures/glass-frosted.jpg" 1.5]
+   [(string "Marble") :marble "/textures/marble.jpg" 1]
+   [(string "Metal (brushed 1)") :metal-brushed-1 "/textures/metal-brushed-1.jpg" 1]
+   [(string "Metal (brushed 2)") :metal-brushed-2 "/textures/metal-brushed-2.jpg" 1]
+   [(string "Metal (rough)") :metal-rough "/textures/metal-rough.jpg" 1]
+   [(string "Metal (scraped)") :metal-scraped "/textures/metal-scraped.jpg" 1]
+   [(string "Metal (smooth)") :metal-smooth "/textures/metal-smooth.jpg" 1]
+   [(string "Mosaic") :mosaic "/textures/mosaic.jpg" 2]
+   [(string "Satin") :satin "/textures/satin.jpg" 10]
+   [(string "Stone") :stone "/textures/stone.jpg" 1]
+   [(string "Stone (rough)") :stone-rough "/textures/stone-rough.jpg" 1]
+   [(string "Wood") :wood "/textures/wood.jpg" 1]])
 
 (def choices
   (->> textures
