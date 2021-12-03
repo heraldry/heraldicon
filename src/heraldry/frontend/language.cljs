7f0af718206a84babc443ad3a21c817565761337
Oliver Runge <oliver.runge@gmail.com>
2021-12-03 18:11:55 +0100

    Make language selector a submenu
    Also give each flag the same width, preserving their respective aspect
    ratio. It doesn't look weird in the vertical menu and avoids stretching
    flags into the wrong aspect ratio.

--- src/heraldry/frontend/language.cljs
+++ src/heraldry/frontend/language.cljs
@@ -2,6 +2,7 @@
   (:require
    [clojure.string :as s]
    [heraldry.frontend.macros :as macros]
+   [heraldry.frontend.state :as state]
    [heraldry.static :as static]
    [heraldry.util :as util]
    [hodgepodge.core :refer [get-item local-storage set-item]]
@@ -10,11 +11,15 @@
 (def language-path
   [:ui :language])
 
+(def language-menu-open?-path
+  [:ui :menu :language-menu :open?])
+
 (def local-storage-language-name
   "hd-language")
 
 (def known-languages
-  #{:en :de})
+  {:en (static/static-url "/img/flag-united-kingdom.svg")
+   :de (static/static-url "/img/flag-germany.svg")})
 
 (defn store-language-setting [language]
   (set-item local-storage local-storage-language-name language))
@@ -23,15 +28,6 @@
   (fn [db _]
     (or (get-in db language-path)
         :en)))
-
-(rf/reg-sub ::selector-opacity
-  (fn [_ _]
-    (rf/subscribe [::selected-language]))
-
-  (fn [selected-language [_ language]]
-    (if (= selected-language language)
-      1
-      0.25)))
 
 (defn set-language [db language]
   (if (known-languages language)
@@ -51,24 +47,36 @@
                             (s/starts-with? loaded-language ":") (-> (subs 1) keyword))]
       (set-language db loaded-language))))
 
+(defn language-flag [language-code & {:keys [on-click]}]
+  [:img {:src (get known-languages language-code)
+         :on-click on-click
+         :style {:width "2em"
+                 :filter "drop-shadow(0 0 5px #888)";
+                 :vertical-align "middle"}}])
+
 (defn selector []
-  [:div {:style {:white-space "nowrap"
-                 :position "relative"
-                 :top "50%"
-                 :transform "translate(0,-50%)"}}
-   [:img {:src (static/static-url "/img/flag-united-kingdom.svg")
-          :on-click #(rf/dispatch [::set-language :en])
-          :style {:width "2em"
-                  :height "1em"
-                  :cursor "pointer"
-                  :opacity @(rf/subscribe [::selector-opacity :en])}}]
-   " "
-   [:img {:src (static/static-url "/img/flag-germany.svg")
-          :on-click #(rf/dispatch [::set-language :de])
-          :style {:width "2em"
-                  :height "1em"
-                  :cursor "pointer"
-                  :opacity @(rf/subscribe [::selector-opacity :de])}}]])
+  [:li.nav-menu-item.nav-menu-has-children.nav-menu-allow-hover
+   {:style {:min-width "3em"}
+    :on-mouse-leave #(rf/dispatch [:heraldry.frontend.header/clear-menu-open?
+                                   language-menu-open?-path])}
+   [:<>
+    [:a.nav-menu-link {:href "#"
+                       :on-click #(state/dispatch-on-event
+                                   % [:heraldry.frontend.header/toggle-menu-open?
+                                      language-menu-open?-path])}
+     [language-flag @(rf/subscribe [::selected-language])]
+     " "]
+    [:ul.nav-menu.nav-menu-children
+     {:style {:display (if @(rf/subscribe [:heraldry.frontend.header/menu-open?
+                                           language-menu-open?-path])
+                         "block"
+                         "none")}}
+     (doall
+      (for [language-code (keys known-languages)]
+        [:li.nav-menu-item
+         [:a.nav-menu-link
+          [language-flag language-code
+           :on-click #(rf/dispatch [::set-language language-code])]]]))]]])
 
 (defn tr [data]
   (when data
