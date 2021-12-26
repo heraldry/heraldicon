b93395fc57a8c5057a67c052996042c23f517d56
Oliver Runge <oliver.runge@gmail.com>
2021-12-26 14:22:42 +0100

    Translate cottise remove button tooltips

--- src/heraldry/frontend/ui/form/ordinary.cljs
+++ src/heraldry/frontend/ui/form/ordinary.cljs
@@ -152,34 +152,34 @@
               cottise-2? (conj {:context cottise-2-context
                                 :buttons [{:icon "far fa-trash-alt"
                                            :remove? true
-                                           :tooltip "remove"
+                                           :tooltip (string "remove")
                                            :handler #(state/dispatch-on-event % [:remove-cottise cottise-2-context])}]})
               cottise-1? (conj {:context cottise-1-context
                                 :buttons [{:icon "far fa-trash-alt"
                                            :remove? true
-                                           :tooltip "remove"
+                                           :tooltip (string "remove")
                                            :handler #(state/dispatch-on-event % [:remove-cottise cottise-1-context])}]})
               cottise-opposite-1? (conj {:context cottise-opposite-1-context
                                          :buttons [{:icon "far fa-trash-alt"
                                                     :remove? true
-                                                    :tooltip "remove"
+                                                    :tooltip (string "remove")
                                                     :handler #(state/dispatch-on-event % [:remove-cottise cottise-opposite-1-context])}]})
 
               cottise-opposite-2? (conj {:context cottise-opposite-2-context
                                          :buttons [{:icon "far fa-trash-alt"
                                                     :remove? true
-                                                    :tooltip "remove"
+                                                    :tooltip (string "remove")
                                                     :handler #(state/dispatch-on-event % [:remove-cottise cottise-opposite-2-context])}]})
               cottise-extra-1? (conj {:context cottise-extra-1-context
                                       :buttons [{:icon "far fa-trash-alt"
                                                  :remove? true
-                                                 :tooltip "remove"
+                                                 :tooltip (string "remove")
                                                  :handler #(state/dispatch-on-event % [:remove-cottise cottise-extra-1-context])}]})
 
               cottise-extra-2? (conj {:context cottise-extra-2-context
                                       :buttons [{:icon "far fa-trash-alt"
                                                  :remove? true
-                                                 :tooltip "remove"
+                                                 :tooltip (string "remove")
                                                  :handler #(state/dispatch-on-event % [:remove-cottise cottise-extra-2-context])}]}))}))
 
 (defmethod ui-interface/component-form-data :heraldry.component/ordinary [_context]
