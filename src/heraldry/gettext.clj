13aaf7e16498c3891fb8bb617479ddc0adf4fa8e
Oliver Runge <oliver.runge@gmail.com>
2021-12-31 00:09:08 +0100

    Add some helper functions to migrate translation strings

--- src/heraldry/gettext.clj
+++ src/heraldry/gettext.clj
@@ -1,24 +1,48 @@
 (ns heraldry.gettext
-  (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]
-            [pottery.core :as pottery]
-            [pottery.scan :as pottery-scan]
-            [shadow.resource :as res]))
+  (:require
+   [clojure.data.json :as json]
+   [clojure.java.io :as io]
+   [clojure.string :as s]
+   [pottery.core :as pottery]
+   [pottery.scan :as pottery-scan]
+   [shadow.build.warnings :as warnings]
+   [shadow.resource :as res]))
 
 (defmacro inline-dict [filename]
   (pottery/read-po-str (res/slurp-resource &env filename)))
 
-(defn build-keyword-keys [data & {:keys [prefix]}]
+(def required-key-pattern
+  #"^[a-z0-9.?-]+$")
+
+(defn build-keyword-keys [data & {:keys [prefix
+                                         source-file]}]
   (->> data
        (mapcat (fn [[k v]]
-                 (if (map? v)
-                   (build-keyword-keys v :prefix (str prefix "." k))
-                   [[(keyword prefix k) v]])))
+                 (if-not (re-matches required-key-pattern k)
+                   (do
+                     (warnings/print-warning {:msg (str "Warning: key '" k "' doesn't match pattern: " required-key-pattern)
+                                              :line 1
+                                              :column 1
+                                              :file source-file
+                                              :soure-name source-file
+                                              :resource-name source-file
+                                              :source-excerpt {:start-idx 0
+                                                               :before ["<unknown>"]
+                                                               :after ["<unknown>"]
+                                                               :line (str "\"" k "\"")}})
+                     [])
+                   (if (map? v)
+                     (build-keyword-keys v
+                                         :prefix (str prefix "." k)
+                                         :source-file source-file)
+                     [[(keyword prefix k) v]]))))
        (into {})))
 
 (defmacro inline-dict-json [filename]
   (let [data (json/read-str (res/slurp-resource &env filename))]
-    (build-keyword-keys data :prefix "strings")))
+    (build-keyword-keys data
+                        :prefix "string"
+                        :source-file filename)))
 
 (defn gettext-do-scan! []
   (pottery/scan-codebase!
@@ -29,8 +53,58 @@
                  [(:or 'string) & _] (pottery-scan/extraction-warning
                                       "Could not extract strings for the form:"))}))
 
+(defn replace-string [keyword-lookup [all s]]
+  (let [k (get keyword-lookup s)]
+    (if k
+      (str k)
+      all)))
+
+(defn replace-strings-by-keywords []
+  (let [json-data (inline-dict-json "en-UK.json")
+        keyword-lookup (->> json-data
+                            (group-by second)
+                            (map (fn [[k v]]
+                                   (when (-> v count (> 1))
+                                     (println "warning: best ambiguous keyword: " (ffirst v)))
+                                   [k (ffirst v)]))
+                            (into {}))
+        replace-fn (fn [args]
+                     (replace-string keyword-lookup args))
+        files (into []
+                    (filter #(re-matches #".*\.cljs" (.getName %)))
+                    (file-seq (io/file "src")))]
+    (doall
+     (for [file files]
+       (let [file-path (.getAbsolutePath file)
+             data (slurp file-path)]
+         (->> (s/replace data #"\(string \"([^\"]*)\"\)" replace-fn)
+              (spit file-path)))))
+    nil))
+
+(defn check-translation-string-usage []
+  (let [json-data (inline-dict-json "en-UK.json")
+        files (into []
+                    (filter #(re-matches #".*\.cljs" (.getName %)))
+                    (file-seq (io/file "src")))]
+    (doall
+     (for [file files]
+       (->> file
+            .getAbsolutePath
+            slurp
+            (re-seq #":(string[.][a-z0-9.?-]+/[a-z0-9.?-]+)")
+            (map (fn [[_ s]]
+                   (let [k (keyword s)]
+                     (when-not (contains? json-data k)
+                       (println (str "Warning: unknown key '" k "'"))))))
+            doall)))
+    nil))
+
 (comment
   (gettext-do-scan!)
+
+  (replace-strings-by-keywords)
+
+  (check-translation-string-usage)
 
   ;;
   )
