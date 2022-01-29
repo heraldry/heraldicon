59cdc6c180de1f9aabea040eada4da74501f20aa
Oliver Runge <oliver.runge@gmail.com>
2022-01-29 22:28:29 +0100

    List ribbons with preview as well

--- src/heraldry/frontend/ui/element/ribbon_select.cljs
+++ src/heraldry/frontend/ui/element/ribbon_select.cljs
@@ -3,12 +3,11 @@
    [cljs.core.async :refer [go]]
    [clojure.string :as s]
    [com.wsscode.common.async-cljs :refer [<?]]
-   [heraldry.attribution :as attribution]
    [heraldry.frontend.api.request :as api-request]
    [heraldry.frontend.filter :as filter]
    [heraldry.frontend.language :refer [tr]]
+   [heraldry.frontend.preview :as preview]
    [heraldry.frontend.state :as state]
-   [heraldry.frontend.ui.element.tags :as tags]
    [heraldry.frontend.user :as user]
    [re-frame.core :as rf]
    [taoensso.timbre :as log]))
@@ -57,7 +56,9 @@
 (defn invalidate-ribbon-cache [key]
   (state/invalidate-cache list-db-path key))
 
-(defn component [ribbon-list link-fn refresh-fn & {:keys [hide-ownership-filter?]}]
+
+(defn component [ribbon-list link-fn refresh-fn & {:keys [hide-ownership-filter?
+                                                          selected-ribbon]}]
   (let [user-data (user/data)]
     [filter/component
      :ribbon-list
@@ -65,28 +66,21 @@
      ribbon-list
      [:name :username :tags]
      (fn [& {:keys [items]}]
-       [:ul.ribbon-list
+       [:ul.filter-results
         (doall
-         (for [ribbon (sort-by (comp s/lower-case :name) items)]
-           (let [username (-> ribbon :username)]
-             ^{:key (:id ribbon)}
-             [:li.ribbon
-              (if (-> ribbon :is-public)
-                [:div.tag.public {:style {:width "0.9em"}} [:i.fas.fa-lock-open]]
-                [:div.tag.private {:style {:width "0.9em"}} [:i.fas.fa-lock]])
-              " "
-              [link-fn ribbon]
-              " "
-              [tr :string.miscellaneous/by]
-              " "
-              [:a {:href (attribution/full-url-for-username username)
-                   :target "_blank"} username]
-              " "
-              [tags/tags-view (-> ribbon :tags keys)]])))])
+         (for [ribbon items]
+           ^{:key (:id ribbon)}
+           [preview/ribbon ribbon link-fn :selected? (filter/selected-item? selected-ribbon ribbon)]))])
      refresh-fn
-     :hide-ownership-filter? hide-ownership-filter?]))
+     :sort-fn (fn [ribbon]
+                [(not (and selected-ribbon
+                           (filter/selected-item? selected-ribbon ribbon)))
+                 (-> ribbon :name s/lower-case)])
+     :page-size 10
+     :hide-ownership-filter? hide-ownership-filter?
+     :selected-item selected-ribbon]))
 
-(defn list-ribbon [link-to-ribbon]
+(defn list-ribbons [link-to-ribbon & {:keys [selected-ribbon]}]
   (let [[status ribbon-list] (state/async-fetch-data
                               list-db-path
                               :all
@@ -95,5 +89,6 @@
       [component
        ribbon-list
        link-to-ribbon
-       #(invalidate-ribbon-cache :all)]
+       #(invalidate-ribbon-cache :all)
+       :selected-ribbon selected-ribbon]
       [:div [tr :string.miscellaneous/loading]])))
