0174f086c5a682bed163f9ed288fc848c898e8cf
Oliver Runge <oliver.runge@gmail.com>
2022-03-31 19:52:55 +0200

    Choose more sensible defaults for charge arrangements on ordinaries

--- src/heraldry/blazonry/parser.cljs
+++ src/heraldry/blazonry/parser.cljs
@@ -589,10 +589,7 @@
     (-> {:charges [charge]}
         (cond->
           (nil? arrangement-type) (merge
-                                   {:type :heraldry.charge-group.type/rows
-                                    :spacing (/ 95 amount)
-                                    :strips [{:type :heraldry.component/charge-group-strip
-                                              :slots (vec (repeat amount 0))}]})
+                                   {::default-charge-group-amount amount})
           (= :FESSWISE
              arrangement-type) (merge
                                 {:type :heraldry.charge-group.type/rows
@@ -702,10 +699,78 @@
 (defmethod ast->hdn :blazon [[_ node]]
   (ast->hdn node))
 
+(defn add-charge-group-defaults [{::keys [default-charge-group-amount]
+                                  :keys [type]
+                                  :as hdn} & {:keys [parent-ordinary-type]}]
+  (let [type-namespace (some-> type namespace)]
+    (cond-> hdn
+      default-charge-group-amount (->
+                                   (dissoc ::default-charge-group-amount)
+                                   (merge
+                                    (case (some-> parent-ordinary-type name keyword)
+                                      :pale {:type :heraldry.charge-group.type/columns
+                                             :spacing (/ 95 default-charge-group-amount)
+                                             :strips [{:type :heraldry.component/charge-group-strip
+                                                       :slots (vec (repeat default-charge-group-amount 0))}]}
+
+                                      :chevron {:type :heraldry.charge-group.type/rows
+                                                :spacing (/ 90 default-charge-group-amount)
+                                                :strips (->> (range (-> default-charge-group-amount
+                                                                        inc
+                                                                        (/ 2)))
+                                                             (map (fn [index]
+                                                                    {:type :heraldry.component/charge-group-strip
+                                                                     :stretch (if (and (zero? index)
+                                                                                       (even? default-charge-group-amount))
+                                                                                1
+                                                                                (if (and (pos? index)
+                                                                                         (even? default-charge-group-amount))
+                                                                                  (+ 1 (/ (inc index)
+                                                                                          index))
+                                                                                  2))
+                                                                     :slots (if (zero? index)
+                                                                              (if (odd? default-charge-group-amount)
+                                                                                [0]
+                                                                                [0 0])
+                                                                              (-> (concat [0]
+                                                                                          (repeat (dec index) nil)
+                                                                                          [0])
+                                                                                  vec))}))
+                                                             vec)}
+
+                                      :bordure {:type :heraldry.charge-group.type/in-orle
+                                                :slots (vec (repeat default-charge-group-amount 0))}
+
+                                      :orle {:type :heraldry.charge-group.type/in-orle
+                                             :distance 2.5
+                                             :slots (vec (repeat default-charge-group-amount 0))}
+
+                                      {:type :heraldry.charge-group.type/rows
+                                       :spacing (/ 95 default-charge-group-amount)
+                                       :strips [{:type :heraldry.component/charge-group-strip
+                                                 :slots (vec (repeat default-charge-group-amount 0))}]})))
+
+      (= type-namespace
+         "heraldry.ordinary.type") (update
+                                    :field
+                                    (fn [field]
+                                      (cond-> field
+                                        (:components field) (update
+                                                             :components
+                                                             (fn [components]
+                                                               (mapv
+                                                                (fn [component]
+                                                                  (add-charge-group-defaults
+                                                                   component
+                                                                   :parent-ordinary-type type))
+                                                                components)))))))))
+
 (defn blazon->hdn [data]
-  (some-> data
-          parse
-          ast->hdn))
+  (let [hdn (some-> data
+                    parse
+                    ast->hdn)]
+    (->> hdn
+         (walk/prewalk add-charge-group-defaults))))
 
 (comment
   grammar
