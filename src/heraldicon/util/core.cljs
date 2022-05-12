d6e3bf1cb6ac5d46368980c85cafd34696f07062
Oliver Runge <oliver.runge@gmail.com>
2022-05-12 21:42:53 +0200

    Move iso-now and map-keys to util.core

--- src/heraldicon/util/core.cljs
+++ src/heraldicon/util/core.cljs
@@ -1,5 +1,7 @@
 (ns heraldicon.util.core
   (:require
+   [cljs-time.core :as time]
+   [cljs-time.format :as format]
    [heraldicon.config :as config]))
 
 (defn deep-merge-with [f & maps]
@@ -29,3 +31,20 @@
 
 (defn integer-string? [s]
   (re-matches #"^[0-9]+$" s))
+
+(defn map-keys
+  "Applies f to each key of m. Also to keys of m's vals and so on."
+  [f m]
+  (zipmap
+   (map (fn [k]
+          (f k))
+        (keys m))
+   (map (fn [v]
+          (if (map? v)
+            (map-keys f v)
+            v))
+        (vals m))))
+
+(defn iso-now []
+  (->> (time/time-now)
+       (format/unparse (:date-time format/formatters))))
