c81d59ea0a03c8fa8554d0bf8e7a7efa280c5b57
Oliver Runge <oliver.runge@gmail.com>
2022-05-20 20:01:08 +0200

    Add position spec tests

---
+++ test/spec/heraldry/position_test.cljs
@@ -0,0 +1,31 @@
+(ns spec.heraldry.position-test
+  (:require
+   [cljs.test :refer-macros [are deftest]]
+   [spec.heraldicon.test-util :as tu]
+   [spec.heraldry.specs]))
+
+(deftest valid-position
+  (are [form] (tu/valid? :heraldry/position form)
+    {}
+
+    {:point (tu/example :heraldry.position/point)}
+
+    {:offset-x 10}
+
+    {:offset-y 5}
+
+    {:alignment (tu/example :heraldry.position/alignment)}
+
+    {:type (tu/example :heraldry.position/type)}))
+
+(deftest invalid-position
+  (are [form] (tu/invalid? :heraldry/position form)
+    {:point :wrong}
+
+    {:offset-x :wrong}
+
+    {:offset-y :wrong}
+
+    {:alignment :wrong}
+
+    {:type :wrong}))
