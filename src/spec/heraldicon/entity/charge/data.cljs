7dc109867cf2c135220a9b1c5868e6040fc5107f
Oliver Runge <oliver.runge@gmail.com>
2022-05-21 20:49:36 +0200

    Add charge.data spec with tests

--- src/spec/heraldicon/entity/charge/data.cljs
+++ src/spec/heraldicon/entity/charge/data.cljs
@@ -1,8 +1,49 @@
 (ns spec.heraldicon.entity.charge.data
   (:require
-   [cljs.spec.alpha :as s]))
+   [cljs.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as g]
+   [heraldicon.heraldry.option.attributes :as attributes]
+   [heraldicon.heraldry.tincture :as tincture]
+   [spec.heraldicon.spec-util :as su]))
 
 (s/def :heraldicon.entity.charge.data/type #{:heraldicon.entity.charge/data})
 
-;; TODO: add data
-(s/def :heraldicon.entity.charge/data (s/keys :req-un [:heraldicon.entity.charge.data/type]))
+(s/def :heraldicon.entity.charge.data.edn-data/width number?)
+(s/def :heraldicon.entity.charge.data.edn-data/height number?)
+(s/def :heraldicon.entity.charge.data.edn-data/data vector?)
+(s/def :heraldicon.entity.charge.data/edn-data
+  (s/keys :req-un [:heraldicon.entity.charge.data.edn-data/width
+                   :heraldicon.entity.charge.data.edn-data/height
+                   :heraldicon.entity.charge.data.edn-data/data]))
+
+(s/def :heraldicon.entity.charge.data/data (s/keys :req-un [:heraldicon.entity.charge.data.data/edn-data]))
+(s/def :heraldicon.entity.charge.data/charge-type (s/or :keyword keyword?
+                                                        :string su/non-blank-string?))
+(s/def :heraldicon.entity.charge.data/landscape? boolean?)
+(s/def :heraldicon.entity.charge.data/attitude (su/key-in? attributes/attitude-map))
+(s/def :heraldicon.entity.charge.data/facing (su/key-in? attributes/facing-map))
+(s/def :heraldicon.entity.charge.data/fixed-tincture (su/key-in? tincture/fixed-tincture-map))
+(s/def :heraldicon.entity.charge.data/attributes (s/map-of (su/key-in? attributes/attribute-map)
+                                                           boolean?))
+
+(s/def :heraldicon.entity.charge.data.colour/key (s/with-gen
+                                                   (s/and string?
+                                                          #(re-matches #"^#[a-fA-F0-9]{6}$" %))
+                                                   #(g/return "#123456")))
+(s/def :heraldicon.entity.charge.data.colour.value/modifier (su/key-in? attributes/tincture-modifier-for-charge-map))
+(s/def :heraldicon.entity.charge.data.colour.value/qualifier (su/key-in? attributes/tincture-modifier-qualifier-for-charge-map))
+(s/def :heraldicon.entity.charge.data.colour/value (s/or :modifier :heraldicon.entity.charge.data.colour.value/modifier
+                                                         :vector (s/tuple :heraldicon.entity.charge.data.colour.value/modifier
+                                                                          :heraldicon.entity.charge.data.colour.value/qualifier)))
+(s/def :heraldicon.entity.charge.data/colours (s/map-of :heraldicon.entity.charge.data.colour/key
+                                                        :heraldicon.entity.charge.data.colour/value))
+
+(s/def :heraldicon.entity.charge/data (s/keys :req-un [:heraldicon.entity.charge.data/type
+                                                       :heraldicon.entity.charge.data/charge-type]
+                                              :opt-un [:heraldicon.entity.charge.data/edn-data
+                                                       :heraldicon.entity.charge.data/landscape?
+                                                       :heraldicon.entity.charge.data/attitude
+                                                       :heraldicon.entity.charge.data/facing
+                                                       :heraldicon.entity.charge.data/fixed-tincture
+                                                       :heraldicon.entity.charge.data/attributes
+                                                       :heraldicon.entity.charge.data/colours]))
---
+++ test/spec/heraldicon/entity/charge/data_test.cljs
@@ -0,0 +1,165 @@
+(ns spec.heraldicon.entity.charge.data-test
+  (:require
+   [cljs.test :refer-macros [are deftest]]
+   [spec.heraldicon.specs]
+   [spec.heraldicon.test-util :as tu]
+   [spec.heraldry.specs]))
+
+(deftest valid-charge-data
+  (are [form] (tu/valid? :heraldicon.entity.charge/data form)
+    {:type :heraldicon.entity.charge/data
+     :charge-type "foobar"}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :heraldry.charge.type/foobar}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width 100
+                :height 100
+                :data [:g]}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :landscape? true}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :attitude (tu/example :heraldicon.entity.charge.data/attitude)}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :facing (tu/example :heraldicon.entity.charge.data/facing)}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :fixed-tincture (tu/example :heraldicon.entity.charge.data/fixed-tincture)}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :attributes (tu/example :heraldicon.entity.charge.data/attributes)}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :colours (tu/example :heraldicon.entity.charge.data/colours)}))
+
+(deftest invalid-charge-data
+  (are [form] (tu/invalid? :heraldicon.entity.charge/data form)
+    {}
+
+    {:type :wrong
+     :charge-type :foobar}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type 10}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {;; :width missing
+                :height 100
+                :data [:g]}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width :wrong
+                :height 100
+                :data [:g]}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width 100
+                :height :wrong
+                :data [:g]}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width 100
+                ;; :height missing
+                :data [:g]}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width 100
+                :height 100
+                :data :wrong}}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :edn-data {:width 100
+                :height 100
+                ;; :data missing
+                }}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :landscape? :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :attitude :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :facing :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :fixed-tincture :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :attributes :wrong}
+
+    {:type :heraldicon.entity.charge/data
+     :charge-type :foobar
+     :colours :wrong}))
+
+(deftest valid-charge-data-attributes
+  (are [form] (tu/valid? :heraldicon.entity.charge.data/attributes form)
+    {}
+
+    {:erased true
+     :supporter false}))
+
+(deftest invalid-charge-data-attributes
+  (are [form] (tu/invalid? :heraldicon.entity.charge.data/attributes form)
+    {:erased :wrong}
+
+    {:wrong true}
+
+    {"foobar" true}))
+
+(deftest valid-charge-data-colours
+  (are [form] (tu/valid? :heraldicon.entity.charge.data/colours form)
+    {}
+
+    {"#192dfa" :primary}
+
+    {"#000000" :outline}
+
+    {"#00FF00" :shadow}
+
+    {"#192dfa" [:primary :shadow-05]}
+
+    {"#ffFFff" [:tertiary :highlight-25]}))
+
+(deftest invalid-charge-data-colours
+  (are [form] (tu/invalid? :heraldicon.entity.charge.data/colours form)
+    {:wrong :primary}
+
+    {"wrong" :primary}
+
+    {"#123" :primary}
+
+    {"#192dfa" :wrong}
+
+    {"#192dfa" [:wrong :shadow-05]}
+
+    {"#ffFFff" [:tertiary :wrong]}))
