6ffb17d411b366ffc6c956a896f1d895f0691f99
Oliver Runge <oliver.runge@gmail.com>
2022-05-27 11:13:36 +0200

    Fix state hook after data restructuring

--- src/heraldicon/frontend/state_hook.cljs
+++ src/heraldicon/frontend/state_hook.cljs
@@ -1,13 +1,13 @@
 (ns heraldicon.frontend.state-hook)
 
-(defn handle-db-changes [old-db new-db]
-  (let [scope-path [:arms-form :render-options :scope]]
+(defn ^:export handle-db-changes [old-db new-db]
+  (let [scope-path [:arms-form :data :achievement :render-options :scope]]
     (cond-> new-db
       (and (= (get-in new-db scope-path)
               :coat-of-arms)
-           (not= (-> old-db :arms-form :helms)
-                 (-> new-db :arms-form :helms))) (assoc-in scope-path :coat-of-arms-and-helm)
+           (not= (-> old-db :arms-form :data :achievement :helms)
+                 (-> new-db :arms-form :data :achievement :helms))) (assoc-in scope-path :coat-of-arms-and-helm)
       (and (#{:coat-of-arms
               :coat-of-arms-and-helm} (get-in new-db scope-path))
-           (not= (-> old-db :arms-form :ornaments)
-                 (-> new-db :arms-form :ornaments))) (assoc-in scope-path :achievement))))
+           (not= (-> old-db :arms-form :data :achievement :ornaments)
+                 (-> new-db :arms-form :data :achievement :ornaments))) (assoc-in scope-path :achievement))))
