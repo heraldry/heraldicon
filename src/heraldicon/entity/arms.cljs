c6bcd588716f8c302433c1cc413abf00b715c0d1
Oliver Runge <oliver.runge@gmail.com>
2022-06-16 09:47:43 +0200

    Include version in shortened arms URL

--- src/heraldicon/entity/arms.cljs
+++ src/heraldicon/entity/arms.cljs
@@ -3,11 +3,14 @@
    [heraldicon.config :as config]
    [heraldicon.entity.id :as id]))
 
-(defn short-url [arms-data]
+(defn short-url [arms-data & {:keys [without-version?]}]
   (if (= (config/get :stage) "prod")
-    (let [{:keys [id version]} arms-data]
+    (let [{:keys [id version latest-version]} arms-data
+          link-version (if (zero? version)
+                         latest-version
+                         version)]
       (when (and id version)
-        (if (zero? version)
+        (if without-version?
           (str "https://coa.to/" (id/for-url id))
-          (str "https://coa.to/" (id/for-url id) "/" version))))
+          (str "https://coa.to/" (id/for-url id) "/" link-version))))
     "https://dev"))
