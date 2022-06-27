505e3845687615d8944691c890809fe346d1662a
Oliver Runge <oliver.runge@gmail.com>
2022-06-27 12:40:34 +0200

    Add more number parsing to util.number

--- src/heraldicon/util/number.cljs
+++ src/heraldicon/util/number.cljs
@@ -40,3 +40,96 @@
 
 (defn from-roman [s]
   (reduce add-numeral (map numerals (reverse (s/upper-case s)))))
+
+(def ^:private digits
+  #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
+
+(def ^:private number-strings
+  {"one" 1
+   "two" 2
+   "double" 2
+   "three" 3
+   "triple" 3
+   "four" 4
+   "five" 5
+   "six" 6
+   "seven" 7
+   "eight" 8
+   "nine" 9
+   "ten" 10
+   "eleven" 11
+   "twelve" 12
+   "thirteen" 13
+   "fourteen" 14
+   "fifteen" 15
+   "sixteen" 16
+   "seventeen" 17
+   "eighteen" 18
+   "nineteen" 19
+   "twenty" 20
+   "thirty" 30
+   "forty" 40
+   "fifty" 50
+   "sixty" 60
+   "seventy" 70
+   "eighty" 80
+   "ninety" 90})
+
+(defn from-string [s]
+  (cond
+    (s/blank? s) nil
+    (every? digits s) (js/parseInt s)
+    (every? numerals (some-> s s/upper-case)) (from-roman s)
+    :else (get number-strings (some-> s s/lower-case))))
+
+(defn- ordinal-plain [s]
+  (when-let [s (some-> s s/trim)]
+    (when (s/ends-with? s ".")
+      (from-string (subs s 0 (dec (count s)))))))
+
+(def ^:private ordinals
+  {"1st" 1
+   "2nd" 2
+   "3rd" 3
+   "4th" 4
+   "5th" 5
+   "6th" 6
+   "7th" 7
+   "8th" 8
+   "9th" 9
+   "10th" 10
+   "11th" 11
+   "12th" 12
+   "13th" 13
+   "14th" 14
+   "15th" 15
+   "16th" 16
+   "17th" 17
+   "18th" 18
+   "19th" 19
+   "20th" 20
+
+   "first" 1
+   "second" 2
+   "third" 3
+   "fourth" 4
+   "fifth" 5
+   "sixth" 6
+   "seventh" 7
+   "eighth" 8
+   "ninth" 9
+   "tenth" 10
+   "eleventh" 11
+   "twelfth" 12
+   "thirteenth" 13
+   "fourteenth" 14
+   "fifteenth" 15
+   "sixteenth" 16
+   "seventeenth" 17
+   "eighteenth" 18
+   "nineteenth" 19
+   "twentieth" 20})
+
+(defn ordinal-from-string [s]
+  (or (ordinal-plain s)
+      (ordinals s)))
