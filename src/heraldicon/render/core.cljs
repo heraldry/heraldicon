2a88301219a042c3b87c0151bf6f9d7f278046d1
Oliver Runge <oliver.runge@gmail.com>
2022-06-27 20:56:02 +0200

    Move helms rendering to render.helms

--- src/heraldicon/render/core.cljs
+++ src/heraldicon/render/core.cljs
@@ -13,76 +13,11 @@
    [heraldicon.math.core :as math]
    [heraldicon.math.vector :as v]
    [heraldicon.render.coat-of-arms :as coat-of-arms]
+   [heraldicon.render.helms :as helms]
    [heraldicon.render.outline :as outline]
    [heraldicon.svg.path :as path]
    [heraldicon.util.colour :as colour]
    [heraldicon.util.uid :as uid]))
-
-(defn helm [context & {:keys [below-shield?]}]
-  (into [:<>]
-        (map (fn [[idx self-below-shield?]]
-               ^{:key idx}
-               [interface/render-component
-                (-> context
-                    (c/++ :components idx)
-                    (assoc :auto-resize? false
-                           :self-below-shield? self-below-shield?
-                           :render-pass-below-shield? below-shield?))])
-             (interface/get-element-indices (c/++ context :components)))))
-
-(defn helms [context width]
-  (let [num-helms (interface/get-list-size (c/++ context :elements))]
-    (if (zero? num-helms)
-      {:width 0
-       :height 0
-       :result nil}
-      (let [gap-part-fn (fn [n] (+ 2 (* 2 (dec n))))
-            gap-part (gap-part-fn num-helms)
-            helm-width (/ (* gap-part width)
-                          (inc (* (inc gap-part)
-                                  num-helms)))
-            helm-height (* 2 helm-width)
-            total-height helm-height
-            gap (/ helm-width gap-part)
-            helm-width-with-gap (+ helm-width gap)]
-        {:width (- width
-                   (* 2 gap))
-         :height total-height
-         :result-below-shield (into [:g]
-                                    (map (fn [idx]
-                                           (let [helm-environment (environment/create
-                                                                   nil
-                                                                   {:bounding-box (bb/BoundingBox.
-                                                                                   (+ (* idx helm-width-with-gap)
-                                                                                      gap)
-                                                                                   (+ (* idx helm-width-with-gap)
-                                                                                      gap
-                                                                                      helm-width)
-                                                                                   (- helm-height)
-                                                                                   0)})]
-                                             ^{:key idx}
-                                             [helm (-> context
-                                                       (c/++ :elements idx)
-                                                       (assoc :environment helm-environment))
-                                              :below-shield? true])))
-                                    (range num-helms))
-         :result-above-shield (into [:g]
-                                    (map (fn [idx]
-                                           (let [helm-environment (environment/create
-                                                                   nil
-                                                                   {:bounding-box (bb/BoundingBox.
-                                                                                   (+ (* idx helm-width-with-gap)
-                                                                                      gap)
-                                                                                   (+ (* idx helm-width-with-gap)
-                                                                                      gap
-                                                                                      helm-width)
-                                                                                   (- helm-height)
-                                                                                   0)})]
-                                             ^{:key idx}
-                                             [helm (-> context
-                                                       (c/++ :elements idx)
-                                                       (assoc :environment helm-environment))])))
-                                    (range num-helms))}))))
 
 (defn ribbon [{:keys [select-component-fn
                       svg-export?] :as context}
@@ -455,7 +390,7 @@
                                  {:width 0
                                   :height 0
                                   :result nil}
-                                 (helms
+                                 (helms/render
                                   (c/++ context :helms)
                                   100))
 
---
+++ src/heraldicon/render/helms.cljs
@@ -0,0 +1,72 @@
+(ns heraldicon.render.helms
+  (:require
+   [heraldicon.context :as c]
+   [heraldicon.heraldry.field.environment :as environment]
+   [heraldicon.interface :as interface]
+   [heraldicon.math.bounding-box :as bb]))
+
+(defn helm [context & {:keys [below-shield?]}]
+  (into [:<>]
+        (map (fn [[idx self-below-shield?]]
+               ^{:key idx}
+               [interface/render-component
+                (-> context
+                    (c/++ :components idx)
+                    (assoc :auto-resize? false
+                           :self-below-shield? self-below-shield?
+                           :render-pass-below-shield? below-shield?))])
+             (interface/get-element-indices (c/++ context :components)))))
+
+(defn render [context width]
+  (let [num-helms (interface/get-list-size (c/++ context :elements))]
+    (if (zero? num-helms)
+      {:width 0
+       :height 0
+       :result nil}
+      (let [gap-part-fn (fn [n] (+ 2 (* 2 (dec n))))
+            gap-part (gap-part-fn num-helms)
+            helm-width (/ (* gap-part width)
+                          (inc (* (inc gap-part)
+                                  num-helms)))
+            helm-height (* 2 helm-width)
+            total-height helm-height
+            gap (/ helm-width gap-part)
+            helm-width-with-gap (+ helm-width gap)]
+        {:width (- width
+                   (* 2 gap))
+         :height total-height
+         :result-below-shield (into [:g]
+                                    (map (fn [idx]
+                                           (let [helm-environment (environment/create
+                                                                   nil
+                                                                   {:bounding-box (bb/BoundingBox.
+                                                                                   (+ (* idx helm-width-with-gap)
+                                                                                      gap)
+                                                                                   (+ (* idx helm-width-with-gap)
+                                                                                      gap
+                                                                                      helm-width)
+                                                                                   (- helm-height)
+                                                                                   0)})]
+                                             ^{:key idx}
+                                             [helm (-> context
+                                                       (c/++ :elements idx)
+                                                       (assoc :environment helm-environment))
+                                              :below-shield? true])))
+                                    (range num-helms))
+         :result-above-shield (into [:g]
+                                    (map (fn [idx]
+                                           (let [helm-environment (environment/create
+                                                                   nil
+                                                                   {:bounding-box (bb/BoundingBox.
+                                                                                   (+ (* idx helm-width-with-gap)
+                                                                                      gap)
+                                                                                   (+ (* idx helm-width-with-gap)
+                                                                                      gap
+                                                                                      helm-width)
+                                                                                   (- helm-height)
+                                                                                   0)})]
+                                             ^{:key idx}
+                                             [helm (-> context
+                                                       (c/++ :elements idx)
+                                                       (assoc :environment helm-environment))])))
+                                    (range num-helms))}))))
