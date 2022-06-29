6f2bf6aa9ce232f2a2303aa7676714294e7b480d
Oliver Runge <oliver.runge@gmail.com>
2022-06-29 14:21:44 +0200

    Remove unused context key :component-selected-fn?

--- src/heraldicon/frontend/context.cljs
+++ src/heraldicon/frontend/context.cljs
@@ -4,6 +4,4 @@
 
 (def default
   {:load-charge-data charge/fetch-charge-data
-   ;; TODO: re-write this feature: allowing highlighting/selecting components in the preview
-   :component-selected-fn? nil
    :select-component-fn nil})
