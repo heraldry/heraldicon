e1622692b09bc8924dcd9c3910c269c5a7cb5af8
Oliver Runge <oliver.runge@gmail.com>
2022-06-29 14:12:45 +0200

    Remove unnecessary threading

--- src/heraldicon/frontend/http.cljs
+++ src/heraldicon/frontend/http.cljs
@@ -7,15 +7,13 @@
 
 (defn fetch [url]
   (go-catch
-   (-> (http/get url)
-       <?
-       (as-> response
-             (let [status (:status response)
-                   body (:body response)
-                   data (if (and (s/ends-with? url ".edn")
-                                 (string? body))
-                          (reader/read-string body)
-                          body)]
-               (if (= status 200)
-                 data
-                 (throw (ex-info "HTTP request failed" {:error body} :fetch-url-failed))))))))
+   (let [response (<? (http/get url))
+         status (:status response)
+         body (:body response)
+         data (if (and (s/ends-with? url ".edn")
+                       (string? body))
+                (reader/read-string body)
+                body)]
+     (if (= status 200)
+       data
+       (throw (ex-info "HTTP request failed" {:error body} :fetch-url-failed))))))
