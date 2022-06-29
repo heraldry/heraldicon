de010d822e67acbe455e1939918e5986b48db940
Oliver Runge <oliver.runge@gmail.com>
2022-06-29 12:32:11 +0200

    Unwind math.core/close-to-zero?

--- src/heraldicon/math/core.cljs
+++ src/heraldicon/math/core.cljs
@@ -1,9 +1,7 @@
 (ns heraldicon.math.core)
 
 (defn close-to-zero? [^js/Number value]
-  (-> value
-      Math/abs
-      (< 0.000000000001)))
+  (< (Math/abs value) 0.000000000001))
 
 (defn percent-of [^js/Number base-value v]
   (when v
