684230840d9a9a33080f157c59eb099ff6cc7362
Oliver Runge <oliver.runge@gmail.com>
2022-08-22 15:37:20 +0200

    Implement new rendering system for mascle charge

--- src/heraldicon/heraldry/charge/type/mascle.cljs
+++ src/heraldicon/heraldry/charge/type/mascle.cljs
@@ -2,7 +2,7 @@
   (:require
    [heraldicon.heraldry.charge.interface :as charge.interface]
    [heraldicon.heraldry.charge.shared :as charge.shared]
-   [heraldicon.math.vector :as v]))
+   [heraldicon.interface :as interface]))
 
 (def charge-type :heraldry.charge.type/mascle)
 
@@ -13,28 +13,27 @@
       (update :geometry dissoc :mirrored?)
       (update :geometry dissoc :reversed?)))
 
-(defmethod charge.interface/render-charge charge-type
-  [context]
-  (charge.shared/make-charge
-   context
-   :height
-   (fn [height]
-     (let [width (/ height 1.3)
-           width-half (/ width 2)
-           height-half (/ height 2)
-           hole-width (* width 0.55)
-           hole-height (* height 0.55)
-           hole-width-half (/ hole-width 2)
-           hole-height-half (/ hole-height 2)]
-       {:shape {:paths [["m" (v/Vector. 0 (- height-half))
-                         "l" (v/Vector. width-half height-half)
-                         "l " (v/Vector. (- width-half) height-half)
-                         "l" (v/Vector. (- width-half) (- height-half))
-                         "z"]
-                        ["m" (v/Vector. 0 (- hole-height-half))
-                         "l" (v/Vector. hole-width-half hole-height-half)
-                         "l " (v/Vector. (- hole-width-half) hole-height-half)
-                         "l" (v/Vector. (- hole-width-half) (- hole-height-half))
-                         "z"]]}
-        :charge-width width
-        :charge-height height}))))
+(def ^:private base
+  (let [height 100
+        width (/ height 1.3)
+        width-half (/ width 2)
+        height-half (/ height 2)
+        hole-width (* width 0.55)
+        hole-height (* height 0.55)
+        hole-width-half (/ hole-width 2)
+        hole-height-half (/ hole-height 2)]
+    {:base-shape [["M" 0 (- height-half)
+                   "l" width-half height-half
+                   "l " (- width-half) height-half
+                   "l" (- width-half) (- height-half)
+                   "z"]
+                  ["M" 0 (- hole-height-half)
+                   "l" hole-width-half hole-height-half
+                   "l " (- hole-width-half) hole-height-half
+                   "l" (- hole-width-half) (- hole-height-half)
+                   "z"]]
+     :base-width width
+     :base-height height}))
+
+(defmethod interface/properties charge-type [context]
+  (charge.shared/process-shape context base))
