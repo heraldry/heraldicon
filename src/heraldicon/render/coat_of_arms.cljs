18b7a5694a7d304d782853eb9cbac38f103e3b21
Oliver Runge <oliver.runge@gmail.com>
2022-08-26 14:56:13 +0200

    Remove render.coat-of-arms after all usages have been migrated

--- src/heraldicon/render/coat_of_arms.cljs
+++
@@ -1,8 +0,0 @@
-(ns heraldicon.render.coat-of-arms
-  (:require
-   [heraldicon.interface :as interface]))
-
-(defn render [context width]
-  (let [context (assoc context :target-width width)]
-    {:environment (interface/get-environment context)
-     :result (interface/render-component context)}))
