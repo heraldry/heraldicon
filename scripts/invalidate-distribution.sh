f8fce06fe83599ac88e1dc7da5a534b82a91a4aa
Oliver Runge <oliver.runge@gmail.com>
2023-01-23 22:00:45 +0100

    Rewrite invalidate-distribution.sh to use babashka

--- scripts/invalidate-distribution.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-distribution_id="$(aws --profile heraldry cloudfront list-distributions | jq -r '.DistributionList.Items[] | select([.Aliases.Items[] == "'"$1"'"] | any) | .Id')"
-aws --profile heraldry cloudfront create-invalidation --distribution-id "$distribution_id" --paths '/*' | cat
