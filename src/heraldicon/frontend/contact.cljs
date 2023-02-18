08b4d4503857331eebcfe732924354ff934ba2ce
Oliver Runge <oliver.runge@gmail.com>
2023-02-18 16:51:04 +0100

    Replace contact page strings and keep them English

--- src/heraldicon/frontend/contact.cljs
+++ src/heraldicon/frontend/contact.cljs
@@ -1,7 +1,6 @@
 (ns heraldicon.frontend.contact
   (:require
    [heraldicon.config :as config]
-   [heraldicon.frontend.language :refer [tr]]
    [heraldicon.frontend.title :as title]
    [re-frame.core :as rf]))
 
@@ -12,24 +11,22 @@
                  :min-width "30em"
                  :max-width "60em"
                  :margin "auto"}}
-   [:h2 [tr :string.menu/contact]]
+   [:h2 "Contact"]
    [:p
-    [tr :string.text.contact/feel-free-to-contact-me]
-    " "
+    "Email me for feature requests, bug reports, feedback, or questions: "
     (let [email-address (config/get :email-address)]
       [:a {:href (str "mailto:" email-address)} email-address])]
 
    [:p {:style {:margin-top "1em"}}
-    [tr :string.text.contact/project-info]
-    ": "
+    "The project can be found here: "
     [:a {:href "https://github.com/heraldry" :target "_blank"} "github:heraldry"]]
 
-   [:p [tr :string.text.contact/developer-info] ":"]
+   [:p "I can be found here:"]
    [:ul
-    [:li [:a {:href "https://www.reddit.com/r/heraldry/" :target "_blank"} "reddit/heraldry"]
-     ", " [tr :string.text.contact/my-username-is] " " [:em "tierced"]]
     [:li [:a {:href "https://discord.gg/EGbMW8dth2" :target "_blank"} "discord:Heraldicon"]
-     ", " [tr :string.text.contact/my-username-is] " " [:em "or#5915"]]]
+     ", my username is " [:em "or#5915"]]
+    [:li [:a {:href "https://www.reddit.com/r/heraldry/" :target "_blank"} "reddit/heraldry"]
+     ", my username is " [:em "tierced"]]]
    [:iframe
     {:sandbox "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts",
      :frameBorder "0",
@@ -37,15 +34,14 @@
      :height "500",
      :width "350",
      :src "https://discord.com/widget?id=847884812836012072&theme=dark"}]
-   [:h2 [tr :string.text.contact/translation]]
+   [:h2 "Translation"]
    [:p
-    [tr :string.text.contact/translation-not-yet-complete]
-    " "
+    "Not all strings have been translated yet. If you want to help out or correct something, then reach out on Discord or the "
     [:a {:href "https://crowdin.com/project/heraldicon/" :target "_blank"}
-     [tr :string.text.contact/crowdin-project]]
+     "Crowdin-Project"]
     "."]
-   [:h3 [tr :string.text.contact/translation-credits]]
-   [:p [tr :string.text.contact/translation-credits-intro]]
+   [:h3 "Credits"]
+   [:p "Thanks to everybody helping out with the translations!"]
    [:ul
     [:li "Українська: Dughorm"]
     [:li "Italiano: ashoppio#2022 (Brando Maria Pernice)"]]])
