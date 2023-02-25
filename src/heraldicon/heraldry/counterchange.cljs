523f90c6a7b37067b794b0c8f62521b51a883d6f
Oliver Runge <oliver.runge@gmail.com>
2023-02-25 16:34:55 +0100

    Use breadth-first search for counterchange tinctures

--- src/heraldicon/heraldry/counterchange.cljs
+++ src/heraldicon/heraldry/counterchange.cljs
@@ -2,20 +2,33 @@
   (:require
    [heraldicon.context :as c]))
 
+(defn- breadth-first-tree-seq [branch? children tree]
+  (loop [result []
+         queue (conj [] tree)]
+    (if (seq queue)
+      (let [next-node (first queue)]
+        (if (branch? next-node)
+          (recur (conj result next-node)
+                 (into (next queue) (children next-node)))
+          (recur (conj result next-node)
+                 (next queue))))
+      result)))
+
 (defn- collect-tinctures [field context]
   (let [tincture-mapping (c/tincture-mapping context)]
     (->> field
-         (tree-seq #(or (map? %)
-                        (vector? %)
-                        (seq? %)) (fn [data]
-                                    (if (map? data)
-                                     ;; look in this order to find the most important two tinctures
-                                      (concat (:fields data)
-                                              [(:field data)]
-                                              [(:charge data)]
-                                              (:components data)
-                                              (:charges data))
-                                      (seq data))))
+         (breadth-first-tree-seq #(or (map? %)
+                                      (vector? %)
+                                      (seq? %))
+                                 (fn [data]
+                                   (if (map? data)
+                       ;; look in this order to find the most important two tinctures
+                                     (concat (:fields data)
+                                             [(:field data)]
+                                             [(:charge data)]
+                                             (:components data)
+                                             (:charges data))
+                                     (seq data))))
 
          (filter #(and (map? %)
                        (-> % :type (= :heraldry.field.type/plain))
