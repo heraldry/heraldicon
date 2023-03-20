13d1aefceb525ef6d3bafb7909009b3bd0ed064d
Oliver Runge <oliver.runge@gmail.com>
2023-03-20 21:06:19 +0100

    Improve flory line-style shape and its eccentricity

--- src/heraldicon/heraldry/line/type/flory.cljs
+++ src/heraldicon/heraldry/line/type/flory.cljs
@@ -1,8 +1,73 @@
 (ns heraldicon.heraldry.line.type.flory
-  (:require [heraldicon.math.vector :as v]))
+  (:require
+   [heraldicon.math.vector :as v]))
 
-(defn- mirror [v]
+(defn mirror [v]
   (v/dot v (v/Vector. -1 1)))
+
+(defn scale-vectors [vs width height-factor]
+  (let [stretch (v/Vector. 1 height-factor)]
+    (into {}
+          (map (fn [[k v]]
+                 [k (-> v
+                        (v/mul width)
+                        (v/dot stretch))]))
+          vs)))
+
+(defn morph [from to t]
+  (into {}
+        (map (fn [k]
+               (let [from-v (get from k)
+                     to-v (get to k)]
+                 [k (-> (v/sub to-v from-v)
+                        (v/mul t)
+                        (v/add from-v))])))
+        (keys from)))
+
+(def normal-vectors
+  {:base-left (v/Vector. 0.3 0)
+   :base-right (v/Vector. 0.7 0)
+   :point-left (v/Vector. 0.17 -0.13)
+   :point-right (v/Vector. 0.83 -0.13)
+   :tip (v/Vector. 0.5 -0.75)
+   :top-left (v/Vector. 0.45 -0.25)
+   :top-right (v/Vector. 0.55 -0.25)
+   :anchor1 (v/Vector. 0.2 0)
+   :anchor2 (v/Vector. 0.2 -0.2)
+   :anchor3 (v/Vector. -0.15 -0.15)
+   :anchor4 (v/Vector. -0.15 -0.3)
+   :anchor5 (v/Vector. 0 -0.25)
+   :anchor6 (v/Vector. -0.25 0.25)})
+
+(def low-eccentricity-vectors
+  {:base-left (v/Vector. 0.35 0)
+   :base-right (v/Vector. 0.65 0)
+   :point-left (v/Vector. 0.17 -0.1)
+   :point-right (v/Vector. 0.83 -0.1)
+   :tip (v/Vector. 0.5 -0.75)
+   :top-left (v/Vector. 0.4 -0.25)
+   :top-right (v/Vector. 0.6 -0.25)
+   :anchor1 (v/Vector. 0.1 0)
+   :anchor2 (v/Vector. 0.3 -0.1)
+   :anchor3 (v/Vector. 0 -0.15)
+   :anchor4 (v/Vector. -0.2 -0.05)
+   :anchor5 (v/Vector. 0 -0.25)
+   :anchor6 (v/Vector. -0.1 0.15)})
+
+(def high-eccentricity-vectors
+  {:base-left (v/Vector. 0.35 0)
+   :base-right (v/Vector. 0.65 0)
+   :point-left (v/Vector. 0.25 -0.1)
+   :point-right (v/Vector. 0.75 -0.1)
+   :tip (v/Vector. 0.5 -0.75)
+   :top-left (v/Vector. 0.45 -0.2)
+   :top-right (v/Vector. 0.55 -0.2)
+   :anchor1 (v/Vector. 0.15 -0.1)
+   :anchor2 (v/Vector. -0.0 -0.25)
+   :anchor3 (v/Vector. -0.3 -0.2)
+   :anchor4 (v/Vector. -0.2 -0.4)
+   :anchor5 (v/Vector. 0.05 -0.3)
+   :anchor6 (v/Vector. -0.3 0.25)})
 
 (def pattern
   {:display-name :string.line.type/flory
@@ -10,47 +75,22 @@
                           height
                           width]}
                   _line-options]
-               (let [dx (* (-> eccentricity
-                               (* 2)))
-                     dx2 (* (-> eccentricity
-                                (* 0.5)
-                                (+ 0.75)))
-                     dx2 (Math/sqrt dx2)
-                     dx3 1
-                     half-width (/ width 2)
-                     base-width (* (* 0.6 dx2) half-width)
-                     half-base-width (/ base-width 2)
-                     ds (* half-base-width dx)
-                     base-dist (/ base-width 2)
-                     base-left (v/Vector. (- half-width base-dist) 0)
-                     base-right (v/Vector. (+ half-width base-dist) 0)
-                     point-dist (* 1.1 dx3 base-width)
-                     point-y (- (* 0.9 dx3 (/ base-width 2)))
-                     point-left (v/Vector. (- half-width point-dist) point-y)
-                     point-right (v/Vector. (+ half-width point-dist) point-y)
-                     tip (v/Vector. half-width (- (* 3 0.5 half-width)))
-                     top-dist (/ base-width 5)
-                     top-y (- (* 1 dx3 0.5 half-width))
-                     top-left (v/Vector. (- half-width top-dist) top-y)
-                     top-right (v/Vector. (+ half-width top-dist) top-y)
-                     anchor1 (v/Vector. ds (- ds))
-                     anchor2 (v/Vector. 0 (* -1.5 ds dx2))
-                     anchor3 (v/Vector. (* -1.75 ds dx2) (* -1.5 ds dx2))
-                     anchor4 (v/Vector. (* -1 ds dx2) (* -2 ds dx2))
-                     anchor5 (v/Vector. 0 (* -1.5 ds dx2))
-                     anchor6 (v/Vector. (* -1.75 ds dx2) (* 1.75 ds dx2))
-
-                     tip (v/dot tip (v/Vector. 1 height))
-                     top-left (v/dot top-left (v/Vector. 1 height))
-                     top-right (v/dot top-right (v/Vector. 1 height))
-                     point-left (v/dot point-left (v/Vector. 1 height))
-                     point-right (v/dot point-right (v/Vector. 1 height))
-                     anchor1 (v/dot anchor1 (v/Vector. 1 height))
-                     anchor2 (v/dot anchor2 (v/Vector. 1 height))
-                     anchor3 (v/dot anchor3 (v/Vector. 1 height))
-                     anchor4 (v/dot anchor4 (v/Vector. 1 height))
-                     anchor5 (v/dot anchor5 (v/Vector. 1 height))
-                     anchor6 (v/dot anchor6 (v/Vector. 1 height))]
+               (let [vectors (if (< eccentricity 0.5)
+                               (morph normal-vectors low-eccentricity-vectors (- 1 (* 2 eccentricity)))
+                               (morph normal-vectors high-eccentricity-vectors (* 2 (- eccentricity 0.5))))
+                     {:keys [base-left
+                             base-right
+                             tip
+                             top-left
+                             top-right
+                             point-left
+                             point-right
+                             anchor1
+                             anchor2
+                             anchor3
+                             anchor4
+                             anchor5
+                             anchor6]} (scale-vectors vectors width height)]
 
                  {:pattern ["l"
                             base-left
--- src/heraldicon/heraldry/line/type/flory_counter_flory.cljs
+++ src/heraldicon/heraldry/line/type/flory_counter_flory.cljs
@@ -1,8 +1,7 @@
 (ns heraldicon.heraldry.line.type.flory-counter-flory
-  (:require [heraldicon.math.vector :as v]))
-
-(defn- mirror [v]
-  (v/dot v (v/Vector. -1 1)))
+  (:require
+   [heraldicon.heraldry.line.type.flory :as flory]
+   [heraldicon.math.vector :as v]))
 
 (defn- flip [v]
   (v/dot v (v/Vector. 1 -1)))
@@ -14,47 +13,23 @@
                           width
                           spacing]}
                   _line-options]
-               (let [dx (* (-> eccentricity
-                               (* 2)))
-                     dx2 (* (-> eccentricity
-                                (* 0.5)
-                                (+ 0.75)))
-                     dx2 (Math/sqrt dx2)
-                     dx3 1
-                     half-width (/ width 4)
-                     base-width (* (* 0.6 dx2) half-width)
-                     half-base-width (/ base-width 2)
-                     ds (* half-base-width dx)
-                     base-dist (/ base-width 2)
-                     base-left (v/Vector. (- half-width base-dist) 0)
-                     base-right (v/Vector. (+ half-width base-dist) 0)
-                     point-dist (* 1.1 dx3 base-width)
-                     point-y (- (* 0.9 dx3 (/ base-width 2)))
-                     point-left (v/Vector. (- half-width point-dist) point-y)
-                     point-right (v/Vector. (+ half-width point-dist) point-y)
-                     tip (v/Vector. half-width (- (* 3 0.5 half-width)))
-                     top-dist (/ base-width 5)
-                     top-y (- (* 1 dx3 0.5 half-width))
-                     top-left (v/Vector. (- half-width top-dist) top-y)
-                     top-right (v/Vector. (+ half-width top-dist) top-y)
-                     anchor1 (v/Vector. ds (- ds))
-                     anchor2 (v/Vector. 0 (* -1.5 ds dx2))
-                     anchor3 (v/Vector. (* -1.75 ds dx2) (* -1.5 ds dx2))
-                     anchor4 (v/Vector. (* -1 ds dx2) (* -2 ds dx2))
-                     anchor5 (v/Vector. 0 (* -1.5 ds dx2))
-                     anchor6 (v/Vector. (* -1.75 ds dx2) (* 1.75 ds dx2))
-
-                     tip (v/dot tip (v/Vector. 1 height))
-                     top-left (v/dot top-left (v/Vector. 1 height))
-                     top-right (v/dot top-right (v/Vector. 1 height))
-                     point-left (v/dot point-left (v/Vector. 1 height))
-                     point-right (v/dot point-right (v/Vector. 1 height))
-                     anchor1 (v/dot anchor1 (v/Vector. 1 height))
-                     anchor2 (v/dot anchor2 (v/Vector. 1 height))
-                     anchor3 (v/dot anchor3 (v/Vector. 1 height))
-                     anchor4 (v/dot anchor4 (v/Vector. 1 height))
-                     anchor5 (v/dot anchor5 (v/Vector. 1 height))
-                     anchor6 (v/dot anchor6 (v/Vector. 1 height))
+               (let [half-width (/ width 2)
+                     vectors (if (< eccentricity 0.5)
+                               (flory/morph flory/normal-vectors flory/low-eccentricity-vectors (- 1 (* 2 eccentricity)))
+                               (flory/morph flory/normal-vectors flory/high-eccentricity-vectors (* 2 (- eccentricity 0.5))))
+                     {:keys [base-left
+                             base-right
+                             tip
+                             top-left
+                             top-right
+                             point-left
+                             point-right
+                             anchor1
+                             anchor2
+                             anchor3
+                             anchor4
+                             anchor5
+                             anchor6]} (flory/scale-vectors vectors width height)
 
                      flory ["l"
                             base-left
@@ -75,18 +50,18 @@
                             (v/sub tip top-left)
 
                             "c"
-                            (mirror anchor6)
-                            (v/add (v/sub top-right tip) (mirror anchor5))
+                            (flory/mirror anchor6)
+                            (v/add (v/sub top-right tip) (flory/mirror anchor5))
                             (v/sub top-right tip)
 
                             "c"
-                            (mirror anchor4)
-                            (v/add (v/sub point-right top-right) (mirror anchor3))
+                            (flory/mirror anchor4)
+                            (v/add (v/sub point-right top-right) (flory/mirror anchor3))
                             (v/sub point-right top-right)
 
                             "c"
-                            (mirror anchor2)
-                            (v/add (v/sub base-right point-right) (mirror anchor1))
+                            (flory/mirror anchor2)
+                            (v/add (v/sub base-right point-right) (flory/mirror anchor1))
                             (v/sub base-right point-right)
 
                             "l" (- (* 2 half-width) (:x base-right)) 0]
