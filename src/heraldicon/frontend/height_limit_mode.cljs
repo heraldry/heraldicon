704f270e48222892576219726e757404c1e196aa
Oliver Runge <oliver.runge@gmail.com>
2024-01-26 16:48:04 +0100

    Add tooltip for height-limit-mode button

--- src/heraldicon/frontend/height_limit_mode.cljs
+++ src/heraldicon/frontend/height_limit_mode.cljs
@@ -1,6 +1,7 @@
 (ns heraldicon.frontend.height-limit-mode
   (:require
    [heraldicon.frontend.js-event :as js-event]
+   [heraldicon.frontend.language :refer [tr]]
    [heraldicon.frontend.user.session :as session]
    [re-frame.core :as rf]))
 
@@ -9,9 +10,13 @@
     "height-limited"))
 
 (defn selector []
-  [:a.fas {:class (if @(rf/subscribe [::session/height-limit-mode?])
-                    "fa-arrows-alt"
-                    "fa-expand")
-           :href "#"
-           :on-click (js-event/handled
-                      #(rf/dispatch [::session/toggle-height-limit-mode]))}])
+  (let [height-limit-mode? @(rf/subscribe [::session/height-limit-mode?])]
+    [:a.fas {:class (if height-limit-mode?
+                      "fa-arrows-alt"
+                      "fa-expand")
+             :title (tr (if height-limit-mode?
+                          :string.tooltip/expand
+                          :string.tooltip/fit-in-window))
+             :href "#"
+             :on-click (js-event/handled
+                        #(rf/dispatch [::session/toggle-height-limit-mode]))}]))
