68e7a0041503a0ba8d799f575f091e5c07fcac65
Oliver Runge <oliver.runge@gmail.com>
2024-02-10 17:34:35 +0100

    Request with and accept transit encoded data

--- src/heraldicon/frontend/api.cljs
+++ src/heraldicon/frontend/api.cljs
@@ -2,15 +2,20 @@
   (:require
    [cljs-http.client :as http]
    [com.wsscode.async.async-cljs :refer [<? go-catch]]
-   [heraldicon.config :as config]))
+   [heraldicon.config :as config]
+   [heraldicon.util.encoding :as encoding]))
 
 (defn call [name payload session]
   (go-catch
-   (let [response (<? (http/post (config/get :heraldicon-api-endpoint)
-                                 {:headers (when-let [session-id (:session-id session)]
-                                             {"Session-Id" session-id})
-                                  :edn-params {:call name
-                                               :data payload}}))
+   (let [content-type "application/transit+json"
+         accept "application/transit+json"
+         {:keys [encode-fn]} (encoding/for-mimetype content-type)
+         response (<? (http/post (config/get :heraldicon-api-endpoint)
+                                 {:headers {"Session-Id" (:session-id session)
+                                            "Content-Type" content-type
+                                            "Accept" accept}
+                                  :body (encode-fn {:call name
+                                                    :data payload})}))
          status (:status response)
          body (:body response)]
      (if (= status 200)
