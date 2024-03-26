35c87dc97a615385babe3799b7a8e7fd29e750f1
Oliver Runge <oliver.runge@gmail.com>
2024-03-26 11:59:17 +0100

    Remove request-id from logging

--- src/heraldicon/util/log_appender.cljs
+++ src/heraldicon/util/log_appender.cljs
@@ -2,18 +2,11 @@
   (:require
    [clojure.string :as str]))
 
-(def ^:dynamic *request-id*
-  nil)
-
-(defn set-request-id
-  [context]
-  (set! *request-id* (:awsRequestId (js->clj context :keywordize-keys true))))
-
 (defn simple-appender
   [{:as _appender-opts :keys []}]
   {:enabled? true
    :fn (fn [{:keys [output_]}]
-         (let [message (str *request-id* " " (str/trim (force output_)))]
+         (let [message (str/trim (force output_))]
            (if js/process.stdout
              (js/process.stdout.write (str message "\n"))
              (js/console.log message))))})
