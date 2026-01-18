92a9cf30ed877c9e0bcc453dbb0054ac0abb317b
Oliver Runge <oliver.runge@gmail.com>
2026-01-18 13:47:18 +0100

    Upgrade random to 5.4.1

--- src/heraldicon/util/random.cljs
+++ src/heraldicon/util/random.cljs
@@ -1,11 +1,10 @@
 (ns heraldicon.util.random
   (:refer-clojure :exclude [float])
   (:require
-   ["random" :as random-lib]
-   ["seedrandom" :as seedrandom]))
+   ["random" :as random-lib]))
 
 (defn seed [seed]
-  (.use random-lib (seedrandom seed)))
+  (.use random-lib/default seed))
 
 (defn float []
-  (.float random-lib))
+  (.float random-lib/default))
